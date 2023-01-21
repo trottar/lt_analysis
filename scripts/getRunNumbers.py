85fc6e184ed49b754a6fa28ebed174558955f9f8
Richard Trotta <trotta@cua.edu>
2023-01-21 12:59:46 -0500

    Testing phi vs t plots new script

--- scripts/getRunNumbers.py
+++ scripts/getRunNumbers.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-21 12:57:31 trottar"
+# Time-stamp: "2023-01-21 12:58:05 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -15,7 +15,7 @@ import sys, os
 inp_f = sys.argv[1]
 
 if not os.path.exists(inp_f):
-    print("{} not found...")
+    print("")
 else:
     # Read in run numbers from run list and save as a string where each run is separated by a space (format for bash input)
     f_data = ''
