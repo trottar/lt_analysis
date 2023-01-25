014986c7660c2810876418e57565cd6b0ecd9cf1
Richard Trotta <trotta@cua.edu>
2023-01-24 20:45:25 -0500

    Combined lists for simc and prod

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-24 20:34:33 trottar"
+# Time-stamp: "2023-01-24 20:34:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -551,8 +551,8 @@ def defineHists(phi_setting):
         if(HMS_FixCut & HMS_Acceptance & SHMS_FixCut & SHMS_Acceptance & Diamond):
 
           MM_vs_CoinTime_DATA.Fill(evt.MM, evt.CTime_ROC1)
-          CoinTime_vs_beta_DATA.Fill(evt.CTime_ROC1,evt.P_gtr_eta)
-          MM_vs_beta_DATA.Fill(evt.MM,evt.P_gtr_eta)
+          CoinTime_vs_beta_DATA.Fill(evt.CTime_ROC1,evt.P_gtr_beta)
+          MM_vs_beta_DATA.Fill(evt.MM,evt.P_gtr_beta)
           phiq_vs_t_DATA.Fill(evt.ph_q, -evt.MandelT)
           Q2_vs_W_DATA.Fill(evt.Q2, evt.W)
             
