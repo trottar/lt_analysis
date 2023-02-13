b51c1833944c9895d81974de0339892344250d39
Richard Trotta <trotta@cua.edu>
2023-02-13 13:02:27 -0500

    testing new root combine script

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -531,8 +531,7 @@ if [[ $a_flag = "true" ]]; then
 	arg2="Cut_Kaon_Events_all_RF"
 	arg3="${OutDATAFilename}_Right"
 	arg4="${data_right[*]}"
-	rootrun="root -l<<
-	EOF	
+	rootrun="root -l<<EOF	
 	.L $SCRIPT+		
 	CombineRootFiles($arg1, $arg2, $arg3, arg4)
 	EOF"
