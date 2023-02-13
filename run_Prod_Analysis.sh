692bf356551d01be59de26ee2d6aca7917e87aa7
Richard Trotta <trotta@cua.edu>
2023-02-13 13:22:10 -0500

    testing new root combine script

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -528,9 +528,11 @@ if [[ $a_flag = "true" ]]; then
 	echo
 	cd "${LTANAPATH}/scripts"
 	SCRIPT="mergeRootFiles.C"
-	rootrun="root -l<<EOF	
-	.L $SCRIPT+		
-	mergeRootFiles(\"${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/\", \"_-1_Raw_Data\", \"Cut_Kaon_Events_all_RF\", \"${OutDATAFilename}_Right\", \"${data_right[*]}\")EOF"
+	rootrun=
+"root -l<<EOF
+.L $SCRIPT+
+mergeRootFiles(\"${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/\", \"_-1_Raw_Data\", \"Cut_Kaon_Events_all_RF\", \"${OutDATAFilename}_Right\", \"${data_right[*]}\")
+EOF"
 	eval "${rootrun}"
 	for i in "${data_right[@]}"
 	do
