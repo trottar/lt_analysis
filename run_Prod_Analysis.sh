2781f88801efc747473c180a0345964036ecc51a
Richard Trotta <trotta@cua.edu>
2023-02-13 16:21:17 -0500

    testing new root combine script

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -533,10 +533,10 @@ if [[ $a_flag = "true" ]]; then
 	echo
 	cd "${LTANAPATH}/scripts"
 	#python3 mergeRootFiles.py "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/" "_-1_Raw_Data" "Uncut_Kaon_Events Cut_Kaon_Events_all_noRF Cut_Kaon_Events_prompt_noRF Cut_Kaon_Events_rand_noRF Cut_Kaon_Events_all_RF Cut_Kaon_Events_prompt_RF Cut_Kaon_Events_rand_RF" "${OutDATAFilename}_Right" "${data_right[*]}"
-	mv ${i}_-1_Proc_Data.root ${i}_-1_Raw_Data.root # <runNum>_-1_Proc_Data.root is used in later LT_analysis
 	if [ -f "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/${OutDATAFilename}_Right.root" ]; then
 	    for i in "${data_right[@]}"
 	    do
+		mv ${i}_-1_Proc_Data.root ${i}_-1_Raw_Data.root # <runNum>_-1_Proc_Data.root is used in later LT_analysis
 		if [ -f "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/${i}_-1_Raw_Data.root" ]; then
 		    cd "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT"
 		    echo "Combining run $i with ${OutDATAFilename}_Right.root..."
@@ -562,10 +562,10 @@ if [[ $a_flag = "true" ]]; then
 	echo
 	cd "${LTANAPATH}/scripts"
 	#python3 mergeRootFiles.py "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/" "_-1_Raw_Data" "Uncut_Kaon_Events Cut_Kaon_Events_all_noRF Cut_Kaon_Events_prompt_noRF Cut_Kaon_Events_rand_noRF Cut_Kaon_Events_all_RF Cut_Kaon_Events_prompt_RF Cut_Kaon_Events_rand_RF" "${OutDATAFilename}_Left" "${data_left[*]}"
-	mv ${i}_-1_Proc_Data.root ${i}_-1_Raw_Data.root # <runNum>_-1_Proc_Data.root is used in later LT_analysis
 	if [ -f "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/${OutDATAFilename}_Left.root" ]; then
 	    for i in "${data_left[@]}"
 	    do
+		mv ${i}_-1_Proc_Data.root ${i}_-1_Raw_Data.root # <runNum>_-1_Proc_Data.root is used in later LT_analysis
 		if [ -f "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/${i}_-1_Raw_Data.root" ]; then
 		    cd "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT"
 		    echo "Combining run $i with ${OutDATAFilename}_Left.root..."
@@ -591,10 +591,10 @@ if [[ $a_flag = "true" ]]; then
 	echo
 	cd "${LTANAPATH}/scripts"
 	#python3 mergeRootFiles.py "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/" "_-1_Raw_Data" "Uncut_Kaon_Events Cut_Kaon_Events_all_noRF Cut_Kaon_Events_prompt_noRF Cut_Kaon_Events_rand_noRF Cut_Kaon_Events_all_RF Cut_Kaon_Events_prompt_RF Cut_Kaon_Events_rand_RF" "${OutDATAFilename}_Center" "${data_center[*]}"
-	mv ${i}_-1_Proc_Data.root ${i}_-1_Raw_Data.root # <runNum>_-1_Proc_Data.root is used in later LT_analysis
 	if [ -f "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/${OutDATAFilename}_Center.root" ]; then
 	    for i in "${data_center[@]}"
 	    do
+		mv ${i}_-1_Proc_Data.root ${i}_-1_Raw_Data.root # <runNum>_-1_Proc_Data.root is used in later LT_analysis
 		if [ -f "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT/${i}_-1_Raw_Data.root" ]; then
 		    cd "${LTANAPATH}/OUTPUT/Analysis/${ANATYPE}LT"
 		    echo "Combining run $i with ${OutDATAFilename}_Center.root..."
