96032059f1084149b08a6a939078b31f509bc3e8
Richard Trotta <trotta@cua.edu>
2023-02-15 13:33:23 -0500

    testing  vs phi plotting

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-15 13:30:05 trottar"
+# Time-stamp: "2023-02-15 13:33:12 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1316,7 +1316,7 @@ def defineHists(phi_setting):
 # Call histogram function above to define dictonaries for right, left, center settings
 # Put these all into an array so that if we are missing a setting it is easier to remove
 # Plus it makes the code below less repetitive
-phisetlist = ["Right","Left","Center"]
+phisetlist = ["Center","Left","Right"]
 histlist = []
 for phiset in phisetlist:
     histlist.append(defineHists(phiset))
