5f8b0606c63e969ff46bb9049c8d92539ccbd98b
Richard Trotta <trotta@cua.edu>
2023-02-16 17:41:35 -0500

    testing new t binning code with phi binning

--- scripts/Prod/subtraction.py
+++ scripts/Prod/subtraction.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-16 17:39:00 trottar"
+# Time-stamp: "2023-02-16 17:41:32 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -94,8 +94,8 @@ def defineHists(phi_setting, inpDict):
     Q2Val = float(Q2.replace("p","."))
     WVal = float(W.replace("p","."))
 
-    Q2min = Q2Val - (3/7)*Q2Val # Minimum value of Q2 on the Q2 vs W plot
-    Q2max = Q2Val + (3/7)*Q2Val # Maximum value of Q2 on the Q2 vs W plot
+    Q2min = Q2Val - (1/7)*Q2Val # Minimum value of Q2 on the Q2 vs W plot
+    Q2max = Q2Val + (1/7)*Q2Val # Maximum value of Q2 on the Q2 vs W plot
     Wmin = WVal - (3/7)*WVal # min y-range for Q2vsW plot
     Wmax = WVal + (3/7)*WVal # max y-range for Q2vsW plot
 
