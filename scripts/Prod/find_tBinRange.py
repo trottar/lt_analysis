f9554a40d24bcf05568236366f756e12c64b8f15
Richard Trotta <trotta@cua.edu>
2023-02-16 01:46:16 -0500

    testing  vs phi plotting

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-16 01:00:20 trottar"
+# Time-stamp: "2023-02-16 01:46:13 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1896,7 +1896,8 @@ for i,hist in enumerate(histlist):
     # set colors for the TGraphPolar object
     hist["polar_phiq_vs_t_DATA"].SetMarkerSize(2)
     hist["polar_phiq_vs_t_DATA"].SetMarkerColor(i+1)
-    hist["polar_phiq_vs_t_DATA"].Draw("AOP")
+    #hist["polar_phiq_vs_t_DATA"].Draw("AOP")
+    hist["polar_phiq_vs_t_DATA"].Draw("SURF2 POL")
     Cphtsame.Update()
     hist["polar_phiq_vs_t_DATA"].GetPolargram().SetRangeRadial(0, 2.0)
     # Hide radial axis labels since redefined below
