b4234aea18aa3d53349ba7d189f47a9da4b3a4bb
Richard Trotta <trotta@cua.edu>
2023-02-19 17:43:37 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-19 17:37:45 trottar"
+# Time-stamp: "2023-02-19 17:43:33 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -237,7 +237,7 @@ def find_tbins(H_t_BinTest):
         # In this case, this returns evenly spaced values within a given interval
         # yp -> np.sort(x) : he y-coordinates of the data points
         # In this case, this returns a sorted copy of the array
-        return np.interp(np.linspace(0, npt, nbin + 1),np.arange(npt),np.sort(x))
+        return np.interp(np.linspace(0, npt, nbin),np.arange(npt),np.sort(x))
 
     print("\nFinding t bins...")
     # Histogram takes the array data set and the bins as input
@@ -336,10 +336,6 @@ tbinedges = binned_t[1]
 #tbinedges = np.append(binned_t[1],tmin)
 #tbinedges = np.append(tbinedges, tmax)
 phibinedges = binned_phi[1]
-
-print("\n\n~~~~~~~~~~~~~~~~~~")
-print(tbinedges)
-print(phibinedges)
 
 for i,hist in enumerate(histlist):
     for j in range(NumtBins):
