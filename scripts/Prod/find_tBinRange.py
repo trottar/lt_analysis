4acbc803e39b599da1ebd9c9db1c2d8f2ab5ce62
Richard Trotta <trotta@cua.edu>
2023-02-19 18:24:24 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-19 18:17:30 trottar"
+# Time-stamp: "2023-02-19 18:24:21 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -336,8 +336,8 @@ tbinedges = binned_t[1]
 phibinedges = binned_phi[1]
 
 for i,hist in enumerate(histlist):
-    for j in range(len(tbinedges)):
-        for k in range(len(phibinedges)):
+    for j in range(NumtBins+1):
+        for k in range(NumPhiBins+1):
             hist["H_tbins_DATA"].Fill(tbinedges[j])
             hist["H_phibins_DATA"].Fill(phibinedges[k])
         
