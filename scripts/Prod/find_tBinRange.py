83a716c8fa1b5810b2c739d507da570fdfdb3d07
Richard Trotta <trotta@cua.edu>
2023-02-20 03:07:47 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-20 03:04:25 trottar"
+# Time-stamp: "2023-02-20 03:07:44 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -396,9 +396,8 @@ for i,hist in enumerate(histlist):
     # Extract the desired values from each group
     yieldDict = {}
     for key, val in groups.items():
-        yieldDict[key].append(integrate.simps(val)*hist["normfac_data"])
+        hist["yieldDictData"][key] = integrate.simps(val)*hist["normfac_data"]
         hist["H_yield_DATA"].Fill(integrate.simps(val),hist["normfac_data"])
-    hist["yieldDictData"] = yieldDict
 
     print("\n\n~~~~~~~~~~~~~~~",hist["yieldDictSimc"])
     print("~~~~~~~~~~~~~~~",hist["H_yield_DATA"])
@@ -442,9 +441,8 @@ for i,hist in enumerate(histlist):
     # Extract the desired values from each group
     yieldDict = {}
     for key, val in groups.items():
-        yieldDict[key].append(integrate.simps(val)*hist["normfac_simc"])
+        hist["yieldDictSimc"][key] = integrate.simps(val)*hist["normfac_simc"]
         hist["H_yield_SIMC"].Fill(integrate.simps(val),hist["normfac_simc"])
-    hist["yieldDictSimc"] = yieldDict
 
     print("\n\n~~~~~~~~~~~~~~~",hist["yieldDictSimc"])
     print("~~~~~~~~~~~~~~~",hist["H_yield_SIMC"])
