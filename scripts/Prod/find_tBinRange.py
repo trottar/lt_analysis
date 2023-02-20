182c23f4c45b52e7342f34228b4a9cb1d17e15cd
Richard Trotta <trotta@cua.edu>
2023-02-19 21:10:34 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-19 21:03:50 trottar"
+# Time-stamp: "2023-02-19 21:10:18 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -451,15 +451,14 @@ c_yield_simc.Print(outputpdf)
 
 c_relyield_data = TCanvas()
 
-for i,hist in enumerate(histlist):
-    hist["H_yield_DATA"].Sumw2()
-    hist["H_yield_SIMC"].Sumw2()
-    yieldClone = hist["H_yield_DATA"].Clone()
-    yieldClone.Divide(hist["H_yield_SIMC"])
-    for i in range(yieldClone.GetNbinsX()):
-        print(yieldClone.GetBinContent(i+1))
+x = np.array([hist["H_yield_DATA"].GetBinContent(i) for i in range(1, hist["H_yield_DATA"].GetNbinsX()+1)])
+y = np.array([hist["H_yield_SIMC"].GetBinContent(i) for i in range(1, hist["H_yield_SIMC"].GetNbinsX()+1)])
 
-    hist["H_relyield_DATA"].Add(yieldClone)
+z = x/y
+
+for i,hist in enumerate(histlist):
+    for j,val in enumerate(hist["H_yield_DATA"]):
+        hist["H_relyield_DATA"].SetBinContent(i+1,z[i])
 
 for i,hist in enumerate(histlist):
     hist["H_relyield_DATA"].SetLineColor(i+1)
