8c6565b655667262319a90366b81ae0402642573
Richard Trotta <trotta@cua.edu>
2023-02-20 00:59:58 -0500

    Testing integration of average kinematics fortran script

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -159,8 +159,7 @@ NumPhiBins=16
 # Define global variables for lt_analysis scripts
 POL="+1" # All KaonLT is positive polarity
 TMIN=0.010
-#TMAX=0.990
-TMAX=1.990
+TMAX=0.990
 KSet=1 # Arbitrary value
 
 # Efficiency csv file
