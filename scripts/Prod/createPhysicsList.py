00eefe8d2815d236aa7605fb1a4cc19b1231fbcc
Richard Trotta <trotta@cua.edu>
2023-02-20 01:47:19 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-20 01:44:19 trottar"
+# Time-stamp: "2023-02-20 01:47:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -111,6 +111,16 @@ if float(runNumRight[0]) != 0:
     relyield_right_data = [TBRANCH_RIGHT_DATA.Get("H_relyield_DATA").GetBinContent(i) for i in range(1, TBRANCH_RIGHT_DATA.Get("H_relyield_DATA").GetNbinsX()+1)]
     phibin_right_data = [TBRANCH_RIGHT_DATA.Get("H_phibins_DATA").GetBinContent(i) for i in range(1, TBRANCH_RIGHT_DATA.Get("H_phibins_DATA").GetNbinsX()+1)]
     tbin_right_data = [TBRANCH_RIGHT_DATA.Get("H_tbins_DATA").GetBinContent(i) for i in range(1, TBRANCH_RIGHT_DATA.Get("H_tbins_DATA").GetNbinsX()+1)]
+
+if float(runNumLeft[0]) != 0:
+    relyield_left_data = [TBRANCH_LEFT_DATA.Get("H_relyield_DATA").GetBinContent(i) for i in range(1, TBRANCH_LEFT_DATA.Get("H_relyield_DATA").GetNbinsX()+1)]
+    phibin_left_data = [TBRANCH_LEFT_DATA.Get("H_phibins_DATA").GetBinContent(i) for i in range(1, TBRANCH_LEFT_DATA.Get("H_phibins_DATA").GetNbinsX()+1)]
+    tbin_left_data = [TBRANCH_LEFT_DATA.Get("H_tbins_DATA").GetBinContent(i) for i in range(1, TBRANCH_LEFT_DATA.Get("H_tbins_DATA").GetNbinsX()+1)]
+
+if float(runNumCenter[0]) != 0:
+    relyield_center_data = [TBRANCH_CENTER_DATA.Get("H_relyield_DATA").GetBinContent(i) for i in range(1, TBRANCH_CENTER_DATA.Get("H_relyield_DATA").GetNbinsX()+1)]
+    phibin_center_data = [TBRANCH_CENTER_DATA.Get("H_phibins_DATA").GetBinContent(i) for i in range(1, TBRANCH_CENTER_DATA.Get("H_phibins_DATA").GetNbinsX()+1)]
+    tbin_center_data = [TBRANCH_CENTER_DATA.Get("H_tbins_DATA").GetBinContent(i) for i in range(1, TBRANCH_CENTER_DATA.Get("H_tbins_DATA").GetNbinsX()+1)]
 
 InFile_DATA.Close()
 
