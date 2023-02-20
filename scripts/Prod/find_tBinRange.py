bada84ecf4195abff4918190a86f0abefc871096
Richard Trotta <trotta@cua.edu>
2023-02-19 20:49:08 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-19 20:44:38 trottar"
+# Time-stamp: "2023-02-19 20:49:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -440,8 +440,8 @@ for i,hist in enumerate(histlist):
     # Extract the desired values from each group
     yieldDict = {}
     for key, val in groups.items():
-        if integrate.simps(val)*hist["normfac_simc"] == 0.0:
-            print("\n\n~~~~~~~~~~~~~~~~~")
+        if integrate.simps(val)*hist["normfac_simc"] =< 1e-9:
+            print("\n\n~~~~~~~~~~~~~~~~~",integrate.simps(val)*hist["normfac_simc"])
         yieldDict[key] = integrate.simps(val)*hist["normfac_simc"]
         hist["H_yield_SIMC"].Fill(integrate.simps(val)*hist["normfac_simc"])
     hist["yieldDictSimc"] = yieldDict
