ae6c6a0024e72768192fbaad5ac102af1064c5e5
Richard Trotta <trotta@cua.edu>
2023-02-20 04:11:39 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-20 04:08:37 trottar"
+# Time-stamp: "2023-02-20 04:11:35 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -99,17 +99,17 @@ InFile_DATA = up.open(InDATAFilename)
 ###############################################################################################################################################
 
 if float(runNumRight[0]) != 0:
-    yield_right_data = InFile_DATA["Right Data/H_yield_DATA"].edges
+    yield_right_data = InFile_DATA["Right Data/H_yield_DATA"].axis().edges()
     phibin_right_data = InFile_DATA["Right Data/H_phibins_DATA"].values
     tbin_right_data = InFile_DATA["Right Data/H_tbins_DATA"].values
 
 if float(runNumLeft[0]) != 0:
-    yield_left_data = InFile_DATA["Left Data/H_yield_DATA"].edges
+    yield_left_data = InFile_DATA["Left Data/H_yield_DATA"].axis().edges()
     phibin_left_data = InFile_DATA["Left Data/H_phibins_DATA"].values
     tbin_left_data = InFile_DATA["Left Data/H_tbins_DATA"].values
 
 if float(runNumCenter[0]) != 0:
-    yield_center_data = InFile_DATA["Center Data/H_yield_DATA"].edges
+    yield_center_data = InFile_DATA["Center Data/H_yield_DATA"].axis().edges()
     phibin_center_data = InFile_DATA["Center Data/H_phibins_DATA"].values
     tbin_center_data = InFile_DATA["Center Data/H_tbins_DATA"].values
 
