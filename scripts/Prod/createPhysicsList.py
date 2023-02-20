92e4a6e22b11f9108532808a203b9a36fd80cb63
Richard Trotta <trotta@cua.edu>
2023-02-20 05:02:48 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-20 04:58:25 trottar"
+# Time-stamp: "2023-02-20 05:02:45 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -94,24 +94,14 @@ OUTPATH=lt.OUTPATH
 
 InDATAFilename = OUTPATH+"/" + OutFilename + ".root"
 
-InFile_DATA = up.open(InDATAFilename)
+InFile_DATA = ROOT.TFile.Open(InDATAFilename,"READ")
 
 ###############################################################################################################################################
 
 if float(runNumRight[0]) != 0:
-    yield_right_data = InFile_DATA["Right Data/H_yield_DATA"].GetArray()
-    phibin_right_data = InFile_DATA["Right Data/H_phibin_DATA"].GetArray()
-    tbin_right_data = InFile_DATA["Right Data/H_tbin_DATA"].GetArray()
-
-if float(runNumLeft[0]) != 0:
-    yield_left_data = InFile_DATA["Left Data/H_yield_DATA"].GetArray()
-    phibin_left_data = InFile_DATA["Left Data/H_phibin_DATA"].GetArray()
-    tbin_left_data = InFile_DATA["Left Data/H_tbin_DATA"].GetArray()
-
-if float(runNumCenter[0]) != 0:
-    yield_center_data = InFile_DATA["Center Data/H_yield_DATA"].GetArray()
-    phibin_center_data = InFile_DATA["Center Data/H_phibin_DATA"].GetArray()
-    tbin_center_data = InFile_DATA["Center Data/H_tbin_DATA"].GetArray()
+    yield_right_data = InFile_DATA.Get["Right Data/H_yield_DATA"].GetArray()
+    phibin_right_data = InFile_DATA.Get["Right Data/H_phibin_DATA"].GetArray()
+    tbin_right_data = InFile_DATA.Get["Right Data/H_tbin_DATA"].GetArray()
 
 print("\n\n~~~~~~~~~",yield_left_data)
 print("~~~~~~~~~",tbin_left_data)
