0b9b4e4b341af93f677138dd2daf090f9b76475d
Richard Trotta <trotta@cua.edu>
2023-02-24 11:16:42 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-24 11:06:14 trottar"
+# Time-stamp: "2023-02-24 11:16:32 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -326,7 +326,7 @@ if float(runNumRight[0]) != 0:
                     write_to_file(f_list,check_line)
         lines = f.readlines()
     first_line = "{:.6f}\n".format(1.0)
-    last_line = "{:.6f}\n".format(1.0)
+    last_line = "{:.6f}".format(1.0)
     lines.insert(0,first_line)
     lines.append(last_line)
     write_to_file(f_list,"".join(lines),write_mode='w')
@@ -347,7 +347,7 @@ if float(runNumLeft[0]) != 0:
                     write_to_file(f_list,check_line)
         lines = f.readlines()
     first_line = "{:.6f}\n".format(1.0)
-    last_line = "{:.6f}\n".format(1.0)
+    last_line = "{:.6f}".format(1.0)
     lines.insert(0,first_line)
     lines.append(last_line)
     write_to_file(f_list,"".join(lines),write_mode='w')
