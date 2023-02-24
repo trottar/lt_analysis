50ed0fe123a0ddae64bc990946b815b65a55a3df
Richard Trotta <trotta@cua.edu>
2023-02-24 11:58:33 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-24 11:56:11 trottar"
+# Time-stamp: "2023-02-24 11:58:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -334,7 +334,7 @@ if float(runNumRight[0]) != 0:
         write_to_file(f_list,"".join(lines),write_mode='w')
 
 if float(runNumLeft[0]) != 0:
-    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_-{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_left*1000))
+    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_+{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_left*1000))
 
     if not os.path.exists(f_list):
         open(f_list, "w").close()    
@@ -357,7 +357,7 @@ if float(runNumLeft[0]) != 0:
         write_to_file(f_list,"".join(lines),write_mode='w')
 
 if float(runNumCenter[0]) != 0:
-    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_-{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_center*1000))
+    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_-0000.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100)
 
     if not os.path.exists(f_list):
         open(f_list, "w").close()    
