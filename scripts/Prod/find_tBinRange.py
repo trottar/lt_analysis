b735e0cafe3fd9e4787c8adda1a33d6c206c076c
Richard Trotta <trotta@cua.edu>
2023-02-25 16:23:00 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/find_tBinRange.py
+++ scripts/Prod/find_tBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-25 16:08:13 trottar"
+# Time-stamp: "2023-02-25 16:22:58 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -442,7 +442,7 @@ for i,hist in enumerate(histlist):
                     groups[key].append((t[2], Q2_aver[j][1], W_aver[j][1], t_aver[j][1]))
                 else:
                     groups[key] = [(t[2], Q2_aver[j][1], W_aver[j][1], t_aver[j][1])]
-    print(groups)        
+                    
     yieldValData = array('d', [0])
     hist["yieldTree"].Branch("yield_data", yieldValData, "yield_data/D")
     Q2binValData = array('d', [0])
@@ -459,6 +459,7 @@ for i,hist in enumerate(histlist):
         W_tmp = []
         t_tmp = []
         for tup in val:
+            print(key, tup[1])
             MM_tmp.append(tup[0])
             Q2_tmp.append(tup[1])
             W_tmp.append(tup[2])
