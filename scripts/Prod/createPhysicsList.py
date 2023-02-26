bd8966f92648e34dfb34d7b8a39d57c33fb477fc
Richard Trotta <trotta@cua.edu>
2023-02-25 19:18:09 -0500

    Testing integration of average kinematics fortran script

--- scripts/Prod/createPhysicsList.py
+++ scripts/Prod/createPhysicsList.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-25 19:11:23 trottar"
+# Time-stamp: "2023-02-25 19:18:01 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -335,11 +335,9 @@ if float(runNumRight[0]) != 0:
     with open(f_list, 'r') as f:
         lines = f.readlines()[1:-1]
         tbin_right_data.append(0.0)
-        for i, relyield in enumerate(relyield_right):
-            #if relyield != 0.0:
-            if tbin_right_data[i] != tbin_right_data[i+1]:
-                check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_right_data[i], 1.0, averW_right_data[i], 1.0, avert_right_data[i], 1.0)
-                write_to_file(f_list,check_line)
+        for i, Q2 in enumerate(averQ2_right_data):
+            check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_right_data[i], 1.0, averW_right_data[i], 1.0, avert_right_data[i], 1.0)
+            write_to_file(f_list,check_line)
         lines = f.readlines()
         first_line = "{:.6f}\n".format(1.0)
         last_line = "{:.6f}".format(1.0)
@@ -356,11 +354,9 @@ if float(runNumLeft[0]) != 0:
     with open(f_list, 'r') as f:
         lines = f.readlines()[1:-1]
         tbin_left_data.append(0.0)
-        for i, relyield in enumerate(relyield_left):
-            #if relyield != 0.0:
-            if tbin_left_data[i] != tbin_left_data[i+1]:
-                check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_left_data[i], 1.0, averW_left_data[i], 1.0, avert_left_data[i], 1.0)
-                write_to_file(f_list,check_line)
+        for i, Q2 in enumerate(averQ2_right_data):
+            check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_left_data[i], 1.0, averW_left_data[i], 1.0, avert_left_data[i], 1.0)
+            write_to_file(f_list,check_line)
         lines = f.readlines()
         first_line = "{:.6f}\n".format(1.0)
         last_line = "{:.6f}".format(1.0)
@@ -377,11 +373,9 @@ if float(runNumCenter[0]) != 0:
     with open(f_list, 'r') as f:
         lines = f.readlines()[1:-1]
         tbin_center_data.append(0.0)
-        for i, relyield in enumerate(relyield_center):
-            #if relyield != 0.0:
-            if tbin_center_data[i] != tbin_center_data[i+1]:
-                check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_center_data[i], 1.0, averW_center_data[i], 1.0, avert_center_data[i], 1.0)
-                write_to_file(f_list,check_line)
+        for i, Q2 in enumerate(averQ2_right_data):
+            check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_center_data[i], 1.0, averW_center_data[i], 1.0, avert_center_data[i], 1.0)
+            write_to_file(f_list,check_line)
         lines = f.readlines()
         first_line = "{:.6f}\n".format(1.0)
         last_line = "{:.6f}".format(1.0)
