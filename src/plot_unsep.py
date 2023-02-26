9b36839fbe39848271eb7769a6403e0a0cb1069d
Richard Trotta <trotta@cua.edu>
2023-02-26 18:53:11 -0500

    testing plotting script for unsep

--- src/plot_unsep.py
+++ src/plot_unsep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-26 18:50:08 trottar"
+# Time-stamp: "2023-02-26 18:52:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -89,7 +89,7 @@ setting_file = LTANAPATH+"/src/list.settings"
 file_df_dict['setting_df'] = file_to_df(setting_file, ['POL', 'Q2', 'EPSVAL', 'thpq', 'TMIN', 'TMAX', 'NumtBins', 'Kset'])
 
 for i,row in file_df_dict['setting_df'].iterrows():
-    if row['Q2'] == Q2:
+    if row['Q2'] == Q2.replace("p","."):
         file_df_dict['beam_file'] = file_to_df(LTANAPATH+"/src/beam/Eb_KLT.dat", ['ebeam', 'Q2', 'EPSVAL'])
         file_df_dict['avek_file'] = file_to_df(LTANAPATH+"/src/averages/avek.{}.dat".format(Q2.replace("p","")) \
                                                , ['W', 'dW', 'Q2', 'dQ2', 't', 'dt', 'th_pos'])
