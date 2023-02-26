b011eb02f6a22d100e44ac6383e69d553c33ded2
Richard Trotta <trotta@cua.edu>
2023-02-26 13:03:26 -0500

    Testing integration of average kinematics fortran script

--- run_xsect.sh
+++ run_xsect.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-26 12:55:51 trottar"
+# Time-stamp: "2023-02-26 13:03:22 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -37,6 +37,120 @@ HOST=`echo ${PATHFILE_INFO} | cut -d ','  -f15`
 SIMCPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f16`
 LTANAPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f17`
 
+# Flag definitions (flags: h)
+while getopts 'h' flag; do
+    case "${flag}" in
+        h) 
+        echo "--------------------------------------------------------------"
+        echo "./run_xsect.sh -{flags} {variable arguments, see help}"
+	echo
+        echo "Description: Get the average kinematics per t,phi bin."
+	echo "             Then find the unsep xsect for given kinematic."
+        echo "--------------------------------------------------------------"
+        echo
+        echo "The following flags can be called for the heep analysis..."
+        echo "    -h, help"
+	echo "     EPSILON=arg1, Q2=arg2, W=arg3"
+	echo
+	echo " Avaliable Kinematics..."	
+	echo "                      Q2=5p5, W=3p02"
+	echo "                      Q2=4p4, W=2p74"
+	echo "                      Q2=3p0, W=3p14"
+	echo "                      Q2=3p0, W=2p32"
+	echo "                      Q2=2p1, W=2p95"
+	echo "                      Q2=0p5, W=2p40"
+        exit 0
+        ;;
+	a) a_flag='true' ;;
+        t) t_flag='true' ;;
+        *) print_usage
+        exit 1 ;;
+    esac
+done
+
+
+EPSILON=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+Q2=$2
+W=$3
+echo "Epsilon must be - high - low - Case Sensitive!"
+echo "Q2 must be one of - [5p5 - 4p4 - 3p0 - 2p1 - 0p5]"
+echo "W must be one of - [3p02 - 2p74 - 3p14 - 2p32 - 2p95 - 2p40]"
+if [[ -z "$1" || ! "$EPSILON" =~ high|low ]]; then # Check the 1st argument was provided and that it's one of the valid options
+    echo ""
+    echo "I need a valid epsilon..."
+    while true; do
+	echo ""
+	read -p "Epsilon must be - high - low - Case Sensitive! - or press ctrl-c to exit : " EPSILON
+	case $EPSILON in
+	    '');; # If blank, prompt again
+	    'high'|'low') break;; # If a valid option, break the loop and continue
+	esac
+    done
+fi
+if [[ -z "$2" || ! "$Q2" =~ 5p5|4p4|3p0|2p1|0p5 ]]; then # Check the 2nd argument was provided and that it's one of the valid options
+    echo ""
+    echo "I need a valid Q2..."
+    while true; do
+	echo ""
+	read -p "Q2 must be one of - [5p5 - 4p4 - 3p0 - 2p1 - 0p5] - or press ctrl-c to exit : " Q2
+	case $Q2 in
+	    '');; # If blank, prompt again
+	    '5p5'|'4p4'|'3p0'|'2p1'|'0p5') break;; # If a valid option, break the loop and continue
+	esac
+    done
+fi
+if [[ -z "$3" || ! "$W" =~ 3p02|2p74|3p14|2p32|2p95|2p40 ]]; then # Check the 3rd argument was provided and that it's one of the valid options
+    echo ""
+    echo "I need a valid W..."
+    while true; do
+	echo ""
+	read -p "W must be one of - [3p02 - 2p74 - 3p14 - 2p32 - 2p95 - 2p40] - or press ctrl-c to exit : " W
+	case $W in
+	    '');; # If blank, prompt again
+	    '3p02'|'2p74'|'3p14'|'2p32'|'2p95'|'2p40') break;; # If a valid option, break the loop and continue
+	esac
+    done
+fi
+
+
+echo
+echo "---------------------------------------------------------"
+echo
+echo "Finding unesp xsect for Q2=${Q2}, W=${W}  setting..."
+echo
+echo "---------------------------------------------------------"
+echo
+
+if [[ $Q2 = "5p5" && $W = "3p02" ]]; then
+    LOEPS=0.1838
+    HIEPS=0.5291
+fi
+
+if [[ $Q2 = "4p4" && $W = "2p74" ]]; then
+    LOEPS=0.4805
+    HIEPS=0.7148
+fi
+
+if [[ $Q2 = "3p0" && $W = "3p14" ]]; then
+    LOEPS=0.3935
+    HIEPS=0.6668
+fi
+
+if [[ $Q2 = "3p0" && $W = "2p32" ]]; then
+    LOEPS=0.5736
+    HIEPS=0.8791
+fi
+
+if [[ $Q2 = "2p1" && $W = "2p95" ]]; then
+    LOEPS=0.2477
+    HIEPS=0.7864
+fi
+
+if [[ $Q2 = "0p5" && $W = "2p40" ]]; then
+    LOEPS=0.4515
+    HIEPS==0.6979
+fi
+
 
 cd "${LTANAPATH}/src/"
 echo
@@ -44,7 +158,7 @@ echo "Compiling average_kinematics.f..."
 eval "gfortran -o average_kinematics average_kinematics.f"
 echo
 echo "Running average_kinematics..."
-./average_kinematics.expect ${Q2} ${LOEPS} ${LOEPS}
+./average_kinematics.expect ${Q2} ${LOEPS} ${HIEPS}
 
 echo
 echo "Compiling calc_xsect.f..."
