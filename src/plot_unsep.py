61c416c7c8582f9190fa648930f86d1993735907
Richard Trotta <trotta@cua.edu>
2023-02-27 17:09:16 -0500

    testing plotting script for unsep

--- src/plot_unsep.py
+++ src/plot_unsep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-27 17:08:33 trottar"
+# Time-stamp: "2023-02-27 17:09:14 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -205,19 +205,17 @@ C_Q2_tbin.SetGrid()
 
 l_Q2_tbin = ROOT.TLegend(0.8,0.8,0.95,0.95)
 
-
-G_Q2_tbin.SetTitle(" ; #theta_{cm}; Q^{2}")
-
 for i in range(len(file_df_dict['avek_file']['Q2'].tolist())):
     G_Q2_tbin = ROOT.TGraphErrors()
     print(i+1, np.array(file_df_dict['avek_file']['Q2'].tolist())[i])
     G_Q2_tbin.SetPoint(i, np.array(file_df_dict['avek_file']['th_pos'].tolist())[i], np.array(file_df_dict['avek_file']['Q2'].tolist())[i])
     G_Q2_tbin.SetPointError(i, 0, np.array(file_df_dict['avek_file']['dQ2'].tolist())[i])
     l_Q2_tbin.AddEntry(G_Q2_tbin, "t = {:.4f}".format(np.array(file_df_dict['avek_file']['t'].tolist())[i]))
+    G_Q2_tbin.SetMarkerStyle(21)
+    G_Q2_tbin.SetMarkerSize(1)
     G_Q2_tbin.SetMarkerColor(i+1)
 
-G_Q2_tbin.SetMarkerStyle(21)
-G_Q2_tbin.SetMarkerSize(1)
+G_Q2_tbin.SetTitle(" ; #theta_{cm}; Q^{2}")
     
 G_Q2_tbin.Draw('AP')
 l_Q2_tbin.Draw()
