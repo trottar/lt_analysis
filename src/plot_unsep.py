f145fa66ddfe98e279c48b30ec389628e435b9ca
Richard Trotta <trotta@cua.edu>
2023-02-27 16:35:59 -0500

    testing plotting script for unsep

--- src/plot_unsep.py
+++ src/plot_unsep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-02-27 16:32:15 trottar"
+# Time-stamp: "2023-02-27 16:34:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -209,16 +209,14 @@ for i in range(len(file_df_dict['avek_file']['Q2'].tolist())):
     print(i+1, np.array(file_df_dict['avek_file']['Q2'].tolist())[i])
     G_Q2_tbin.SetPoint(i, np.array(file_df_dict['avek_file']['th_pos'].tolist())[i], np.array(file_df_dict['avek_file']['Q2'].tolist())[i])
     G_Q2_tbin.SetPointError(i, np.array(len(file_df_dict['avek_file']['th_pos'].tolist())*[0])[i], np.array(file_df_dict['avek_file']['dQ2'].tolist())[i])
-    G_Q2_tbin.SetMarkerStyle(21)
-    G_Q2_tbin.SetMarkerSize(1)
-    G_Q2_tbin.SetMarkerColor(i+1)
+    G_Q2_tbin.SetLineColor(i+1)
 
 G_Q2_tbin.Draw('AP')
 
 G_Q2_tbin.SetTitle(" ; #theta; Q^{2}")
 
 for i,t in enumerate(file_df_dict['avek_file']['t'].tolist()):
-    l_Q2_tbin.AddEntry(G_Q2_tbin, "t = {}".format(t))
+    l_Q2_tbin.AddEntry(G_Q2_tbin, "t = {:.4f}".format(t))
 l_Q2_tbin.Draw()
 
 C_Q2_tbin.Print(outputpdf)
