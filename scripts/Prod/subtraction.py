72a15d003dab5e851d5448f6fa80492c11c9a505
Richard Trotta <trotta@cua.edu>
2023-04-24 15:10:29 -0400

    Testing pi/p subtraction changes

--- scripts/Prod/subtraction.py
+++ scripts/Prod/subtraction.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-04-24 15:05:48 trottar"
+# Time-stamp: "2023-04-24 15:10:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -2343,6 +2343,8 @@ def defineHists(phi_setting, inpDict):
         l_mm.Draw()
         cmm.Print(outputpdf.replace("kaon_","{}_kaon_MM_subtract_".format(phi_setting)))
 
+        print("\n\n\n\n\n\n¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬",H_tbins_DATA)
+        
         histDict = {
             "phi_setting" : phi_setting,
             "pid_text" : pid_text,
