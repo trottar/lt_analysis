c56d05c9a4f14a502b9808ee5009f0f2cdfa6f24
Richard Trotta <trotta@cua.edu>
2023-04-24 14:36:27 -0400

    Testing pi/p subtraction changes

--- scripts/Prod/subtraction.py
+++ scripts/Prod/subtraction.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-04-24 14:29:35 trottar"
+# Time-stamp: "2023-04-24 14:36:23 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1765,11 +1765,13 @@ def defineHists(phi_setting, inpDict):
             normfac_subpion_dummy = 1/(300000)
             normfac_subpion_data = 1/(300000)
         if phi_setting == "Left":
+            # 5p5, low
             normfac_subpion_dummy = 1/(300000)
             normfac_subpion_data = 1/(300000)
         if phi_setting == "Center":
-            normfac_subpion_dummy = 1/(300000)
-            normfac_subpion_data = 1/(300000)
+            # 5p5, low
+            normfac_subpion_dummy = 1/(500000)
+            normfac_subpion_data = 1/(500000)
 
         H_ssxfp_SUBPION_DUMMY.Scale(normfac_subpion_dummy)
         H_ssyfp_SUBPION_DUMMY.Scale(normfac_subpion_dummy)
@@ -1886,14 +1888,16 @@ def defineHists(phi_setting, inpDict):
         #H_ct_epi_SUBPION_DUMMY_RAND.Scale(normfac_subpion_dummy/nWindows)
 
         if phi_setting == "Right":
-            normfac_subproton_dummy = 1/(15000)
-            normfac_subproton_data = 1/(15000)
+            normfac_subproton_dummy = 1/(12000)
+            normfac_subproton_data = 1/(12000)
         if phi_setting == "Left":
-            normfac_subproton_dummy = 1/(15000)
-            normfac_subproton_data = 1/(15000)
+            # 5p5, low
+            normfac_subproton_dummy = 1/(12000)
+            normfac_subproton_data = 1/(12000)
         if phi_setting == "Center":
-            normfac_subproton_dummy = 1/(15000)
-            normfac_subproton_data = 1/(15000)
+            # 5p5, low
+            normfac_subproton_dummy = 1/(12000)
+            normfac_subproton_data = 1/(12000)
 
         H_ssxfp_SUBPROTON_DUMMY.Scale(normfac_subproton_dummy)
         H_ssyfp_SUBPROTON_DUMMY.Scale(normfac_subproton_dummy)
