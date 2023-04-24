0291911621cb2b5311d49a2f4270a6e0d1a2c126
Richard Trotta <trotta@cua.edu>
2023-04-24 16:35:58 -0400

    Testing pi/p subtraction changes

--- scripts/Prod/subtraction.py
+++ scripts/Prod/subtraction.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-04-24 16:30:44 trottar"
+# Time-stamp: "2023-04-24 16:35:47 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -2421,9 +2421,7 @@ def defineHists(phi_setting, inpDict):
         histDict.update(tbinDict)
 
         for key,val in histDict.items():
-            print("#####", key, " ", val)
-            if val == None:
-                print("!!!!!!!!!!!!!!!!!!!!!!", val)
+            print("#####", key, " ", type(val))
 
         ###
         # Plot MM for each particle type
