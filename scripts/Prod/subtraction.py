c1b3c8a586bfdd5056cb61755727c119c646409a
Richard Trotta <trotta@cua.edu>
2023-04-27 10:35:58 -0400

    Added CT plot to mm sub

--- scripts/Prod/subtraction.py
+++ scripts/Prod/subtraction.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-04-27 10:32:47 trottar"
+# Time-stamp: "2023-04-27 10:35:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -2505,7 +2505,7 @@ def defineHists(phi_setting, inpDict):
 
         c_pid.Draw()
 
-        c_pid.Print(outputpdf.replace("kaon_","{}_kaon_MM_subtract_".format(phi_setting))
+        c_pid.Print(outputpdf.replace("kaon_","{}_kaon_MM_subtract_".format(phi_setting)))
 
 
         ###
