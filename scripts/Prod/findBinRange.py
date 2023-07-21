18ff64b5443d8e925c9fe1f2e3cdf3d0ffa9a585
Richard Trotta <trotta@cua.edu>
2023-07-21 19:03:31 -0400

    Testing binning fixes

--- scripts/Prod/findBinRange.py
+++ scripts/Prod/findBinRange.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-07-21 18:54:42 trottar"
+# Time-stamp: "2023-07-21 19:03:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -387,7 +387,7 @@ for hist in histlist:
 # Initialize NumPy arrays
 aver_lst = []
 mm_list = []
-'''
+#'''
 # Loop through tbinedges
 print("~~~~~~~~~~~~~~~~~~~",t, phi_deg, Q2, W,"~~~~~~~~~~~~~~~~~~~")
 for i, evt in enumerate(t):
@@ -423,7 +423,7 @@ for j in range(len(tbinedges) - 1):
                 print("-------------------",t_val, phi_deg[k], Q2_val, W_val,"-------------------")
                 mm_list.append((tbin_index, phibin_index, np.sqrt(pow(emiss[tbin_index], 2) - pow(pmiss[tbin_index], 2))))
                 
-
+'''
 groups = {}
 # Group the tuples by the first two elements using a dictionary
 for t in aver_lst:
