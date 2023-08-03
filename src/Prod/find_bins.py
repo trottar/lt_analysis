7a1a59ae66ff24de2b276d52af71310fee59397e
Richard Trotta <trotta@cua.edu>
2023-08-03 13:14:53 -0400

    Working diamond and rand_sub

--- src/Prod/find_bins.py
+++ src/Prod/find_bins.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-03 13:10:47 trottar"
+# Time-stamp: "2023-08-03 13:14:50 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -128,58 +128,57 @@ def find_bins(histlist, inpDict):
 
     # Concatenate the H_phi arrays for Right, Left, and Center
     H_phi_BinTest = np.concatenate((H_phi_Right, H_phi_Left, H_phi_Center))
-        
+
+    def find_phibins(H_phi_BinTest):
+
+        print("\nFinding phi bins...")
+        phi_arr = np.linspace(0.0, 360.0, inpDict["NumPhiBins"]+1)
+
+        n, bins, patches = plt.hist(H_phi_BinTest, phi_arr)
+
+        return [n,bins]
+
+    def find_tbins(H_t_BinTest):
+
+
+        ################################################################################################################################################
+
+        def histedges_equalN(x, nbin):
+            # Grab number of events in array
+            npt = len(x)
+            # One-dimensional linear interpolation for monotonically increasing sample points.
+            # Returns the one-dimensional piecewise linear interpolant to a function with given
+            # discrete data points (xp, fp), evaluated at x.
+            #
+            # np.interp(x, xp, fp)
+            # x -> np.linspace(0, npt, nbin + 1) : The x-coordinates at which to evaluate the interpolated values
+            # In this case, this is an array of evenly spaced t-bins
+            # xp -> np.arange(npt) : The x-coordinates of the data points
+            # In this case, this returns evenly spaced values within a given interval
+            # yp -> np.sort(x) : he y-coordinates of the data points
+            # In this case, this returns a sorted copy of the array
+            return np.interp(np.linspace(0, npt, nbin + 1),np.arange(npt),np.sort(x))
+
+        print("\nFinding t bins...")
+        # Histogram takes the array data set and the bins as input
+        # The bins are determined by a linear interpolation (see function above)
+        # This returns the binned data with equal number of events per bin
+        # Returns...
+        # n -> The values of the histogram bins
+        # bins -> The edges of the bins
+        # patches -> Container of individual artists used to create the histogram or list of
+        # such containers if there are multiple input datasets.
+        n, bins, patches = plt.hist(H_t_BinTest, histedges_equalN(H_t_BinTest, inpDict["NumtBins"]))
+
+        # Write t_bin_interval for lt_analysis scripts
+        lines = []
+        #with open("{}/src/t_bin_interval_{}_{:.0f}".format(LTANAPATH,Q2.replace("p",""),float(EPSVAL)*100), "w") as file:
+        with open("{}/src/t_bin_interval".format(LTANAPATH), "w") as file:
+            file.write("{}\t{}\t{}\n".format(Q2.replace("p","."),inpDict["NumtBins"],inpDict["NumPhiBins"]))
+            for i,t in enumerate(bins):
+                lines.append("\t{:.2f}".format(float(t)))
+            file.writelines(lines)
+
+        return [n,bins]
+    
     return [find_phibins(H_phi_BinTest), find_tbins(H_t_BinTest)]
-
-def find_phibins(H_phi_BinTest):
-
-    print("\nFinding phi bins...")
-    phi_arr = np.linspace(0.0, 360.0, inpDict["NumPhiBins"]+1)
-
-    n, bins, patches = plt.hist(H_phi_BinTest, phi_arr)
-
-    return [n,bins]
-
-def find_tbins(H_t_BinTest):
-
-                
-    ################################################################################################################################################
-
-    def histedges_equalN(x, nbin):
-        # Grab number of events in array
-        npt = len(x)
-        # One-dimensional linear interpolation for monotonically increasing sample points.
-        # Returns the one-dimensional piecewise linear interpolant to a function with given
-        # discrete data points (xp, fp), evaluated at x.
-        #
-        # np.interp(x, xp, fp)
-        # x -> np.linspace(0, npt, nbin + 1) : The x-coordinates at which to evaluate the interpolated values
-        # In this case, this is an array of evenly spaced t-bins
-        # xp -> np.arange(npt) : The x-coordinates of the data points
-        # In this case, this returns evenly spaced values within a given interval
-        # yp -> np.sort(x) : he y-coordinates of the data points
-        # In this case, this returns a sorted copy of the array
-        return np.interp(np.linspace(0, npt, nbin + 1),np.arange(npt),np.sort(x))
-
-    print("\nFinding t bins...")
-    # Histogram takes the array data set and the bins as input
-    # The bins are determined by a linear interpolation (see function above)
-    # This returns the binned data with equal number of events per bin
-    # Returns...
-    # n -> The values of the histogram bins
-    # bins -> The edges of the bins
-    # patches -> Container of individual artists used to create the histogram or list of
-    # such containers if there are multiple input datasets.
-    n, bins, patches = plt.hist(H_t_BinTest, histedges_equalN(H_t_BinTest, inpDict["NumtBins"]))
-    
-    # Write t_bin_interval for lt_analysis scripts
-    lines = []
-    #with open("{}/src/t_bin_interval_{}_{:.0f}".format(LTANAPATH,Q2.replace("p",""),float(EPSVAL)*100), "w") as file:
-    with open("{}/src/t_bin_interval".format(LTANAPATH), "w") as file:
-        file.write("{}\t{}\t{}\n".format(Q2.replace("p","."),inpDict["NumtBins"],inpDict["NumPhiBins"]))
-        for i,t in enumerate(bins):
-            lines.append("\t{:.2f}".format(float(t)))
-        file.writelines(lines)
-    
-    return [n,bins]
-
