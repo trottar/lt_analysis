a280185c5f03515220076fd7ae4f3d7b6e3d73df
Richard Trotta <trotta@cua.edu>
2023-08-03 14:37:17 -0400

    Running high and low eps at once

--- src/Prod/main.py
+++ src/Prod/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-03 14:33:57 trottar"
+# Time-stamp: "2023-08-03 14:37:09 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -228,7 +228,8 @@ try:
         # Check if the file has at least two lines
         if len(all_lines) >= 2:
             # Extract the second line and remove leading/trailing whitespace
-            t_bins = all_lines[1].split("\t").pop(0)
+            t_bins = all_lines[1].split("\t")
+            del t_bins[0]
 except FileNotFoundError:
     print("{} not found...".format("{}/src/t_bin_interval".format(LTANAPATH)))
 except IOError:
@@ -241,7 +242,8 @@ try:
         # Check if the file has at least two lines
         if len(all_lines) >= 2:
             # Extract the second line and remove leading/trailing whitespace
-            phi_bins = all_lines[1].split("\t").pop(0)
+            phi_bins = all_lines[1].split("\t")
+            del phi_bins[0]
 except FileNotFoundError:
     print("{} not found...".format("{}/src/phi_bin_interval".format(LTANAPATH)))
 except IOError:
