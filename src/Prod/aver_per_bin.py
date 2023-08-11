18a895b31c454cbb833d9abdb0719d86ec258e94
Richard Trotta <trotta@cua.edu>
2023-08-11 17:51:23 -0400

    Changing bin for kin to average

--- src/Prod/aver_per_bin.py
+++ src/Prod/aver_per_bin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-11 16:19:31 trottar"
+# Time-stamp: "2023-08-11 17:51:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -63,12 +63,19 @@ def calculate_aver_data(hist_data, hist_dummy, t_data, t_bins):
     binned_hist_dummy = []
 
     # Loop through bins in t_data and identify events in specified bins
-    for bin_index in range(1, t_data.GetNbinsX() + 1):
-        bin_center = t_data.GetBinCenter(bin_index)
-        if t_bins[0] <= bin_center <= t_bins[-1]:
-            binned_t_data.append(bin_center)
-            binned_hist_data.append(hist_data.GetBinContent(bin_index))
-            binned_hist_dummy.append(hist_dummy.GetBinContent(bin_index))
+    for j in range(len(t_bins) - 1):
+        for bin_index in range(1, t_data.GetNbinsX() + 1):
+            tmp_t_data = []
+            tmp_hist_data = []
+            tmp_hist_dummy = []
+            bin_center = t_data.GetBinCenter(bin_index)
+            if t_bins[j] <= bin_center <= t_bins[j+1]:
+                tmp_t_data.append(bin_center)
+                tmp_hist_data.append(hist_data.GetBinContent(bin_index))
+                tmp_hist_dummy.append(hist_dummy.GetBinContent(bin_index))
+        binned_t_data.append(tmp_t_data)
+        binned_hist_data.append(tmp_hist_data)
+        binned_hist_dummy.append(tmp_hist_dummy)
 
     # Convert the lists to numpy arrays for subtraction
     binned_t_data = np.array(binned_t_data)
