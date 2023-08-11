da6938467266ee86a7297836773c6c596e657bf4
Richard Trotta <trotta@cua.edu>
2023-08-11 11:15:04 -0400

    Changing bin for kin to average

--- src/Prod/utility.py
+++ src/Prod/utility.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-10 22:42:42 trottar"
+# Time-stamp: "2023-08-11 11:10:12 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -110,55 +110,6 @@ def calculate_aver_data2(hist_data, hist_dummy, t_bins):
         average_hist_data.Fill(average_value)
 
     return convert_TH1F_to_numpy(average_hist_data)  # Return the processed histogram as a numpy array
-
-def calculate_aver_data(hist_data, hist_dummy, t_data, t_dummy, t_bins):
-
-    # Ensure that the input histograms are properly initialized
-    if not hist_data or not hist_dummy or not t_data or not t_dummy:
-        print("Error: Input histograms are not properly initialized.")
-        return []
-
-    # Bin t_data and t_dummy in t_bins
-    binned_t_data = t_data.Rebin(len(t_bins)-1, "binned_t_data", array('d', t_bins))
-    binned_t_dummy = t_dummy.Rebin(len(t_bins)-1, "binned_t_dummy", array('d', t_bins))
-
-    print("Binned t data contents:", [binned_t_data.GetBinContent(i) for i in range(1, binned_t_data.GetNbinsX()+1)])
-    print("Binned t dummy contents:", [binned_t_dummy.GetBinContent(i) for i in range(1, binned_t_dummy.GetNbinsX()+1)])
-    
-    # Get the bin numbers of t_data and t_dummy
-    bin_numbers_t_data = []
-    bin_numbers_t_dummy = []
-    for bin_idx in range(1, len(t_bins)):
-        bin_center = binned_t_data.GetBinCenter(bin_idx)
-        bin_number = hist_data.FindBin(bin_center)
-        bin_numbers_t_data.append(bin_number)
-
-        bin_center_dummy = binned_t_dummy.GetBinCenter(bin_idx)
-        bin_number_dummy = hist_dummy.FindBin(bin_center_dummy)
-        bin_numbers_t_dummy.append(bin_number_dummy)
-
-    # Bin hist_data and hist_dummy using the calculated bin numbers
-    binned_hist_data = hist_data.Rebin(len(bin_numbers_t_data) - 1, "binned_hist_data", array('i', bin_numbers_t_data))
-    binned_hist_dummy = hist_dummy.Rebin(len(bin_numbers_t_dummy) - 1, "binned_hist_dummy", array('i', bin_numbers_t_dummy))
-
-    # Debugging step: Print histogram contents and properties
-    print("Binned hist data contents:", [binned_hist_data.GetBinContent(i) for i in range(1, binned_hist_data.GetNbinsX()+1)])
-    print("Binned hist dummy contents:", [binned_hist_dummy.GetBinContent(i) for i in range(1, binned_hist_dummy.GetNbinsX()+1)])
-    
-    # Create subtracted_hist by cloning binned_hist_data
-    subtracted_hist = binned_hist_data.Clone("subtracted_hist")
-    subtracted_hist.Add(binned_hist_dummy, -1)
-    
-    # Calculate the average per bin of the subtracted bins
-    num_bins = subtracted_hist.GetNbinsX()
-    averaged_values = []
-    for bin_idx in range(1, num_bins+1):
-        bin_content = subtracted_hist.GetBinContent(bin_idx)
-        bin_width = subtracted_hist.GetBinWidth(bin_idx)
-        average_value = bin_content / bin_width
-        averaged_values.append(average_value)
-    
-    return averaged_values
 
 ################################################################################################################################################
 
