b34bf465cfbbaf32d14bf57f1449f07277552f37
Richard Trotta <trotta@cua.edu>
2023-08-11 20:24:20 -0400

    Changing bin for kin to average

--- src/Prod/aver_per_bin.py
+++ src/Prod/aver_per_bin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-11 20:07:43 trottar"
+# Time-stamp: "2023-08-11 20:24:17 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -72,10 +72,11 @@ def calculate_aver_data(hist_data, hist_dummy, t_data, t_bins):
             bin_center = t_data.GetBinCenter(bin_index)
             if t_bins[j] <= bin_center <= t_bins[j+1]:
                 print("Checking if {} <= {} <= {}".format(t_bins[j], bin_center, t_bins[j+1]))
-                print("Bin {}, Hist bin {} Passed with content {}".format(j, hist_data.GetBinCenter(bin_index), hist_data.GetBinContent(bin_index)))
-                tmp_t_data.append(bin_center)
-                tmp_hist_data.append(hist_data.GetBinContent(bin_index))
-                tmp_hist_dummy.append(hist_dummy.GetBinContent(bin_index))
+                if hist_data.GetBinContent(bin_index) > 0:
+                    print("Bin {}, Hist bin {} Passed with content {}".format(j, hist_data.GetBinCenter(bin_index), hist_data.GetBinContent(bin_index)))
+                    tmp_t_data.append(bin_center)
+                    tmp_hist_data.append(hist_data.GetBinContent(bin_index))
+                    tmp_hist_dummy.append(hist_dummy.GetBinContent(bin_index))
         binned_t_data.append(tmp_t_data)
         binned_hist_data.append(tmp_hist_data)
         binned_hist_dummy.append(tmp_hist_dummy)
