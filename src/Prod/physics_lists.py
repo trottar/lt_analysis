1f5510dadec7c4d57d7931fd0ee7cd5433d8d48d
Richard Trotta <trotta@cua.edu>
2023-08-21 21:05:30 -0400

    Editing physics list for rest of lt_analysis scripts

--- src/Prod/physics_lists.py
+++ src/Prod/physics_lists.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-08-21 20:29:32 trottar"
+# Time-stamp: "2023-08-21 21:05:27 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -64,206 +64,208 @@ OUTPATH=lt.OUTPATH
 
 ###############################################################################################################################################
 
-for k, data_key_tuple in enumerate(averDict["binned_DATA"]):
-    i = data_key_tuple[0] # t bin
-    j = data_key_tuple[1] # phi bin
-    tbin = averDict["t_bins"][i]
-    phibin = averDict["phi_bins"][j]
-    # Access the nested dictionary using the tuple key
-    data_nested_dict = averDict["binned_DATA"][data_key_tuple]
-    for phiset in data_nested_dict["phi_setting"]:
-        if phiset == "Right":
-            phibin_right_data = phibin
-            tbin_right_data = tbin
-            averQ2_right_data = phiset["Q2_arr"]
-            averW_right_data = phiset["W_arr"]
-            avert_right_data = phiset["t_arr"] 
-        if phiset == "Left":
-            phibin_left_data = phibin
-            tbin_left_data = tbin
-            averQ2_left_data = phiset["Q2_arr"]
-            averW_left_data = phiset["W_arr"]
-            avert_left_data = phiset["t_arr"]
-        if phiset == "Center":
-            phibin_center_data = phibin
-            tbin_center_data = tbin
-            averQ2_center_data = phiset["Q2_arr"]
-            averW_center_data = phiset["W_arr"]
-            avert_center_data = phiset["t_arr"]
-           
-################################################################################################################################################
+def create_lists(averDict, inpDict):
 
-def write_to_file(f_out,line,write_mode='a'):
-    # Open a file in append mode
-    with open(f_out, write_mode) as f:
-        # Write the value of the variable to the file
-        f.write(line)
+    for k, data_key_tuple in enumerate(averDict["binned_DATA"]):
+        i = data_key_tuple[0] # t bin
+        j = data_key_tuple[1] # phi bin
+        tbin = averDict["t_bins"][i]
+        phibin = averDict["phi_bins"][j]
+        # Access the nested dictionary using the tuple key
+        data_nested_dict = averDict["binned_DATA"][data_key_tuple]
+        for phiset in data_nested_dict["phi_setting"]:
+            if phiset == "Right":
+                phibin_right_data = phibin
+                tbin_right_data = tbin
+                averQ2_right_data = phiset["Q2_arr"]
+                averW_right_data = phiset["W_arr"]
+                avert_right_data = phiset["t_arr"] 
+            if phiset == "Left":
+                phibin_left_data = phibin
+                tbin_left_data = tbin
+                averQ2_left_data = phiset["Q2_arr"]
+                averW_left_data = phiset["W_arr"]
+                avert_left_data = phiset["t_arr"]
+            if phiset == "Center":
+                phibin_center_data = phibin
+                tbin_center_data = tbin
+                averQ2_center_data = phiset["Q2_arr"]
+                averW_center_data = phiset["W_arr"]
+                avert_center_data = phiset["t_arr"]
 
-################################################################################################################################################
+    ################################################################################################################################################
 
-# Define thpq vector relative to middle setting
-if float(runNumRight[0]) != 0:
-    runNums= runNumRight
-    for i, run in enumerate(runNumRight):
-        runNum = run
-        pid_log = "%s/log/Analysed_Prod_%s.log" % (LTANAPATH,runNum)
-        if os.path.exists(pid_log):
-            thpq_right = float("{:.3f}".format(abs(float(pThetaValCenter[i])-float(pThetaValRight[i]))))
-            #thpq_right = 3.000
-            ebeam_right = float(EbeamValRight[i])
-            break
-        else:
-            continue
-        
-if float(runNumLeft[0]) != 0:
-    runNums= runNumLeft
-    for i, run in enumerate(runNumLeft):
-        runNum = run
-        pid_log = "%s/log/Analysed_Prod_%s.log" % (LTANAPATH,runNum)
-        if os.path.exists(pid_log):
-            thpq_left = float("{:.3f}".format(abs(float(pThetaValCenter[i])-float(pThetaValLeft[i]))))
-            #thpq_left = 3.000
-            ebeam_left = float(EbeamValLeft[i])
-            break
-        else:
-            continue
+    def write_to_file(f_out,line,write_mode='a'):
+        # Open a file in append mode
+        with open(f_out, write_mode) as f:
+            # Write the value of the variable to the file
+            f.write(line)
 
-if float(runNumCenter[0]) != 0:
-    runNums= runNumCenter
-    for i, run in enumerate(runNumCenter):
-        runNum = run
-        pid_log = "%s/log/Analysed_Prod_%s.log" % (LTANAPATH,runNum)
-        if os.path.exists(pid_log):
-            thpq_center = 0.000
-            ebeam_center = float(EbeamValCenter[i])
-            break
-        else:
-            continue
-        
-print(thpq_left)
-    
-################################################################################################################################################
-            
-f_list_settings = '{}/src/beam/Eb_KLT.dat'.format(LTANAPATH)
-if not os.path.exists(f_list_settings):
-    open(f_list_settings, "w").close()
+    ################################################################################################################################################
 
-if float(runNumRight[0]) != 0:
-    # First check if line exists
-    with open(f_list_settings, 'r') as f:
-        lines = f.readlines()    
-        check_line = "{:.3f} {} {:.4f}\n".format(ebeam_right, Q2, EPSVAL)
-        # Check if the line already exists
-        if check_line not in lines:
-            write_to_file(f_list_settings,check_line)
-            
-if float(runNumLeft[0]) != 0:
-    # First check if line exists
-    with open(f_list_settings, 'r') as f:
-        lines = f.readlines()    
-        check_line = "{:.3f} {} {:.4f}\n".format(ebeam_left, Q2, EPSVAL)
-        # Check if the line already exists
-        if check_line not in lines:
-            write_to_file(f_list_settings,check_line)
-            
-if float(runNumCenter[0]) != 0:
-    # First check if line exists
-    with open(f_list_settings, 'r') as f:
-        lines = f.readlines()        
-        check_line = "{:.3f} {} {:.4f}\n".format(ebeam_center, Q2, EPSVAL)
-        # Check if the line already exists
-        if check_line not in lines:
-            write_to_file(f_list_settings,check_line)
+    # Define thpq vector relative to middle setting
+    if float(runNumRight[0]) != 0:
+        runNums= runNumRight
+        for i, run in enumerate(runNumRight):
+            runNum = run
+            pid_log = "%s/log/Analysed_Prod_%s.log" % (LTANAPATH,runNum)
+            if os.path.exists(pid_log):
+                thpq_right = float("{:.3f}".format(abs(float(pThetaValCenter[i])-float(pThetaValRight[i]))))
+                #thpq_right = 3.000
+                ebeam_right = float(EbeamValRight[i])
+                break
+            else:
+                continue
 
-################################################################################################################################################
-            
-f_list_settings = '{}/src/list.settings'.format(LTANAPATH)
-if not os.path.exists(f_list_settings):
-    open(f_list_settings, "w").close()
+    if float(runNumLeft[0]) != 0:
+        runNums= runNumLeft
+        for i, run in enumerate(runNumLeft):
+            runNum = run
+            pid_log = "%s/log/Analysed_Prod_%s.log" % (LTANAPATH,runNum)
+            if os.path.exists(pid_log):
+                thpq_left = float("{:.3f}".format(abs(float(pThetaValCenter[i])-float(pThetaValLeft[i]))))
+                #thpq_left = 3.000
+                ebeam_left = float(EbeamValLeft[i])
+                break
+            else:
+                continue
 
-if float(runNumRight[0]) != 0:    
-    # First check if line exists
-    with open(f_list_settings, 'r') as f:
-        lines = f.readlines()
-        check_line = "{} {} {:.4f} -{:.3f} {:.3f} {:.3f} {} {}\n".format(POL, Q2, EPSVAL, thpq_right, TMIN, TMAX, NumtBins, Kset)
-        # Check if the line already exists
-        if check_line not in lines:
-            write_to_file(f_list_settings,check_line)
-            
-if float(runNumLeft[0]) != 0:
-    # First check if line exists
-    with open(f_list_settings, 'r') as f:
-        lines = f.readlines()        
-        check_line = "{} {} {:.4f} +{:.3f} {:.3f} {:.3f} {} {}\n".format(POL, Q2, EPSVAL, thpq_left, TMIN, TMAX, NumtBins, Kset)
-        if check_line not in lines:
-            write_to_file(f_list_settings,check_line)
-            
-if float(runNumCenter[0]) != 0:
-    # First check if line exists
-    with open(f_list_settings, 'r') as f:
-        lines = f.readlines()        
-        check_line = "{} {} {:.4f} +{:.3f} {:.3f} {:.3f} {} {}\n".format(POL, Q2, EPSVAL, thpq_center, TMIN, TMAX, NumtBins, Kset)
-        if check_line not in lines:
-            write_to_file(f_list_settings,check_line)            
-                            
-################################################################################################################################################
+    if float(runNumCenter[0]) != 0:
+        runNums= runNumCenter
+        for i, run in enumerate(runNumCenter):
+            runNum = run
+            pid_log = "%s/log/Analysed_Prod_%s.log" % (LTANAPATH,runNum)
+            if os.path.exists(pid_log):
+                thpq_center = 0.000
+                ebeam_center = float(EbeamValCenter[i])
+                break
+            else:
+                continue
 
-if float(runNumRight[0]) != 0:
-    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_-{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_right*1000))
+    print(thpq_left)
 
-    if not os.path.exists(f_list):
-        open(f_list, "w").close()    
-    # Open a file in read mode
-    with open(f_list, 'r') as f:
-        lines = f.readlines()[1:-1]
-        tbin_right_data.append(0.0)
-        for i, Q2val in enumerate(averQ2_right_data):
-            check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_right_data[i], 1.0, averW_right_data[i], 1.0, avert_right_data[i], 1.0)
-            write_to_file(f_list,check_line)
-        lines = f.readlines()
-        first_line = "{:.6f}\n".format(1.0)
-        last_line = "{:.6f}".format(1.0)
-        lines.insert(0,first_line)
-        lines.append(last_line)
-        write_to_file(f_list,"".join(lines),write_mode='w')
+    ################################################################################################################################################
 
-if float(runNumLeft[0]) != 0:
-    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_+{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_left*1000))
+    f_list_settings = '{}/src/beam/Eb_KLT.dat'.format(LTANAPATH)
+    if not os.path.exists(f_list_settings):
+        open(f_list_settings, "w").close()
 
-    if not os.path.exists(f_list):
-        open(f_list, "w").close()    
-    # Open a file in read mode
-    with open(f_list, 'r') as f:
-        lines = f.readlines()[1:-1]
-        tbin_left_data.append(0.0)
-        for i, Q2val in enumerate(averQ2_left_data):
-            check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_left_data[i], 1.0, averW_left_data[i], 1.0, avert_left_data[i], 1.0)
-            write_to_file(f_list,check_line)
-        lines = f.readlines()
-        first_line = "{:.6f}\n".format(1.0)
-        last_line = "{:.6f}".format(1.0)
-        lines.insert(0,first_line)
-        lines.append(last_line)
-        write_to_file(f_list,"".join(lines),write_mode='w')
+    if float(runNumRight[0]) != 0:
+        # First check if line exists
+        with open(f_list_settings, 'r') as f:
+            lines = f.readlines()    
+            check_line = "{:.3f} {} {:.4f}\n".format(ebeam_right, Q2, EPSVAL)
+            # Check if the line already exists
+            if check_line not in lines:
+                write_to_file(f_list_settings,check_line)
 
-if float(runNumCenter[0]) != 0:
-    f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_+0000.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100)
+    if float(runNumLeft[0]) != 0:
+        # First check if line exists
+        with open(f_list_settings, 'r') as f:
+            lines = f.readlines()    
+            check_line = "{:.3f} {} {:.4f}\n".format(ebeam_left, Q2, EPSVAL)
+            # Check if the line already exists
+            if check_line not in lines:
+                write_to_file(f_list_settings,check_line)
 
-    if not os.path.exists(f_list):
-        open(f_list, "w").close()    
-    # Open a file in read mode
-    with open(f_list, 'r') as f:
-        lines = f.readlines()[1:-1]
-        tbin_center_data.append(0.0)
-        for i, Q2val in enumerate(averQ2_center_data):
-            check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_center_data[i], 1.0, averW_center_data[i], 1.0, avert_center_data[i], 1.0)
-            write_to_file(f_list,check_line)
-        lines = f.readlines()
-        first_line = "{:.6f}\n".format(1.0)
-        last_line = "{:.6f}".format(1.0)
-        lines.insert(0,first_line)
-        lines.append(last_line)
-        write_to_file(f_list,"".join(lines),write_mode='w')
+    if float(runNumCenter[0]) != 0:
+        # First check if line exists
+        with open(f_list_settings, 'r') as f:
+            lines = f.readlines()        
+            check_line = "{:.3f} {} {:.4f}\n".format(ebeam_center, Q2, EPSVAL)
+            # Check if the line already exists
+            if check_line not in lines:
+                write_to_file(f_list_settings,check_line)
 
-################################################################################################################################################
+    ################################################################################################################################################
+
+    f_list_settings = '{}/src/list.settings'.format(LTANAPATH)
+    if not os.path.exists(f_list_settings):
+        open(f_list_settings, "w").close()
+
+    if float(runNumRight[0]) != 0:    
+        # First check if line exists
+        with open(f_list_settings, 'r') as f:
+            lines = f.readlines()
+            check_line = "{} {} {:.4f} -{:.3f} {:.3f} {:.3f} {} {}\n".format(POL, Q2, EPSVAL, thpq_right, TMIN, TMAX, NumtBins, Kset)
+            # Check if the line already exists
+            if check_line not in lines:
+                write_to_file(f_list_settings,check_line)
+
+    if float(runNumLeft[0]) != 0:
+        # First check if line exists
+        with open(f_list_settings, 'r') as f:
+            lines = f.readlines()        
+            check_line = "{} {} {:.4f} +{:.3f} {:.3f} {:.3f} {} {}\n".format(POL, Q2, EPSVAL, thpq_left, TMIN, TMAX, NumtBins, Kset)
+            if check_line not in lines:
+                write_to_file(f_list_settings,check_line)
+
+    if float(runNumCenter[0]) != 0:
+        # First check if line exists
+        with open(f_list_settings, 'r') as f:
+            lines = f.readlines()        
+            check_line = "{} {} {:.4f} +{:.3f} {:.3f} {:.3f} {} {}\n".format(POL, Q2, EPSVAL, thpq_center, TMIN, TMAX, NumtBins, Kset)
+            if check_line not in lines:
+                write_to_file(f_list_settings,check_line)            
+
+    ################################################################################################################################################
+
+    if float(runNumRight[0]) != 0:
+        f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_-{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_right*1000))
+
+        if not os.path.exists(f_list):
+            open(f_list, "w").close()    
+        # Open a file in read mode
+        with open(f_list, 'r') as f:
+            lines = f.readlines()[1:-1]
+            tbin_right_data.append(0.0)
+            for i, Q2val in enumerate(averQ2_right_data):
+                check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_right_data[i], 1.0, averW_right_data[i], 1.0, avert_right_data[i], 1.0)
+                write_to_file(f_list,check_line)
+            lines = f.readlines()
+            first_line = "{:.6f}\n".format(1.0)
+            last_line = "{:.6f}".format(1.0)
+            lines.insert(0,first_line)
+            lines.append(last_line)
+            write_to_file(f_list,"".join(lines),write_mode='w')
+
+    if float(runNumLeft[0]) != 0:
+        f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_+{}.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100, int(thpq_left*1000))
+
+        if not os.path.exists(f_list):
+            open(f_list, "w").close()    
+        # Open a file in read mode
+        with open(f_list, 'r') as f:
+            lines = f.readlines()[1:-1]
+            tbin_left_data.append(0.0)
+            for i, Q2val in enumerate(averQ2_left_data):
+                check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_left_data[i], 1.0, averW_left_data[i], 1.0, avert_left_data[i], 1.0)
+                write_to_file(f_list,check_line)
+            lines = f.readlines()
+            first_line = "{:.6f}\n".format(1.0)
+            last_line = "{:.6f}".format(1.0)
+            lines.insert(0,first_line)
+            lines.append(last_line)
+            write_to_file(f_list,"".join(lines),write_mode='w')
+
+    if float(runNumCenter[0]) != 0:
+        f_list = '{}/src/kindata/kindata.{}_{}_{:.0f}_+0000.dat'.format(LTANAPATH, PID, Q2.replace(".",""), float(EPSVAL)*100)
+
+        if not os.path.exists(f_list):
+            open(f_list, "w").close()    
+        # Open a file in read mode
+        with open(f_list, 'r') as f:
+            lines = f.readlines()[1:-1]
+            tbin_center_data.append(0.0)
+            for i, Q2val in enumerate(averQ2_center_data):
+                check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_center_data[i], 1.0, averW_center_data[i], 1.0, avert_center_data[i], 1.0)
+                write_to_file(f_list,check_line)
+            lines = f.readlines()
+            first_line = "{:.6f}\n".format(1.0)
+            last_line = "{:.6f}".format(1.0)
+            lines.insert(0,first_line)
+            lines.append(last_line)
+            write_to_file(f_list,"".join(lines),write_mode='w')
+
+    ################################################################################################################################################
 
