884a3121cdc6c0c7a5341fabbf42a117f5e9c9b0
Richard Trotta <trotta@cua.edu>
2023-09-06 19:54:32 -0400

    Testing ratio calculations

--- src/Prod/main.py
+++ src/Prod/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-06 19:52:10 trottar"
+# Time-stamp: "2023-09-06 19:54:29 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1280,13 +1280,13 @@ for i, val in enumerate(t_bins):
     G_yieldvsphi_data_plt.GetXaxis().SetTitleOffset(1.5)
     G_yieldvsphi_data_plt.GetXaxis().SetLabelSize(0.04)
 
-    C_yieldvsphi_data_plt.Update()
+    #C_yieldvsphi_data_plt.Update()
     
 #l_yieldvsphi_data_plt.AddEntry(G_yieldvsphi_data,"Data")
 #l_yieldvsphi_data_plt.AddEntry(G_yieldvsphi_simc,"Simc")
 #l_yieldvsphi_data_plt.Draw()
 
-C_yieldvsphi_data_plt.Draw()
+#C_yieldvsphi_data_plt.Draw()
 
 C_yieldvsphi_data_plt.Print(outputpdf.replace("{}_".format(ParticleType),"{}_binned_".format(ParticleType)))
 
