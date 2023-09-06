c74078917a733bdbe26f2116ab8d62523a2482a4
Richard Trotta <trotta@cua.edu>
2023-09-06 18:13:39 -0400

    Testing ratio calculations

--- src/Prod/main.py
+++ src/Prod/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-06 18:09:05 trottar"
+# Time-stamp: "2023-09-06 18:12:36 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1255,6 +1255,8 @@ phibins_simc = match_to_bin(phibins_simc)
 for i, val in enumerate(t_bins):
 
     if val == yield_data[i][0]:
+
+        print("---------------------", i, yield_data[i][1], phibins_data[i][1])
         
         G_yieldvsphi_data_plt = ROOT.TMultiGraph()
         G_yieldvsphi_data = ROOT.TGraphErrors(len(yield_data[i][1]),phibins_data[i][1],yield_data[i][1],np.array([0]*len(phibins_data[i][1])),np.array([0]*len(yield_data[i][1])))
