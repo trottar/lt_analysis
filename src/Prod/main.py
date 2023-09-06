99be9069cfe751a139a51c7963f548d0cade5f75
Richard Trotta <trotta@cua.edu>
2023-09-06 19:14:04 -0400

    Testing ratio calculations

--- src/Prod/main.py
+++ src/Prod/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-06 19:10:05 trottar"
+# Time-stamp: "2023-09-06 19:14:02 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1253,25 +1253,23 @@ phibins_simc = match_to_bin(phibins_simc)
    
 for i, val in enumerate(t_bins):
     
-    if val == yield_data[i][0] and val == yield_simc[i][0]:
+    print("---------------------", i, yield_data[i][1], phibins_data[i][1])
+    print("_____________________", i, yield_simc[i][1], phibins_simc[i][1])
+    
+    G_yieldvsphi_data_plt = ROOT.TMultiGraph()
+    
+    G_yieldvsphi_data = ROOT.TGraphErrors(len(yield_data[i][1]),phibins_data[i][1],yield_data[i][1],np.array([0]*len(phibins_data[i][1])),np.array([0]*len(yield_data[i][1])))
+    G_yieldvsphi_simc = ROOT.TGraphErrors(len(yield_simc[i][1]),phibins_simc[i][1],yield_simc[i][1],np.array([0]*len(phibins_simc[i][1])),np.array([0]*len(yield_simc[i][1])))
 
-        print("---------------------", i, yield_data[i][1], phibins_data[i][1])
-        print("_____________________", i, yield_simc[i][1], phibins_simc[i][1])
+    G_yieldvsphi_data.SetMarkerStyle(21)
+    G_yieldvsphi_data.SetMarkerSize(1)
+    G_yieldvsphi_data.SetMarkerColor(1)
+    G_yieldvsphi_data_plt.Add(G_yieldvsphi_data)
 
-        G_yieldvsphi_data_plt = ROOT.TMultiGraph()
-        
-        G_yieldvsphi_data = ROOT.TGraphErrors(len(yield_data[i][1]),phibins_data[i][1],yield_data[i][1],np.array([0]*len(phibins_data[i][1])),np.array([0]*len(yield_data[i][1])))
-        G_yieldvsphi_simc = ROOT.TGraphErrors(len(yield_simc[i][1]),phibins_simc[i][1],yield_simc[i][1],np.array([0]*len(phibins_simc[i][1])),np.array([0]*len(yield_simc[i][1])))
-
-        G_yieldvsphi_data.SetMarkerStyle(21)
-        G_yieldvsphi_data.SetMarkerSize(1)
-        G_yieldvsphi_data.SetMarkerColor(1)
-        G_yieldvsphi_data_plt.Add(G_yieldvsphi_data)
-
-        G_yieldvsphi_simc.SetMarkerStyle(21)
-        G_yieldvsphi_simc.SetMarkerSize(1)
-        G_yieldvsphi_simc.SetMarkerColor(2)
-        G_yieldvsphi_data_plt.Add(G_yieldvsphi_simc)
+    G_yieldvsphi_simc.SetMarkerStyle(21)
+    G_yieldvsphi_simc.SetMarkerSize(1)
+    G_yieldvsphi_simc.SetMarkerColor(2)
+    G_yieldvsphi_data_plt.Add(G_yieldvsphi_simc)
 
     C_yieldvsphi_data_plt.cd(i+1)
         
