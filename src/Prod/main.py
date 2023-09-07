0dfd5c04ee8db1526fa00e624ee05f46e05407ee
Richard Trotta <trotta@cua.edu>
2023-09-06 20:35:58 -0400

    Testing ratio calculations

--- src/Prod/main.py
+++ src/Prod/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-06 20:33:40 trottar"
+# Time-stamp: "2023-09-06 20:35:56 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1280,7 +1280,7 @@ for i, val in enumerate(t_bins):
     G_yieldvsphi_data_plt.GetXaxis().SetTitleOffset(1.5)
     G_yieldvsphi_data_plt.GetXaxis().SetLabelSize(0.04)
 
-    G_yieldvsphi_data_plt.Clear()
+    G_yieldvsphi_data_plt = ROOT.TMultiGraph()
     
 #l_yieldvsphi_data_plt.AddEntry(G_yieldvsphi_data,"Data")
 #l_yieldvsphi_data_plt.AddEntry(G_yieldvsphi_simc,"Simc")
