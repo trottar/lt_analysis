7c820f2749cfbc1dfd549e30128909672b56784a
Richard Trotta <trotta@cua.edu>
2023-09-07 14:38:40 -0400

    Adjusted locations for fortran scripts to keep pid seprate

--- src/average_kinematics.f
+++ src/average_kinematics.f
@@ -110,11 +110,16 @@ c     Get low, high eps. and neg., pos. polarity data.
                   else
                      stop '*** aver: wrong pol ***'
                   endif
-                  print *, "pid = ", pid
-                  print *, "pol = ", pol
-                  print *, "q2_set = ", q2_set
-                  print *, "eps_set(lh) = ", eps_set(lh)
-                  print *, "th_pq = ", th_pq                  
+! Add a print statement to check variables before read
+                  print *, '(a4,''kindata/kindata.'',a2,''_'',i3.3,
+     *                 ''_'',i2.2,''_'',i5.4,S,''.''dat'')', pid, pol, 
+     *                 nint(q2_set*100.), nint(eps_set(lh)*100.), 
+     *                 nint(th_pq*1000.)
+! Add a print statement to indicate reading from file
+                  print *, '(a4,''kindata/kindata.'',a2,''_'',i3.3,
+     *                 ''_'',i2.2,''_'',i5.4,S,''.''dat'')', pid,
+     *                 pol, nint(q2_set*100.), nint(eps_set(lh)*100.), 
+     *                 nint(th_pq*1000.)
                   write(fn,'(a4,''kindata/kindata.'',a2,''_'',i3.3,''_'',i2.2,
      *                 ''_'',SP,i5.4,S,''.dat'')') pid, pol,
      *                 nint(q2_set*10.),
