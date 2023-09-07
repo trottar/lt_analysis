e0025b4eaaf4ae874928b85d6116b55f3d8b33f7
Richard Trotta <trotta@cua.edu>
2023-09-07 12:27:09 -0400

    Testing ave_kin fortran script

--- src/average_kinematics.f
+++ src/average_kinematics.f
@@ -263,8 +263,8 @@ c     Get Beam energy at first.
 
       do it=1,ntbins
          tm=tmin+(it-0.5)*(tmax-tmin)/ntbins
-         call eps_n_theta(pol_set,Eb,aveW(it),aveQ2(it),tm,th_mod
-     &         ,eps_mod)
+         call eps_n_theta(pol_set,Eb,aveW(it),aveQ2(it),tm,th_mod,
+     &         eps_mod)
          thetacm_only(it)=th_mod*180./3.14159   
       end do
 
@@ -284,6 +284,8 @@ c     Save data.
 c         write(*,'(4f8.5,i3)') aveW(it),errW(it),aveQ2(it),errQ2(it),it
       end do
       close(77)
+
+      end
 
       include 'eps_n_theta.f'
 
