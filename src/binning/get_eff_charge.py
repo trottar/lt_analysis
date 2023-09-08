c05d537853e9450939b6a3cbf7088982bd0e4f70
Richard Trotta <trotta@cua.edu>
2023-09-07 20:02:13 -0400

    Reorganized all directories to make things easier to follow and removed unused scripts. Need to test everything, i probably broke some things

--- src/binning/get_eff_charge.py
+++ src/binning/get_eff_charge.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-07 19:48:16 trottar"
+# Time-stamp: "2023-09-07 20:02:04 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -159,7 +159,7 @@ def get_eff_charge(hist, inpDict):
     ################################################################################################################################################
     # Grab and calculate efficiency
 
-    sys.path.append('../src/setup/')
+    sys.path.append('../setup/')
     from getDataTable import calculate_effError
 
     tot_effError_data = [calculate_effError(run,efficiency_table) for run in runNums.split(' ')]
