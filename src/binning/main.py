19ba46c48d7efd3ec9d840720db70eef9a3199a2
Richard Trotta <trotta@cua.edu>
2023-09-11 23:47:13 -0400

    Saving all new outputs to time stamped directory in cache

--- src/binning/main.py
+++ src/binning/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-11 23:43:41 trottar"
+# Time-stamp: "2023-09-11 23:47:10 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1396,7 +1396,7 @@ create_lists(aveDict, ratioDict, histlist, inpDict, phisetlist, output_file_lst)
 # Get the current date and time
 now = datetime.datetime.now()
 # Format the date and time as a string to use in the file name
-formatted_date = now.strftime("%H%M%S_%Y%B%D")
+formatted_date = now.strftime("H%HM%MS%S_%Y%B%d")
 
 for f in output_file_lst:    
     if OUTPATH in f:
