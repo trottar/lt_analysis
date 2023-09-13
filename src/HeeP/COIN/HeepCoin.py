39d07c3f4993e0f6098c7e6b3f20f63f3ba78b13
Richard Trotta <trotta@cua.edu>
2023-09-13 19:57:32 -0400

    Testing fit ranges for heep

--- src/HeeP/COIN/HeepCoin.py
+++ src/HeeP/COIN/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-13 19:52:28 trottar"
+# Time-stamp: "2023-09-13 19:57:29 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1748,11 +1748,13 @@ def fit_gaussian(hist, x_min, x_max, dtype):
     while hist.GetBinContent(left_bin) > half_max and left_bin > 1:
         left_bin -= 1
         print("~~~~~~~~~~~left_bin", left_bin)
-        print("~~~~~~~~~~~bin_center",hist.GetBinCenter(left_bin))
+        print("!!!!!!!!!!!left_value", hist.GetBinContent(left_bin))
+        print("~~~~~~~~~~~left_center",hist.GetBinCenter(left_bin))
     while hist.GetBinContent(right_bin) > half_max and right_bin < hist.GetNbinsX():
         right_bin += 1
         print("~~~~~~~~~~~right_bin", right_bin)
-        print("~~~~~~~~~~~bin_center",hist.GetBinCenter(right_bin))        
+        print("!!!!!!!!!!!right_value", hist.GetBinContent(right_bin))
+        print("~~~~~~~~~~~right_center",hist.GetBinCenter(right_bin))
     
     min_range = hist.GetBinCenter(left_bin)
     print("min_range",min_range)
