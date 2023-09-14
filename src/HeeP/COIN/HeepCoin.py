1d4e3a804e2975e633ab4be9e0e9f9ae497ac212
Richard Trotta <trotta@cua.edu>
2023-09-13 21:37:38 -0400

    Testing fit ranges for heep

--- src/HeeP/COIN/HeepCoin.py
+++ src/HeeP/COIN/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-13 21:34:46 trottar"
+# Time-stamp: "2023-09-13 21:37:36 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1773,7 +1773,7 @@ def fit_gaussian(hist, x_min, x_max, dtype):
         fit_func.SetLineColor(kRed)
     if dtype == "dummy":
         fit_func.SetLineColor(kGreen)    
-    hist.Fit("fit_func", "QR", "", min_range, max_range)
+    hist.Fit("fit_func", "QR+", "", min_range, max_range)
     mean = fit_func.GetParameter(1)
     #fit_func.Draw("same")
     return mean
