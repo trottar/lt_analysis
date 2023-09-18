3e4b7a895f2eca25d15dda1f0a3856d50987b1fe
Richard Trotta <trotta@cua.edu>
2023-09-18 11:47:29 -0400

    Testing iteration weight being applied to simc

--- src/simc_ana/physics_iterate.f
+++ src/simc_ana/physics_iterate.f
@@ -379,8 +379,8 @@ c	write(6,*)' phicm ',phicm*180./3.14159,phicm_fer*180./3.14159,phipq*180./3.141
 
 
 	   sig219=(sigt+main%epsilon*sigl+main%epsilon*cos(2.*phicm)*sigtt
-     >		+sqrt(2.0*main%epsilon*(1.+main%epsilon))*cos(phicm)*siglt)/1.d0
-	  
+     >		+sqrt(2.0*main%epsilon*(1.+main%epsilon))
+     >		*cos(phicm)*siglt)/1.d0
 c now convert to different W
 c W dependence given by 1/(W^2-M^2)^2
 c factor 15.333 is value of (w**2-ami**2)**2 at W=2.19
