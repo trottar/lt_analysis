1969e2b0f4a47e054464eba56a8d45794d003c62
Richard Trotta <trotta@cua.edu>
2023-09-18 00:54:02 -0400

    Testing iteration weight being applied to simc

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2023-09-18 00:53:20 trottar"
+# Time-stamp: "2023-09-18 00:53:59 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -110,7 +110,7 @@ def iter_weight(param_file, fort_param, formatted_date):
     command = 'gfortran {} -o output && ./output {}'.format(fort_param, inp_fort_param)
 
     # Execute the command and capture the output
-    result = subprocess.run(command, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.call(command, shell=True, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Check if the execution was successful
     if result.returncode == 0:
