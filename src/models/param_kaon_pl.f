6c9c504547ff65fc9d74dda532fef58ca999bdbc
Richard Trotta <trotta@cua.edu>
2023-09-18 01:46:45 -0400

    Testing iteration weight being applied to simc

--- src/models/param_kaon_pl.f
+++ src/models/param_kaon_pl.f
@@ -67,41 +67,5 @@
 	print *, "p11 =", p11
 	print *, "p12 =", p12	
 	
-***     
-*       Parameterization based upon Fpi-1 pi+ IT25, 12.04.18
-*       Revised for IT21, 12.11.06
-*        tav=(0.0735+0.028*log(Q2i))*Q2i
-	q2_gev=float(q2_set)/100.
-        tav=(0.0735+0.028*log(q2_gev))*q2_gev
-        ftav=(ti-tav)/tav
-	ft=ti/(ti+0.139570**2)**2
-
-*	nsigl=(p1+p2*log(Q2i))*exp((p3+p4*log(Q2i))*ti)
-	nsigl=(p1+p2*log(Q2i))*exp((p3+p4*log(Q2i))*(ti-0.2))
-	nsigt=p5+p6*log(Q2i)+(p7+p8*log(Q2i))*ftav
-
-*	nsiglt=p9*exp(p10*ti)*sin(thetacmi)
-	nsiglt=(p9*exp(p10*ti)+p11/ti)*sin(thetacmi)
-	nsigtt=(p12*Q2i*exp(-Q2i))*ft*sin(thetacmi)**2
-
-	nsig219=(nsigt+epsiloni*nsigl+epsiloni*cos(2.*phicmi)*nsigtt
-     1      +sqrt(2.0*epsiloni*(1.+epsiloni))*cos(phicmi)*nsiglt)/1.d0
-
-	wfactor=1.D0/(Wcmi**2-mtar_gev**2)**2
-	nsig=nsig219*wfactor
-
-	nsig=nsig/2./pi/1.d+06   !dsig/dtdphicm in microbarns/MeV**2/rad
-
-	wtn=Weight*nsig/dsigdt
-
-	my_limit=0.20
-        if ((wtn.lt.my_limit).and.(wtn.gt.0.0)) then
-          continue
-        else
-          wtn=0.
-        endif
-
-!       Print the value of wtn
-	print *, "wtn =", wtn
 
 	end program iterWeight
