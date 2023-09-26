891d5cd2954c67af3ab94fdfbfd3755fcda92510
Richard Trotta <trotta@cua.edu>
2023-09-26 14:01:21 -0400

    Plotting separated values

--- src/main_iter.py
+++ src/main_iter.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-26 13:36:57 trottar"
+# Time-stamp: "2023-09-26 14:01:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -414,7 +414,7 @@ if EPSSET == "high":
     # It then runs calc_xsect.f to find unseparated cross section as well as new set of parameters
     # if still iterating weights
     try:
-        subprocess.call(['bash', '{}/run_xsect.sh'.format(LTANAPATH), Q2, W, ParticleType, POL, NumtBins, NumPhiBins])
+        subprocess.call(['bash', '{}/run_xsect.sh'.format(LTANAPATH), Q2, W, ParticleType, POL, str(NumtBins), str(NumPhiBins)])
     except Exception as e:
         print("1 ERROR: {}".format(e))
         sys.exit(2)
