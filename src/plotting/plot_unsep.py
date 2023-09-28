879e6c6cd033fb48d7b7c2ee9821c1f63ce4ba9d
Richard Trotta <trotta@cua.edu>
2023-09-27 21:00:19 -0400

    Plotting separated values

--- src/plotting/plot_unsep.py
+++ src/plotting/plot_unsep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-09-27 20:46:36 trottar"
+# Time-stamp: "2023-09-27 21:00:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -304,7 +304,7 @@ for k in range(NumtBins):
     j=0
     bin_check = False
     for i in range(0,NumtBins*NumPhiBins-1):
-        print(np.array(file_df_dict['unsep_file_loeps']['tm'].tolist())[i]," == ",np.array(file_df_dict['unsep_file_loeps']['tm'].tolist())[i+1])
+        print(np.array(file_df_dict['unsep_file_loeps']['tm'].tolist())[i]," - ",t_bin_centers[k])
         if are_within_tolerance(np.array(file_df_dict['unsep_file_loeps']['tm'].tolist())[i],t_bin_centers[k]):
             print("^^")
             if bin_check:
