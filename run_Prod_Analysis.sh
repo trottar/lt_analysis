6053e76e62d04a9dcd6714681c56d66b763b85a7
Richard Trotta <trotta@cua.edu>
2023-10-12 20:45:29 -0400

    testing production runs

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -88,25 +88,25 @@ else
 fi
 
 # Q2=2p1
-#NumtBins=3
-#NumPhiBins=12
-# Q2=5p5
 NumtBins=3
-NumPhiBins=8
+NumPhiBins=12
+# Q2=5p5
+#NumtBins=3
+#NumPhiBins=8
 
 # Define global variables for lt_analysis scripts
 POL="+1" # All KaonLT is positive polarity
 # TMIN should not equal zero (unless calc_xsect.f is adapted)
 # Make sure 3 sig figs (no more)
 # Q2=2p1
-#TMIN=0.001
-#TMAX=0.600
+TMIN=0.001
+TMAX=0.600
 # Q2=3p0
 #TMIN=0.001
 #TMAX=0.990
 # Q2=5p5
-TMIN=0.001
-TMAX=1.300
+#TMIN=0.001
+#TMAX=1.300
 # Efficiency csv file
 #EffData="coin_production_Prod_efficiency_data_2022_12_05.csv"
 #EffData="coin_production_Prod_efficiency_data_2022_12_30.csv"
