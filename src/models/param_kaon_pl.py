0f2512657fce27dea9e6a636abc9d0fdef5e1c97
Richard Trotta <trotta@cua.edu>
2023-10-31 13:00:35 -0400

    Testing thetacm variable

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-10-31 12:17:40 trottar"
+# Time-stamp: "2023-10-31 12:57:59 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -62,7 +62,11 @@ def iterWeight(arg_str):
     siglt = siglt*wfactor    
     sig = sig / 2.0 / pi / 1e6  # dsig/dtdphicm in microbarns/MeV**2/rad
 
-    #wtn = wt_sim * sig / sigcm_sim
-    wtn = wt_sim
+    wtn = wt_sim * sig / sigcm_sim
+
+    print("sig",sig)
+    print("sigcm",sigcm)
+    print("wtn",wtn)
+    print("wt_sim",wt_sim)
     
     return [float(wtn),float(sig)]
