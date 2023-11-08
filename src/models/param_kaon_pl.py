fe20d51e169e884bb3ddb728f95c2b17070708bb
Richard Trotta <trotta@cua.edu>
2023-11-08 16:30:29 -0500

    Testing issues with sig calculation

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-11-08 16:00:35 trottar"
+# Time-stamp: "2023-11-08 16:29:33 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -32,8 +32,6 @@ def iterWeight(arg_str):
     t_gev = t_sim  # Already GeV
     s = w_sim**2
     s_gev = s # Already GeV
-
-    tmp = s_gev
     
     # Calculate tav, ftav, ft
     #tav = (0.0735 + 0.028 * math.log(q2_set)) * q2_set
@@ -42,6 +40,8 @@ def iterWeight(arg_str):
     ftav = (abs(t_gev) - tav) / tav
     ft = abs(t_gev) / (abs(t_gev) + mkpl**2)**2
 
+    tmp = ftav
+    
     # Calculate sigl, sigt, siglt, sigtt, sig219, sig
     p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12 = params
     #sigl = (p1 + p2 * math.log(q2_gev)) * math.exp((p3 + p4 * math.log(q2_gev)) * (abs(t_gev) - 0.2))
