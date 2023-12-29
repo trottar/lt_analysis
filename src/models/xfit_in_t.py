e85f28a1db80df910203759eb66c658837075d1d
Richard Trotta <trotta@cua.edu>
2023-12-29 00:14:35 -0500

    Testing weight iter script

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-12-28 22:05:11 trottar"
+# Time-stamp: "2023-12-29 00:14:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -115,14 +115,11 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set):
                 prv_par_vec.append(par)
     except FileNotFoundError:
         print("File {} not found.".format(para_file_in))
-
-    print(prv_par_vec)
-    print(len(prv_par_vec))
         
     #t0, t1, t2, t3, l0, l1, l2, l3, lt0, lt1, lt2, lt3, tt0, tt1, tt2, tt3 = prv_par_vec[:16]
     t0, t1, t2, l0, l1, l2, lt0, lt1, lt2, tt0, tt1, tt2 = prv_par_vec[:12] # RLT too many parameters
     
-    ave_file_in = LTANAPATH + "src/averages/avek." + q2_set + ".dat"
+    ave_file_in = LTANAPATH + "src/averages/avek." + q2_set.replace("p","") + ".dat"
     with open(ave_file_in, 'r') as f:
         for line in f:
             w, w_e, q2, q2_e, tt, tt_e, thetacm, it = map(float, line.strip().split())
