48e628dc73bcceaa931c68af4bbca43ad3305af6
Richard Trotta <trotta@cua.edu>
2024-01-02 01:03:16 -0500

    Testing weight iter script

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-02 00:54:02 trottar"
+# Time-stamp: "2024-01-02 01:03:09 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -381,6 +381,8 @@ from get_eff_charge import get_eff_charge
 # Upate hist dictionary with effective charge
 for hist in histlist:
     hist.update(get_eff_charge(hist, inpDict))
+    print("\n\n\n\n\n$$$$$$$$$$$$$$$$$1",hist["G_data_eff"])
+
 
 sys.path.append("simc_ana")    
 from compare_simc import compare_simc
@@ -388,6 +390,7 @@ from compare_simc import compare_simc
 # Upate hist dictionary with effective charge and simc histograms
 for hist in histlist:
     hist.update(compare_simc(hist, inpDict))    
+    print("\n\n\n\n\n$$$$$$$$$$$$$$$$$2",hist["G_data_eff"])
     
 sys.path.append("plotting")
 from data_vs_simc import plot_data_vs_simc
@@ -398,9 +401,6 @@ cut_summary_lst = plot_data_vs_simc(t_bins, phi_bins, histlist, phisetlist, inpD
 if DEBUG:
     show_pdf_with_evince(outputpdf)
 output_file_lst.append(outputpdf)
-
-for hist in histlist:
-    print("\n\n\n\n\n$$$$$$$$$$$$$$$$$1",hist["G_data_eff"])
 
 
 ##############################
@@ -455,7 +455,7 @@ output_file_lst.append(outputpdf.replace("{}_".format(ParticleType),"{}_binned_"
 #if not os.path.exists(foutroot):
 if os.path.exists(foutroot):
     for hist in histlist:
-        print("\n\n\n\n\n$$$$$$$$$$$$$$$$$2",hist["G_data_eff"])
+        print("\n\n\n\n\n$$$$$$$$$$$$$$$$$3",hist["G_data_eff"])
         print("\nSaving {} histograms to {}".format(hist["phi_setting"],foutroot))
         # Loop through all keggys,values of dictionary
         for i, (key, val) in enumerate(hist.items()):
