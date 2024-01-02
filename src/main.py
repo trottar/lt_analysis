63123637d6e9426bf2b6e41fd7b1fe2f4350e47a
Richard Trotta <trotta@cua.edu>
2024-01-02 00:32:31 -0500

    Testing weight iter script

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-02 00:29:08 trottar"
+# Time-stamp: "2024-01-02 00:32:28 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -382,14 +382,13 @@ from get_eff_charge import get_eff_charge
 for hist in histlist:
     hist.update(get_eff_charge(hist, inpDict))
 
-    print("\n\n\n\n\n$$$$$$$$$$$$$$$$$",hist["G_data_eff"])
-
 sys.path.append("simc_ana")    
 from compare_simc import compare_simc
 
 # Upate hist dictionary with effective charge and simc histograms
 for hist in histlist:
     hist.update(compare_simc(hist, inpDict))
+    print("\n\n\n\n\n$$$$$$$$$$$$$$$$$",hist["G_data_eff"])
     
 sys.path.append("plotting")
 from data_vs_simc import plot_data_vs_simc
