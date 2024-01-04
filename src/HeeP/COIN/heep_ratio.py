06fde6a428d3e89ec4a02364a7ea84b2e7d220c8
Richard Trotta <trotta@cua.edu>
2024-01-04 14:24:19 -0500

    Testing heep coin no pid

--- src/HeeP/COIN/heep_ratio.py
+++ src/HeeP/COIN/heep_ratio.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-04 14:07:01 trottar"
+# Time-stamp: "2024-01-04 14:23:34 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -158,8 +158,8 @@ plt.legend()
 plt.tight_layout()
 
 # No PID vs PID
-rel_yield_nopid = [1.1992, 1.1178, 0.9967, 0.8298, ]
-rel_yield_pid = []
+rel_yield_nopid = [1.1992, 1.1178, 0.9967, 0.8298, 0.9668]
+rel_yield_pid = [0.9910, 0.9327, 0.8595, 0.7660, 0.8971]
 
 slope_relyield_nopid, intercept_relyield_nopid, _, _, _ = linregress(Ebeam, rel_yield_nopid)
 
@@ -170,7 +170,7 @@ plt.figure(figsize=(12,8))
 
 # Plot rel_yield on a different plot with a horizontal line at y=1.0
 plt.subplot(221)
-plt.plot(Q2, rel_yield, 'go')
+plt.plot(Q2, rel_yield_pid, 'go')
 plt.plot(Q2, slope_relyield_nopid * np.array(Q2) + intercept_relyield_nopid, 'g--', label='m={:.2f}, b={:.2f}'.format(slope_relyield_nopid, intercept_relyield_nopid))
 plt.axhline(y=1.0, color='gray')
 plt.xlabel('Q2')
@@ -179,31 +179,38 @@ plt.legend()
 
 # Duplicate the third plot for better visualization
 plt.subplot(222)
-plt.plot(Q2, rel_yield, 'go')
+plt.plot(Q2, rel_yield_pid, 'go')
 plt.plot(Q2, slope_relyield_pid * np.array(Q2) + intercept_relyield_pid, 'g--', label='m={:.2f}, b={:.2f}'.format(slope_relyield_pid, intercept_relyield_pid))
 plt.axhline(y=1.0, color='gray')
 plt.xlabel('Q2')
 plt.ylabel('Rel. Yield')
 plt.legend()
 
+# No PID vs PID
+rel_yield_nopid = []
+rel_yield_pid = []
+
+slope_relyield_nopid, intercept_relyield_nopid, _, _, _ = linregress(Ebeam, rel_yield_nopid)
+
+slope_relyield_pid, intercept_relyield_pid, _, _, _ = linregress(Ebeam, rel_yield_pid)
+
 # Plot rel_yield on a different plot with a horizontal line at y=1.0
 plt.subplot(223)
-plt.plot(ebeam, rel_yield, 'go')
-plt.plot(ebeam, slope_relyield_nopid * np.array(ebeam) + intercept_relyield_nopid, 'g--', label='m={:.2f}, b={:.2f}'.format(slope_relyield_nopid, intercept_relyield_nopid))
+plt.plot(Q2, rel_yield_pid, 'go')
+plt.plot(Q2, slope_relyield_nopid * np.array(Q2) + intercept_relyield_nopid, 'g--', label='m={:.2f}, b={:.2f}'.format(slope_relyield_nopid, intercept_relyield_nopid))
 plt.axhline(y=1.0, color='gray')
-plt.xlabel('ebeam')
+plt.xlabel('Q2')
 plt.ylabel('Rel. Yield')
 plt.legend()
 
 # Duplicate the third plot for better visualization
 plt.subplot(224)
-plt.plot(ebeam, rel_yield, 'go')
-plt.plot(ebeam, slope_relyield_pid * np.array(ebeam) + intercept_relyield_pid, 'g--', label='m={:.2f}, b={:.2f}'.format(slope_relyield_pid, intercept_relyield_pid))
+plt.plot(Q2, rel_yield_pid, 'go')
+plt.plot(Q2, slope_relyield_pid * np.array(Q2) + intercept_relyield_pid, 'g--', label='m={:.2f}, b={:.2f}'.format(slope_relyield_pid, intercept_relyield_pid))
 plt.axhline(y=1.0, color='gray')
-plt.xlabel('ebeam')
+plt.xlabel('Q2')
 plt.ylabel('Rel. Yield')
 plt.legend()
-
 
 # Adjust layout for better spacing
 plt.tight_layout()
--- src/setup/getEfficiencyValue.py
+++ src/setup/getEfficiencyValue.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-04 13:51:33 trottar"
+# Time-stamp: "2024-01-04 14:21:25 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -35,8 +35,7 @@ def getEfficiencyValue(runNum,efficiency_table,table_val):
 
         from getDataTable import calculate_efficiency
 
-        #tot_efficiency = calculate_efficiency(runNum,efficiency_table)
-        tot_efficiency = 1.0
+        tot_efficiency = calculate_efficiency(runNum,efficiency_table)
 
         ################################################################################################################################################
 
