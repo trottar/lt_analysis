ac4be8d192df590f1353e6d00a684320baff57b5
Richard Trotta <trotta@cua.edu>
2024-01-10 18:23:35 -0500

    Testing iteration and fitting scripts

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-10 18:22:48 trottar"
+# Time-stamp: "2024-01-10 18:23:26 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -65,7 +65,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set):
         tt = x[0]
         qq = x[1]
         f_av = (abs(tt)-tav)/tav
-        print("Calculating params for func_SigT...\nQ2_{:.1e}, t = {:.3e}\npar = {}",format(qq, tt, par))
+        print("Calculating params for func_SigT...\nQ2_{0:.1e}, t = {1:.3e}\npar = {2}",format(qq, tt, par))
         f = par[0]+par[1]*math.log(qq)+(par[2]+par[3]*math.log(qq))*f_av
         return f
 
