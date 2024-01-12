f8bd0286ecf01701991710ef4fa69a1007db7100
Richard Trotta <trotta@cua.edu>
2024-01-12 14:37:46 -0500

    Added function to utility to check if run root file exists.

--- src/setup/findEffectiveCharge.py
+++ src/setup/findEffectiveCharge.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-12 14:36:15 trottar"
+# Time-stamp: "2024-01-12 14:37:42 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -74,7 +74,7 @@ for runNum in RUNLIST:
         pTheta_val += " " + str(getEfficiencyValue(runNum,efficiency_table,"pTheta"))
     else:
         RUNLIST.remove(runNum)
-        ' '.join(map(str, RUNLIST))
+        ' '.join(str(run) for run in RUNLIST)
         
 BashInput=("{}\n{}\n{}\n{}\n{}\n{}\n{}".format(effective_charge, effective_charge_uncern, tot_efficiency, tot_efficiency_uncern, pTheta_val, ebeam_val, RUNLIST))
 print(BashInput)
