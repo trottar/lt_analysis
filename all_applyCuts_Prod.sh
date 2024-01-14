73fd4e8f3360521979dfff2b77e55268b94c0f4a
Richard Trotta <trotta@cua.edu>
2024-01-14 02:04:03 -0500

    Rerunning Q2=2p1 with hgcer cut

--- all_applyCuts_Prod.sh
+++ all_applyCuts_Prod.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-14 01:59:18 trottar"
+# Time-stamp: "2024-01-14 02:04:00 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -59,8 +59,8 @@ grab_runs () {
 #file_name="Q2p1W2p95center_lowe" # DONE
 #file_name="Q2p1W2p95left_lowe" # DONE
 #file_name="Q2p1W2p95center_highe_dummy" # DONE
-file_name="Q2p1W2p95left_highe_dummy"
-#file_name="Q2p1W2p95right_highe_dummy"
+#file_name="Q2p1W2p95left_highe_dummy" # DONE
+file_name="Q2p1W2p95right_highe_dummy"
 #file_name="Q2p1W2p95center_lowe_dummy"
 #file_name="Q2p1W2p95left_lowe_dummy"
 numbers_to_match=()
@@ -87,7 +87,7 @@ while true; do
             do
                 echo "Running ${number}"
                 cd $kaonlt/../lt_analysis
-		./applyCuts_Prod.sh -p high left 2p1 2p95 dummy ${number} kaon
+		./applyCuts_Prod.sh -p high right 2p1 2p95 dummy ${number} kaon
             done
             break ;;
         [Nn]* ) 
