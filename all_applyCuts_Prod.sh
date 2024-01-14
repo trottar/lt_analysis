87fdb6ff80ed7d92f0643cf8c599389a2e43d98d
Richard Trotta <trotta@cua.edu>
2024-01-14 01:30:40 -0500

    Rerunning Q2=2p1 with hgcer cut

--- all_applyCuts_Prod.sh
+++ all_applyCuts_Prod.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-14 01:29:39 trottar"
+# Time-stamp: "2024-01-14 01:30:32 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -82,19 +82,19 @@ while true; do
             # Finds number of lines of inpFile
             numlines=$(eval "wc -l < ${inpFile}")
             echo
-            echo "Run $(( ${number} + 1 ))/$(( ${numlines} + 1 ))" >> "${file_name}_all_applyCuts_Prod.log"
+            echo "Run $(( ${number} + 1 ))/$(( ${numlines} + 1 ))"
             # Loop through each number in the list
             for number in "${numbers_to_match[@]}"
             do
-                echo "Running ${number}" >> "${file_name}_all_applyCuts_Prod.log"
+                echo "Running ${number}"
                 cd $kaonlt/../lt_analysis
                 ./applyCuts_Prod.sh -p low left 2p1 2p95 LH2 ${number} kaon >> "${file_name}_all_applyCuts_Prod.log"
             done
             break ;;
         [Nn]* ) 
-            echo "Operation aborted." >> "${file_name}_all_applyCuts_Prod.log"
+            echo "Operation aborted."
             exit ;;
         * ) 
-            ;;
+            echo "Please answer yes or no." ;;
     esac
 done
