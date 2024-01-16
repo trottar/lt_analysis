c76dc2a37e7ebb2f59a7c78d4ae391dd89357012
Richard Trotta <trotta@cua.edu>
2024-01-15 20:19:36 -0500

    Rerunning Q2=2p1 with hgcer cut

--- src/plotting/data_vs_simc.py
+++ src/plotting/data_vs_simc.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-15 19:42:05 trottar"
+# Time-stamp: "2024-01-15 20:18:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -385,7 +385,7 @@ def plot_data_vs_simc(t_bins, phi_bins, histlist, phisetlist, inpDict):
         if j == 0:
             tBin_line.SetLineColor(7)
         else:
-            tBin_line.SetLineColor(8)
+            tBin_line.SetLineColor(7)
         l_t.AddEntry(tBin_line,"Evts in {:.2f}-{:.2f}: {:.0f}".format(t_bins[j],t_bins[j+1], events_between[j]))     
         tBin_line.SetLineWidth(4)
         tBin_line.DrawLine(b,0,b,binmax)
