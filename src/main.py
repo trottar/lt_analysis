9885f087b3f6dda49cc4f67d3fece134202eea34
Richard Trotta <trotta@cua.edu>
2024-01-17 12:54:03 -0500

    Testing Q2=2p1

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-17 03:29:30 trottar"
+# Time-stamp: "2024-01-17 12:53:45 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -47,8 +47,8 @@ if len(sys.argv)-1!=37:
 
 ##################################################################################################################################################    
 
-DEBUG = True # Flag for plots
-#DEBUG = False # Flag for plots
+#DEBUG = True # Flag for plots
+DEBUG = False # Flag for plots
 
 # Input params
 kinematics = sys.argv[1].split("_")
