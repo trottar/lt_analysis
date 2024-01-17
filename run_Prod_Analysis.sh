28af6f5164e2b6ec607baad33101b4fe58cad437
Richard Trotta <trotta@cua.edu>
2024-01-17 12:53:28 -0500

    Testing Q2=2p1

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -99,8 +99,10 @@ POL="+1" # All KaonLT is positive polarity
 # TMIN should not equal zero (unless calc_xsect.f is adapted)
 # Make sure 3 sig figs (no more)
 # Q2=2p1
-TMIN=0.001
-TMAX=0.600
+#TMIN=0.001
+#TMAX=0.600
+TMIN=0.250
+TMAX=0.350
 # Q2=3p0
 #TMIN=0.001
 #TMAX=0.990
