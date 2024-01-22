e5dfe2e7bcefad9dd61778bd3afc1c2ef66159c0
Richard Trotta <trotta@cua.edu>
2024-01-22 18:55:18 -0500

    Testing Q2=3.0, W=2.32

--- all_applyCuts_Prod.sh
+++ all_applyCuts_Prod.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-22 18:49:15 trottar"
+# Time-stamp: "2024-01-22 18:55:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -58,16 +58,16 @@ TARGET="LH2"
 
 KIN="Q3p0W2p32"
 
-#EPS="high"
-EPS="low"
+EPS="high"
+#EPS="low"
 
-PHISET="center"
+#PHISET="center"
 #PHISET="left"
-#PHISET="right"
+PHISET="right"
 
 # Q3p0W2p32center_highe
 # Q3p0W2p32left_highe
-# Q3p0W2p32right_highe
+# Q3p0W2p32right_highe # DONE
 # Q3p0W2p32center_lowe # DONE
 # Q3p0W2p32left_lowe # DONE
 # Q3p0W2p32center_highe_dummy # DONE
