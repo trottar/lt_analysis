f17063a573f17c34bd7877728a1b43f3b45d166a
Richard Trotta <trotta@cua.edu>
2024-01-22 20:22:17 -0500

    Testing Q2=3.0, W=2.32

--- src/plotting/plot_xsects.py
+++ src/plotting/plot_xsects.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-21 13:44:28 trottar"
+# Time-stamp: "2024-01-22 20:22:15 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -773,7 +773,6 @@ for k in range(NumtBins):
     l_xmodreal_phi.Draw()
     C_xmodreal_phi.Print(outputpdf)
 
-
 C_xmodreal_phi = TCanvas()
 C_xmodreal_phi.SetGrid()
 C_xmodreal_phi.Divide(1,NumtBins)
@@ -897,6 +896,9 @@ for k in range(NumtBins):
     multiDict["G_xmodreal_phi_{}".format(k+1)].GetYaxis().SetTitleOffset(1.5)
     multiDict["G_xmodreal_phi_{}".format(k+1)].GetXaxis().SetTitleOffset(1.5)
     multiDict["G_xmodreal_phi_{}".format(k+1)].GetXaxis().SetLabelSize(0.04)
+
+    # Set y-axis to logarithmic scale
+    C_xmodreal_phi.GetPad(k + 1).SetLogy()
     
 l_xmodreal_phi.AddEntry(G_xreal_phi_loeps,"loeps")
 l_xmodreal_phi.AddEntry(G_xreal_phi_hieps,"hieps")    
