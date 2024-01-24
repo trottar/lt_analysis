e5091a810f8604fb3a33f0b2b08091ba8c7cf2c9
Richard Trotta <trotta@cua.edu>
2024-01-23 22:43:12 -0500

    Testing HMS cer > 1.5 cut

--- all_applyCuts_Prod.sh
+++ all_applyCuts_Prod.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-23 22:42:27 trottar"
+# Time-stamp: "2024-01-23 22:43:09 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -60,12 +60,12 @@ TARGET="dummy"
 KIN="Q3p0W3p14"
 #KIN="Q0p5W2p40"
 
-EPS="high"
-#EPS="low"
+#EPS="high"
+EPS="low"
 
-#PHISET="center"
+PHISET="center"
 #PHISET="left"
-PHISET="right"
+#PHISET="right"
 
 # center_highe # DONE
 # left_highe # DONE
@@ -75,7 +75,7 @@ PHISET="right"
 # center_highe_dummy # DONE
 # left_highe_dummy # DONE
 # right_highe_dummy # DONE
-# center_lowe_dummy
+# center_lowe_dummy # DONE
 # left_lowe_dummy
 
 if [ $TARGET = "dummy" ]; then
