dfca58c3789f80514511eb92f4ae6c33c027cf52
Richard Trotta <trotta@cua.edu>
2024-01-25 04:37:57 -0500

    Fixed issues with sorting input files properly. Testing for Q2=2.1

--- src/calc_xsect.f
+++ src/calc_xsect.f
@@ -156,25 +156,6 @@ c      pause
       mod_fn='models/xmodel_' // trim(pid) // '_' // trim(pol) // '.f'
       print*,'xmodel: file=',mod_fn
 
-*     Model fit parameters.
-
-      write(fn,10) pid,pol,nint(q2_set*10),nint(q2_set*100)
- 10   format(a4,'/parameters/par.',a2,'_Q',i2.2,'W',i3.3,'.dat')
-      print*, 'param: fn=',fn
-
-      open(56, file=fn)
-      do while (.true.)
-         read(56, *, end=9) p, e, i
-         par(i) = p
-!     Print Statements
-         print *,"param number: ", i
-         print *,"param: ", p
-         print *,"param err: ", e
-! You can customize the format as needed
-      end do
-      
- 9    close(56)
-      
       do it=1,nt
 
          WRITE(*,*) '============'
