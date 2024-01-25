c52b85ff1ef85b2531ccf063e47e36c41df3923f
Richard Trotta <trotta@cua.edu>
2024-01-24 23:19:42 -0500

    Testing HMS cer > 1.5 cut

--- src/calc_xsect.f
+++ src/calc_xsect.f
@@ -166,6 +166,7 @@ c      pause
          WRITE(*,*) 'tmin: ', tmn
          WRITE(*,*) 'tmax: ', tmx
          tm=tmn+(it-0.5)*(tmx-tmn)/nt
+         WRITE(*,*) 'avert: ', (tmx-tmn)/nt
          WRITE(*,*) 'tmin bin center: ', tm
          read(52,*) w,dw,q2,dq2,tt,dtt,th_cm
          write(6,32) w,dw,q2,dq2,tt,dtt,th_cm
