1d53557a24dd2262c44d8fd78cfa2b3bf717211d
Richard Trotta <trotta@cua.edu>
2024-01-25 14:22:18 -0500

    Fixed issues with sorting input files properly. Testing for Q2=2.1

--- src/plotting/data_vs_simc.py
+++ src/plotting/data_vs_simc.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-23 11:49:28 trottar"
+# Time-stamp: "2024-01-25 14:22:03 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -117,37 +117,37 @@ def plot_data_vs_simc(t_bins, phi_bins, histlist, phisetlist, inpDict):
         ##############
         # HARD CODED #
         ##############
-        hist["H_hsdelta_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_hsxptar_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_hsyptar_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssxfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssyfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssxpfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssypfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_hsxfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_hsyfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_hsxpfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_hsypfp_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssdelta_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssxptar_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ssyptar_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_q_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_Q2_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_t_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_epsilon_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_MM_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_th_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ph_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ph_q_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_th_q_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_ph_recoil_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_th_recoil_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_pmiss_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_emiss_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_pmx_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_pmy_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_pmz_SIMC"].Scale(hist["normfac_simc"]*1e6)
-        hist["H_W_SIMC"].Scale(hist["normfac_simc"]*1e6)
+        hist["H_hsdelta_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_hsxptar_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_hsyptar_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssxfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssyfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssxpfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssypfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_hsxfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_hsyfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_hsxpfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_hsypfp_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssdelta_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssxptar_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ssyptar_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_q_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_Q2_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_t_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_epsilon_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_MM_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_th_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ph_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ph_q_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_th_q_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_ph_recoil_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_th_recoil_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_pmiss_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_emiss_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_pmx_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_pmy_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_pmz_SIMC"].Scale(hist["normfac_simc"]*1e5)
+        hist["H_W_SIMC"].Scale(hist["normfac_simc"]*1e5)
         ##############
         ##############
         ##############
