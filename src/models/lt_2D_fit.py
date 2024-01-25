8c302dade2bf7527c0d8f1df153285a775ee1f81
Richard Trotta <trotta@cua.edu>
2024-01-25 05:49:52 -0500

    Fixed issues with sorting input files properly. Testing for Q2=2.1

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-25 05:48:28 trottar"
+# Time-stamp: "2024-01-25 05:49:49 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -623,12 +623,12 @@ def single_setting(q2_set, fn_lo, fn_hi):
         c2.SetRightMargin(0.03)
 
         # Print plots for c1 and c2 canvases
-        glo.Draw("A*")
-        glo.Fit("lo_eps_fit", "R")
-        c1.Print(outputpdf)
-        ghi.Draw("A*")
-        ghi.Fit("hi_eps_fit", "R")
-        c2.Print(outputpdf)
+        #glo.Draw("A*")
+        #glo.Fit("lo_eps_fit", "R")
+        #c1.Print(outputpdf)
+        #ghi.Draw("A*")
+        #ghi.Fit("hi_eps_fit", "R")
+        #c2.Print(outputpdf)
 
         # Clear c1 and c2 canvases
         c1.Clear()
