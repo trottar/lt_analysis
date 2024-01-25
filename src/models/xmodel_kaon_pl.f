0f51aa6cfa5868f3089a0f5c0a5bad119ee2049b
Richard Trotta <trotta@cua.edu>
2024-01-25 06:47:54 -0500

    Fixed issues with sorting input files properly. Testing for Q2=2.1

--- src/models/xmodel_kaon_pl.f
+++ src/models/xmodel_kaon_pl.f
@@ -106,20 +106,11 @@ c     Correct for W.
       sigTT=sigTT*wfactor
       sigLT=sigLT*wfactor
 
-*      sig=sig/2./pi/1.d+06      !dsig/dtdphicm in microbarns/MeV**2/rad
+      sig=sig/2./pi/1.d+06      !dsig/dtdphicm in microbarns/MeV**2/rad
 
       x_mod=sig
       
       th_mod=thetacm
-
-      print *,"!!!!sig: ", sig
-      print *,"    sigL: ", sigL
-      print *,"    sigT: ", sigT
-      print *,"    sigLT: ", sigLT
-      print *,"    sigTT: ", sigTT
-      print *,"    phi: ", phi
-      print *,"    th_mod: ", th_mod
-      print *,"    x_mod: ", x_mod      
       
       end
 
