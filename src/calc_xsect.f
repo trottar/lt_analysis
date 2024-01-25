77d63e1b68d9301a4c9b12e0310bcd3247644680
Richard Trotta <trotta@cua.edu>
2024-01-25 06:46:02 -0500

    Fixed issues with sorting input files properly. Testing for Q2=2.1

--- src/calc_xsect.f
+++ src/calc_xsect.f
@@ -187,8 +187,7 @@ c      pause
          
          do ip=1,nphi
 
-*            phi=(ip-0.5)*2.*3.14159/nphi
-            phi=(ip-0.5)*2.*3.14159/nphi*(180./3.14159)
+            phi=(ip-0.5)*2.*3.14159/nphi
             read(51,*) r,dr
             
             call xmodel(pid,npol_set,Eb,q2_set,w_set,
