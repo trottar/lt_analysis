b07d70ae26c0fd459e4ee10f598aac519c2aa80f
Richard Trotta <trotta@cua.edu>
2024-01-25 04:43:40 -0500

    Fixed issues with sorting input files properly. Testing for Q2=2.1

--- src/calc_xsect.f
+++ src/calc_xsect.f
@@ -163,7 +163,7 @@ c      pause
 
 *     Model fit parameters.
 
-      write(par_fn,50) pid,pol,nint(q2_set*10),nint(q2_set*100)
+      write(par_fn,50) pid,pol,nint(q2_set*10),nint(w_set*100)
  50   format(a4,'/parameters/par.',a2,'_Q',i2.2,'W',i3.3,'.dat')
       print*, 'param: par_fn=',par_fn
 
