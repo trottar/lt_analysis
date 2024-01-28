e8f07ccdba68e405acca3b7ba9ae72861a6129c6
Richard Trotta <trotta@cua.edu>
2024-01-28 00:22:04 -0500

    Updating param files to make Q2/W dependent.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-28 00:19:53 trottar"
+# Time-stamp: "2024-01-28 00:22:03 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -61,8 +61,8 @@ def x_fit_in_t(ParticleType, pol_str, closest_date, Q2, W, inpDict):
 def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, tmax_range):
 
     # xsects range
-    lo_bound = -1.0
-    hi_bound =  1.0
+    lo_bound = -0.5
+    hi_bound =  0.5
 
     tav = (0.1112 + 0.0066*math.log(float(q2_set.replace("p","."))))*float(q2_set.replace("p","."))
 
