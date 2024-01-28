6de6c7f83abd73676f3b747c3059cfa8b57877d3
Richard Trotta <trotta@cua.edu>
2024-01-28 03:33:43 -0500

    Updating param files to make Q2/W dependent.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-28 03:25:49 trottar"
+# Time-stamp: "2024-01-28 03:33:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -252,8 +252,10 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     g_q2_sigt_fit = ROOT.TGraph2DErrors()
     for i in range(g_sigt_fit.GetN()):
-        g_q2_sigt_fit.SetPoint(g_q2_sigt_fit.GetN(), q2_vec[i], g_sigt_fit.GetX()[i], g_sigt_fit.GetY()[i])
-        g_q2_sigt_fit.SetPointError(g_q2_sigt_fit.GetN()-1, 0.0, g_sigt_fit.GetEX()[i], g_sigt_fit.GetEY()[i])
+        #g_q2_sigt_fit.SetPoint(g_q2_sigt_fit.GetN(), q2_vec[i], g_sigt_fit.GetX()[i], g_sigt_fit.GetY()[i])
+        #g_q2_sigt_fit.SetPointError(g_q2_sigt_fit.GetN()-1, 0.0, g_sigt_fit.GetEX()[i], g_sigt_fit.GetEY()[i])
+        g_q2_sigt_fit.SetPoint(g_q2_sigt_fit.GetN(), g_sigt_fit.GetX()[i], g_sigt_fit.GetY()[i], q2_vec[i])
+        g_q2_sigt_fit.SetPointError(g_q2_sigt_fit.GetN()-1, g_sigt_fit.GetEX()[i], g_sigt_fit.GetEY()[i], 0.0)
     
     g_q2_sigt_fit.Fit(f_sigT, "S")
 
@@ -474,13 +476,10 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     g_q2_siglt_fit = ROOT.TGraph2DErrors()
     for i in range(g_siglt_fit.GetN()):
-        g_q2_siglt_fit.SetPoint(g_q2_siglt_fit.GetN(), q2_vec[i], g_siglt_fit.GetX()[i], g_siglt_fit.GetY()[i])
-        g_q2_siglt_fit.SetPointError(g_q2_siglt_fit.GetN()-1, 0.0, g_siglt_fit.GetEX()[i], g_siglt_fit.GetEY()[i])
-    
-    g_q2_siglt_fit = ROOT.TGraph2DErrors()
-    for i in range(g_siglt_fit.GetN()):
-        g_q2_siglt_fit.SetPoint(g_q2_siglt_fit.GetN(), q2_vec[i], g_siglt_fit.GetX()[i], g_siglt_fit.GetY()[i])
-        g_q2_siglt_fit.SetPointError(g_q2_siglt_fit.GetN()-1, 0.0, g_siglt_fit.GetEX()[i], g_siglt_fit.GetEY()[i])
+        #g_q2_siglt_fit.SetPoint(g_q2_siglt_fit.GetN(), q2_vec[i], g_siglt_fit.GetX()[i], g_siglt_fit.GetY()[i])
+        #g_q2_siglt_fit.SetPointError(g_q2_siglt_fit.GetN()-1, 0.0, g_siglt_fit.GetEX()[i], g_siglt_fit.GetEY()[i])
+        g_q2_siglt_fit.SetPoint(g_q2_siglt_fit.GetN(), g_siglt_fit.GetX()[i], g_siglt_fit.GetY()[i], q2_vec[i])
+        g_q2_siglt_fit.SetPointError(g_q2_siglt_fit.GetN()-1, g_siglt_fit.GetEX()[i], g_siglt_fit.GetEY()[i], 0.0)
     
     g_q2_siglt_fit.Fit(f_sigLT, "S")
     
@@ -591,7 +590,14 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigTT = TF2("sig_TT", fun_Sig_TT, tmin_range, tmax_range, lo_bound, hi_bound, 4)
     f_sigTT.SetParameters(tt0, tt1, tt2, tt3)
     
-    g_sigtt_fit.Fit(f_sigTT, "S")
+    g_q2_sigtt_fit = ROOT.TGraph2DErrors()
+    for i in range(g_sigtt_fit.GetN()):
+        #g_q2_sigtt_fit.SetPoint(g_q2_sigtt_fit.GetN(), q2_vec[i], g_sigtt_fit.GetX()[i], g_sigtt_fit.GetY()[i])
+        #g_q2_sigtt_fit.SetPointError(g_q2_sigtt_fit.GetN()-1, 0.0, g_sigtt_fit.GetEX()[i], g_sigtt_fit.GetEY()[i])
+        g_q2_sigtt_fit.SetPoint(g_q2_sigtt_fit.GetN(), g_sigtt_fit.GetX()[i], g_sigtt_fit.GetY()[i], q2_vec[i])
+        g_q2_sigtt_fit.SetPointError(g_q2_sigtt_fit.GetN()-1, g_sigtt_fit.GetEX()[i], g_sigtt_fit.GetEY()[i], 0.0)
+    
+    g_q2_sigtt_fit.Fit(f_sigTT, "S")
         
     for i in range(len(w_vec)):
         
