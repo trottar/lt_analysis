8fe2f386830c560c1f5b14e9e3f02c1783a30448
Richard Trotta <trotta@cua.edu>
2024-01-28 04:38:29 -0500

    Updating param files to make Q2/W dependent.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-28 04:37:15 trottar"
+# Time-stamp: "2024-01-28 04:38:27 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -358,6 +358,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     for i in range(len(w_vec)):
         g_q2_sigl_fit.SetPoint(g_q2_sigl_fit.GetN(), q2_vec[i], g_sigl_fit.GetX()[i], g_sigl_fit.GetY()[i])
         g_q2_sigl_fit.SetPointError(g_q2_sigl_fit.GetN()-1, g_sigl_fit.GetEX()[i], 0.0, g_sigl_fit.GetEY()[i])
+        print("&&&&",g_q2_sigl_fit.GetN(), q2_vec[i], g_sigl_fit.GetX()[i], g_sigl_fit.GetY()[i])
         print((l0+l1*math.log(q2_vec[i])) * math.exp((l2+l3*math.log(q2_vec[i])) * (abs(g_sigl_fit.GetX()[i]))), " =!= ", g_sigl_fit.GetY()[i])
         sigl_X = (f_sigL.Eval(g_sigl.GetX()[i], q2_vec[i])) * g_vec[i]
         g_sigl_fit_tot.SetPoint(i, g_sigl.GetX()[i], sigl_X)
