2f6db8e4690a84bd0e615909aefa2e8b51097292
Richard Trotta <trotta@cua.edu>
2024-01-27 23:32:32 -0500

    Updating param files to make Q2/W dependent.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-27 23:31:21 trottar"
+# Time-stamp: "2024-01-27 23:32:27 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -182,8 +182,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set):
     t_tmin_map = TGraph()
     # Use SetPoint to fill the graph
     for i in range(nsep.GetSelectedRows()):
-        if nsep.GetV1()[i] > nsep.GetV2()[i]:
-            print("ERROR: t < tmin!\n\tt={} < tmin={}".format(nsep.GetV1()[i], nsep.GetV2()[i]))
+        if nsep.GetV1()[i] < nsep.GetV2()[i]:
+            print("ERROR: t_bin {}: t < tmin!\n\tt={} < tmin={}".format(i+1, nsep.GetV1()[i], nsep.GetV2()[i]))
             sys.exit(2)
         t_tmin_map.SetPoint(i, nsep.GetV1()[i], nsep.GetV2()[i])
 
