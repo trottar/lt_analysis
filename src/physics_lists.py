2846dabc3812fa3ff09b73ceb45727731ff67db5
Richard Trotta <trotta@cua.edu>
2024-01-29 14:54:52 -0500

    Updating param files to make Q2/W dependent.

--- src/physics_lists.py
+++ src/physics_lists.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-29 14:54:37 trottar"
+# Time-stamp: "2024-01-29 14:54:50 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -423,6 +423,7 @@ def create_lists(aveDict, ratioDict, histlist, inpDict, phisetlist, output_file_
             for i, line in enumerate(lines):
                 if check_kin in line:
                     print("\n\n!!!!!!!!!",check_line)
+                    sys.exit(2)
                     inLine = True
                     replace_line(f_list_settings, i+1, check_line)
             if not inLine:
