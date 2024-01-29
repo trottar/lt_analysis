ae01f8753f787792c3540f6b5990391adf9e8217
Richard Trotta <trotta@cua.edu>
2024-01-29 14:15:37 -0500

    Updating param files to make Q2/W dependent.

--- src/physics_lists.py
+++ src/physics_lists.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-01-29 14:05:46 trottar"
+# Time-stamp: "2024-01-29 14:15:17 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -293,45 +293,45 @@ def create_lists(aveDict, ratioDict, histlist, inpDict, phisetlist, output_file_
                         
     ################################################################################################################################################
 
-    f_list_settings = '{}/src/{}/beam/Eb_KLT.dat'.format(LTANAPATH, ParticleType)
-    output_file_lst.append(f_list_settings.split('/src/')[1])
+    f_beam = '{}/src/{}/beam/Eb_KLT.dat'.format(LTANAPATH, ParticleType)
+    output_file_lst.append(f_beam.split('/src/')[1])
     # Checks if file exists and creates if not
-    if not os.path.exists(f_list_settings):
-        open(f_list_settings, "w").close()
+    if not os.path.exists(f_beam):
+        open(f_beam, "w").close()
 
     if float(runNumRight[0]) != 0:
         # First check if line exists
-        with open(f_list_settings, 'r') as f:
+        with open(f_beam, 'r') as f:
             lines = f.readlines()
             try:
                 check_line = "{:.3f} {} {} {:.4f}\n".format(ebeam_right, Q2, W, EPSVAL)
                 # Check if the line already exists
                 if check_line not in lines:
-                    write_to_file(f_list_settings,check_line)
+                    write_to_file(f_beam,check_line)
             except ValueError:
                 print("Error: Value not float...",type(ebeam_right), type(Q2), type(W), type(EPSVAL))
 
     if float(runNumLeft[0]) != 0:
         # First check if line exists
-        with open(f_list_settings, 'r') as f:
+        with open(f_beam, 'r') as f:
             lines = f.readlines()
             try:
                 check_line = "{:.3f} {} {} {:.4f}\n".format(ebeam_left, Q2, W, EPSVAL)
                 # Check if the line already exists
                 if check_line not in lines:
-                    write_to_file(f_list_settings,check_line)
+                    write_to_file(f_beam,check_line)
             except ValueError:
                 print("Error: Value not float...",type(ebeam_left), type(Q2), type(W), type(EPSVAL))
 
     if float(runNumCenter[0]) != 0:
         # First check if line exists
-        with open(f_list_settings, 'r') as f:
+        with open(f_beam, 'r') as f:
             lines = f.readlines()
             try:
                 check_line = "{:.3f} {} {} {:.4f}\n".format(ebeam_center, Q2, W, EPSVAL)
                 # Check if the line already exists
                 if check_line not in lines:
-                    write_to_file(f_list_settings,check_line)
+                    write_to_file(f_beam,check_line)
             except ValueError:
                 print("Error: Value not float...",type(ebeam_center), type(Q2), type(W), type(EPSVAL))
                 
@@ -456,107 +456,107 @@ def create_lists(aveDict, ratioDict, histlist, inpDict, phisetlist, output_file_
     ################################################################################################################################################
 
     if float(runNumRight[0]) != 0:
-        f_list = '{}/src/{}/kindata/kindata.{}_Q{}W{}_{:.0f}_-{}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100, int(thpq_right*1000))
-        output_file_lst.append(f_list.split('/src/')[1])
+        f_kindata = '{}/src/{}/kindata/kindata.{}_Q{}W{}_{:.0f}_-{}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100, int(thpq_right*1000))
+        output_file_lst.append(f_kindata.split('/src/')[1])
         # Open the file in write mode, which creates a new empty file or overwrites the existing one
-        open(f_list, "w").close()
+        open(f_kindata, "w").close()
 
         processed_Q2vals = set()
         # Open a file in read mode
-        with open(f_list, 'r') as f:
+        with open(f_kindata, 'r') as f:
             for i, Q2val in enumerate(averQ2_right_data):
                 if Q2val in processed_Q2vals:
                     continue
                 check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_right_data[i], 0.001, averW_right_data[i], 0.001, avert_right_data[i], 0.001)
-                write_to_file(f_list,check_line)
+                write_to_file(f_kindata,check_line)
                 processed_Q2vals.add(Q2val)
 
     if float(runNumLeft[0]) != 0:
-        f_list = '{}/src/{}/kindata/kindata.{}_Q{}W{}_{:.0f}_+{}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100, int(thpq_left*1000))
-        output_file_lst.append(f_list.split('/src/')[1])
+        f_kindata = '{}/src/{}/kindata/kindata.{}_Q{}W{}_{:.0f}_+{}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100, int(thpq_left*1000))
+        output_file_lst.append(f_kindata.split('/src/')[1])
         # Open the file in write mode, which creates a new empty file or overwrites the existing one
-        open(f_list, "w").close()
+        open(f_kindata, "w").close()
 
         processed_Q2vals = set()
         # Open a file in read mode
-        with open(f_list, 'r') as f:
+        with open(f_kindata, 'r') as f:
             for i, Q2val in enumerate(averQ2_left_data):
                 if Q2val in processed_Q2vals:
                     continue
                 check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_left_data[i], 0.001, averW_left_data[i], 0.001, avert_left_data[i], 0.001)
-                write_to_file(f_list,check_line)
+                write_to_file(f_kindata,check_line)
                 processed_Q2vals.add(Q2val)
 
     if float(runNumCenter[0]) != 0:
-        f_list = '{}/src/{}/kindata/kindata.{}_Q{}W{}_{:.0f}_+0000.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
-        output_file_lst.append(f_list.split('/src/')[1])
+        f_kindata = '{}/src/{}/kindata/kindata.{}_Q{}W{}_{:.0f}_+0000.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
+        output_file_lst.append(f_kindata.split('/src/')[1])
         # Open the file in write mode, which creates a new empty file or overwrites the existing one
-        open(f_list, "w").close()
+        open(f_kindata, "w").close()
 
         processed_Q2vals = set()
         # Open a file in read mode
-        with open(f_list, 'r') as f:
+        with open(f_kindata, 'r') as f:
             for i, Q2val in enumerate(averQ2_center_data):
                 if Q2val in processed_Q2vals:
                     continue
                 check_line = "{:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}\n".format(averQ2_center_data[i], 0.001, averW_center_data[i], 0.001, avert_center_data[i], 0.001)
-                write_to_file(f_list,check_line)
+                write_to_file(f_kindata,check_line)
                 processed_Q2vals.add(Q2val)
                 
     ################################################################################################################################################
 
-    f_list = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
-    output_file_lst.append(f_list.split('/src/')[1])
+    f_aver = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
+    output_file_lst.append(f_aver.split('/src/')[1])
     # Open the file in write mode, which creates a new empty file or overwrites the existing one
-    open(f_list, "w").close()
+    open(f_aver, "w").close()
 
     if float(runNumRight[0]) != 0:        
         # Open a file in read mode
-        with open(f_list, 'r') as f:
+        with open(f_aver, 'r') as f:
             lines = f.readlines()
             for i, ratio in enumerate(ratio_right):
                 check_line = "{:.4f} {:.4f} {} {}\n".format(ratio, 0.001, int(phibin_right[i]), int(tbin_right[i]))
                 # Check if the line already exists
                 if check_line not in lines:
-                    write_to_file(f_list,check_line)
+                    write_to_file(f_aver,check_line)
 
-    f_list = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
-    output_file_lst.append(f_list.split('/src/')[1])
+    f_aver = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
+    output_file_lst.append(f_aver.split('/src/')[1])
     # Open the file in write mode, which creates a new empty file or overwrites the existing one
-    open(f_list, "w").close()
+    open(f_aver, "w").close()
 
     if float(runNumLeft[0]) != 0:        
         # Open a file in read mode
-        with open(f_list, 'r') as f:
+        with open(f_aver, 'r') as f:
             lines = f.readlines()
             for i, ratio in enumerate(ratio_left):
                 check_line = "{:.4f} {:.4f} {} {}\n".format(ratio, 0.001, int(phibin_left[i]), int(tbin_left[i]))
                 # Check if the line already exists
                 if check_line not in lines:
-                    write_to_file(f_list,check_line)
+                    write_to_file(f_aver,check_line)
 
-    f_list = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
-    output_file_lst.append(f_list.split('/src/')[1])
+    f_aver = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
+    output_file_lst.append(f_aver.split('/src/')[1])
     # Open the file in write mode, which creates a new empty file or overwrites the existing one
-    open(f_list, "w").close()
+    open(f_aver, "w").close()
 
     if float(runNumCenter[0]) != 0:        
         # Open a file in read mode
-        with open(f_list, 'r') as f:
+        with open(f_aver, 'r') as f:
             lines = f.readlines()
             for i, ratio in enumerate(ratio_center):
                 check_line = "{:.4f} {:.4f} {} {}\n".format(ratio, 0.001, int(phibin_center[i]), int(tbin_center[i]))
                 # Check if the line already exists
                 if check_line not in lines:
-                    write_to_file(f_list,check_line)
+                    write_to_file(f_aver,check_line)
                     
     # Check if the last character is a newline and remove if so
-    with open(f_list, "rb") as f:
+    with open(f_aver, "rb") as f:
         f.seek(-1, os.SEEK_END)
         last_char = f.read(1)
         if last_char == b'\n':
             # Remove the final line
-            with open(f_list, "rb+") as f:
+            with open(f_aver, "rb+") as f:
                 f.seek(-1, os.SEEK_END)
                 f.truncate()                    
     ################################################################################################################################################
