8f2cdd91961f8ef683d1bac7384e41e26e8bce9d
Richard Trotta <trotta@cua.edu>
2024-02-05 12:07:56 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/plotting/plot_xsects.py
+++ src/plotting/plot_xsects.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-05 12:02:48 trottar"
+# Time-stamp: "2024-02-05 12:05:30 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -673,8 +673,8 @@ for k in range(NumtBins):
     
     G_xmod_phi_loeps = TGraph()
     for i in range(NumtBins*NumPhiBins):
-        if np.array(file_df_dict['aver_loeps']['tbin'].tolist())[i] == (k+1):
-            G_xmod_phi_loeps.SetPoint(i, np.array(file_df_dict['aver_loeps']['phibin'].tolist())[i], np.array(file_df_dict['unsep_file_loeps']['x_mod'].tolist())[i])
+        if np.array(file_df_dict['unsep_file_loeps']['t'].tolist())[i-1] == np.array(file_df_dict['unsep_file_loeps']['t'].tolist())[i]:
+            G_xmod_phi_loeps.SetPoint(i, np.array(file_df_dict['unsep_file_loeps']['phi'].tolist())[i], np.array(file_df_dict['unsep_file_loeps']['x_mod'].tolist())[i])
     G_xmod_phi_loeps.SetMarkerStyle(30)
     G_xmod_phi_loeps.SetMarkerSize(1)
     G_xmod_phi_loeps.SetMarkerColor(1)
