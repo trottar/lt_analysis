eaa4e75db6a663dc6d5c5be78797fe43807ac7b4
Richard Trotta <trotta@cua.edu>
2024-02-05 14:32:55 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/plotting/plot_xsects.py
+++ src/plotting/plot_xsects.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-05 14:32:19 trottar"
+# Time-stamp: "2024-02-05 14:32:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -739,26 +739,6 @@ with PdfPages(outputpdf) as pdf:
         pdf.savefig(fig, bbox_inches='tight')
 
     # Create a figure and axis objects
-    fig, axes = plt.subplots(1, 1, figsize=(8, 6), sharex=True)
-
-    # Loop through t bins and plot data
-    for k in range(NumtBins):
-        ax = axes
-        ax.set_title("t = {:.2f}".format(t_bin_centers[k]))
-
-        for i, df_key in enumerate(['unsep_file_loeps', 'unsep_file_hieps']):
-            df = file_df_dict[df_key]
-            mask =  (df['t'][k*NumPhiBins+int(i/NumPhiBins)] == df['t'])
-            ax.scatter(df['t'][mask], df['Q2'][mask], marker=markers[mask], linestyle='None', label=df_key)
-
-        ax.set_xlabel('t')
-        ax.set_ylabel('$Q^2$')
-        ax.legend()
-
-    plt.tight_layout(rect=[0, 0, 1, 0.96])
-    pdf.savefig(fig, bbox_inches='tight')
-
-    # Create a figure and axis objects
     fig, axes = plt.subplots(NumtBins, 1, figsize=(8, 6 * NumtBins), sharex=True)
 
     # Loop through t bins and plot data
@@ -769,7 +749,7 @@ with PdfPages(outputpdf) as pdf:
         for i, df_key in enumerate(['unsep_file_loeps', 'unsep_file_hieps']):
             df = file_df_dict[df_key]
             mask =  (df['t'][k*NumPhiBins+int(i/NumPhiBins)] == df['t'])
-            ax.scatter(df['t'][mask], df['Q2'][mask], marker=markers[i], linestyle='None', label=df_key)
+            ax.scatter(df['phi'][mask], df['Q2'][mask], marker=markers[i], linestyle='None', label=df_key)
 
         ax.set_xlabel('$\phi$')
         ax.set_ylabel('$Q^2$')
