5bf15fe297d05fba8330558c9484b962d49518ea
Richard Trotta <trotta@cua.edu>
2024-02-04 19:37:22 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-04 19:36:14 trottar"
+# Time-stamp: "2024-02-04 19:37:20 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -265,7 +265,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigL.SetLineWidth(2)
 
     # Draw f_sigL
-    #f_sigL.Draw("same")
+    f_sigL.Draw("same")
         
     # Check the fit status for 'f_sigL'
     f_sigL_status = f_sigL.GetNDF()  # GetNDF() returns the number of degrees of freedom
@@ -473,7 +473,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigLT.SetLineWidth(2)
 
     # Draw f_sigLT
-    f_sigLT.Draw("same")
+    #f_sigLT.Draw("same")
         
     # Check the fit status for 'f_sigLT'
     f_sigLT_status = f_sigLT.GetNDF()  # GetNDF() returns the number of degrees of freedom
