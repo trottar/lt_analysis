7bab9035f0cef16245f0c89f716d19b13d6d015b
Richard Trotta <trotta@cua.edu>
2024-02-05 19:26:24 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-05 19:25:29 trottar"
+# Time-stamp: "2024-02-05 19:26:22 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -260,7 +260,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
         print("$$$$$$$$$$$",i, g_sigl.GetX()[i], sigl_X)
     g_q2_sigl_fit.Fit(f_sigL, "SQ")
 
-    f12 = ROOT.TF12("f12",f_sigL,2.115,"y");
+    f12 = ROOT.TF12("f12",f_sigL,2.115,"x");
     
     # Set line properties for f_sigL
     f12.SetLineColor(1)
