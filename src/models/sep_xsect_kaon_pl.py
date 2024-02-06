342965ec2fe663323b6fccefe3f32ca74a9091c5
Richard Trotta <trotta@cua.edu>
2024-02-05 20:29:34 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-05 18:44:28 trottar"
+# Time-stamp: "2024-02-05 20:29:21 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -70,7 +70,7 @@ def import_model(inp_model, arg_str):
     }
 
     sig = modelDict[inp_model]
-    #sig = sig*g
+    sig = sig*g
     sig = sig/2./PI/1e6
 
     print("Model {} = {:.4e}".format(inp_model, sig))
