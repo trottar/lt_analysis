de5ddcb58b0eecae8d8e99bf39da5bb656e38777
Richard Trotta <trotta@cua.edu>
2024-02-05 21:08:06 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-05 21:05:49 trottar"
+# Time-stamp: "2024-02-05 21:08:04 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -323,8 +323,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
         sigt_X_pre = (f_sigT_pre.Eval(g_sigt.GetX()[i], q2_vec[i])) * (g_vec[i]/2/PI/1e6)
         g_sigt_prv.SetPoint(i, g_sigt.GetX()[i], sigt_X_pre)
 
-        sigt_X_fit = (g_sigt.GetY()[i])
-        sigt_X_fit_err = g_sigt.GetEY()[i]
+        sigt_X_fit = (g_sigt.GetY()[i]) / (g_vec[i]/2/PI/1e6)
+        sigt_X_fit_err = g_sigt.GetEY()[i] / (g_vec[i]/2/PI/1e6)
 
         g_sigt_fit.SetPoint(i, g_sigt.GetX()[i], sigt_X_fit)
         g_sigt_fit.SetPointError(i, 0, sigt_X_fit_err)
@@ -423,8 +423,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
         siglt_X_pre = (f_sigLT_pre.Eval(g_siglt.GetX()[i], q2_vec[i]) * math.sin(th_vec[i] * PI / 180)) * (g_vec[i]/2/PI/1e6)
         g_siglt_prv.SetPoint(i, g_sigl.GetX()[i], siglt_X_pre)
 
-        siglt_X_fit = g_siglt.GetY()[i]
-        siglt_X_fit_err = g_siglt.GetEY()[i]
+        siglt_X_fit = g_siglt.GetY()[i] / (g_vec[i]/2/PI/1e6)
+        siglt_X_fit_err = g_siglt.GetEY()[i] / (g_vec[i]/2/PI/1e6)
 
         g_siglt_fit.SetPoint(i, g_siglt.GetX()[i], siglt_X_fit)
         g_siglt_fit.SetPointError(i, 0, siglt_X_fit_err)
@@ -528,8 +528,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
         g_sigtt_prv.SetPoint(i, nsep.GetV2()[i], sigtt_X_pre)
 
-        sigtt_X_fit = g_sigtt.GetY()[i]
-        sigtt_X_fit_err = g_sigtt.GetEY()[i]
+        sigtt_X_fit = g_sigtt.GetY()[i] / (g_vec[i]/2/PI/1e6)
+        sigtt_X_fit_err = g_sigtt.GetEY()[i] / (g_vec[i]/2/PI/1e6)
 
         g_sigtt_fit.SetPoint(i, g_sigtt.GetX()[i], sigtt_X_fit)
         g_sigtt_fit.SetPointError(i, 0, sigtt_X_fit_err)
