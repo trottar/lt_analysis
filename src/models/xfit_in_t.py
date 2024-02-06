f67e02e29249b4a8f9141955c71f3b6f9e07005f
Richard Trotta <trotta@cua.edu>
2024-02-05 19:18:12 -0500

    Reverting sig changes back from GeV^2 to MeV^2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-05 19:16:28 trottar"
+# Time-stamp: "2024-02-05 19:18:10 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -246,7 +246,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     c2.cd(1)
     g_sigl_fit.SetTitle("Sigma L Model Fit")
-    #g_sigl_fit.Draw("A*")
+    g_sigl_fit.Draw("A*")
 
     f_sigL = TF2("sig_L", fun_Sig_L, tmin_range, tmax_range, lo_bound, hi_bound, 4)
     f_sigL.SetParameters(l0, l1, l2, l3)
@@ -259,10 +259,12 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
         g_sigl_fit_tot.SetPoint(i, g_sigl.GetX()[i], sigl_X)
         print("$$$$$$$$$$$",i, g_sigl.GetX()[i], sigl_X)
     g_q2_sigl_fit.Fit(f_sigL, "SQ")
+
+    c3 = TCanvas("c3", "c3", 800, 800)
     
     # Set line properties for f_sigL
-    f_sigL.SetLineColor(1)
-    f_sigL.SetLineWidth(2)
+    #f_sigL.SetLineColor(1)
+    #f_sigL.SetLineWidth(2)
 
     # Draw f_sigL
     f_sigL.Draw()
