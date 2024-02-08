78cc34063645229697266d3a9606d55e83142878
Richard Trotta <trotta@cua.edu>
2024-02-08 16:51:00 -0500

    Added conversion to ub/GeV^2 to unsep xsect calculation

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-08 16:49:47 trottar"
+# Time-stamp: "2024-02-08 16:50:51 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -70,9 +70,6 @@ def import_model(inp_model, arg_str):
     }
 
     sig = modelDict[inp_model]
-    sig = sig*g
-    #sig = sig/2./PI # ub/GeV^2
-    #sig = sig/2./PI/1e6 # ub/MeV^2
 
     print("Model {} = {:.4e}".format(inp_model, sig))
     
--- src/plotting/plot_xsects.py
+++ src/plotting/plot_xsects.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-08 16:48:31 trottar"
+# Time-stamp: "2024-02-08 16:50:58 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -531,7 +531,7 @@ with PdfPages(outputpdf) as pdf:
             ax.errorbar(df['t'], df['{}'.format(sig)], yerr=df['d{}'.format(sig)], marker=markers[i], linestyle='None', label='Data', color=colors[i])
         ax.set_xlabel('t')
         ax.set_ylabel("${}$".format(formatted_sig))
-        #ax.set_xlim(tmin, tmax)
+        ax.set_xlim(tmin, tmax)
         ax.legend()
         # Add grid to subplot
         ax.grid(True, linestyle='--', linewidth=0.5)
