70b56c6a9ea4e123eaa8cf551277de12c61faf8c
Richard Trotta <trotta@cua.edu>
2024-02-08 16:14:29 -0500

    Added conversion to ub/GeV^2 to unsep xsect calculation

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-08 16:07:56 trottar"
+# Time-stamp: "2024-02-08 16:14:20 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -71,8 +71,8 @@ def import_model(inp_model, arg_str):
 
     sig = modelDict[inp_model]
     sig = sig*g
-    sig = sig/2./PI # ub/GeV^2
-    #sig = sig/2./PI/1e6 # ub/MeV^2
+    #sig = sig/2./PI # ub/GeV^2
+    sig = sig/2./PI/1e6 # ub/MeV^2
 
     print("Model {} = {:.4e}".format(inp_model, sig))
     
