5e27803f4d2d3324256a34739f1ca455aa42a9f4
Richard Trotta <trotta@cua.edu>
2024-02-09 03:37:53 -0500

    Added conversion to ub/GeV^2 to unsep xsect calculation

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-09 03:36:01 trottar"
+# Time-stamp: "2024-02-09 03:37:47 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -507,7 +507,8 @@ def single_setting(q2_set, fn_lo, fn_hi):
         # Set properties for the TMultiGraph
         #g.GetHistogram().SetMinimum(0.0)
         #g.GetHistogram().SetMaximum(0.8)
-        g.SetTitle("t = {:.4f}".format(t_list[i]))
+        g.SetTitle("t = {:.3f}".format(t_list[i]))
+        g.SetTitleOffset(1.4)
         g.GetYaxis().SetTitle("Unseparated Cross Section [#mub/GeV^{2}]")
         g.GetYaxis().CenterTitle()
         g.GetYaxis().SetTitleOffset(1.4)
