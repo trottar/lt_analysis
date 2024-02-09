ec9418f3054df29954f851b9c807cde1efa056c2
Richard Trotta <trotta@cua.edu>
2024-02-09 16:51:33 -0500

    Added conversion to ub/GeV^2 to unsep xsect calculation

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-09 16:49:48 trottar"
+# Time-stamp: "2024-02-09 16:51:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -783,7 +783,7 @@ def grab_yield_data(prev_root_file, histlist, inpDict):
         for j in range(len(t_bins) - 1):
             for k in range(len(phi_bins) - 1):
                 binned_sub_data = get_histogram(prev_root_file, \
-                                                "{}/yield".format(hist["phi_setting"]), "H_yield_DATA".format(hist["phi_setting"]))
+                                                "{}/yield".format(hist["phi_setting"]), "H_yield_DATA_{}".format(hist["phi_setting"]))
                 yield_val = binned_sub_data.GetBinContent(i)
                 print("Data yield for t-bin {} phi-bin {}: {:.3f}".format(j+1, k+1, yield_val))
                 i+=1
