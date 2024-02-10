36bb93eb48159d51683ae2afdcdf786f880ae1d5
Richard Trotta <trotta@cua.edu>
2024-02-10 17:28:10 -0500

    Added conversion to ub/GeV^2 to unsep xsect calculation

--- src/binning/calculate_ratio.py
+++ src/binning/calculate_ratio.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-09 17:44:35 trottar"
+# Time-stamp: "2024-02-10 17:28:03 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -64,18 +64,22 @@ def calculate_ratio(kin_type, phiset, yieldDict):
         # Fill histogram
         yield_data = data_nested_dict[kin_type][data_key_tuple]["{}".format(kin_type)][1]
         yield_simc = simc_nested_dict[kin_type][simc_key_tuple]["{}".format(kin_type)][1]
+        yield_err_data = data_nested_dict[kin_type][data_key_tuple]["{}_err".format(kin_type)][1]
+        yield_err_simc = simc_nested_dict[kin_type][simc_key_tuple]["{}_err".format(kin_type)][1]        
         #print("\nYield_data: {}".format(yield_data))
         #print("Yield_simc: {}".format(yield_simc))
         try:
             ratio = yield_data/yield_simc
         except ZeroDivisionError:
             ratio = 0.0
+        # Calculate ratio error in quadrature (%)
+        ratio_err = np.sqrt(yield_err_data**2+yield_err_simc**2)
         if math.isnan(ratio):
             ratio = 0.0
         if math.isinf(ratio):
             ratio = 0.0
-        print("Ratio for t-bin {} phi-bin {}: {:.3f}".format(i+1, j+1, ratio))
-        dict_lst.append((i, j, ratio))
+        print("Ratio for t-bin {} phi-bin {}: {:.3e} +/- {:.3e}".format(i+1, j+1, ratio, (ratio_err/100)*ratio))
+        dict_lst.append((i, j, ratio, ratio_err))
     
     # Group the tuples by the first two elements using defaultdict
     groups = defaultdict(list)
@@ -83,6 +87,7 @@ def calculate_ratio(kin_type, phiset, yieldDict):
         key = (tup[0], tup[1])
         groups[key] = {
             "ratio".format(kin_type) : tup[2],
+            "ratio_err".format(kin_type) : tup[3],
         }            
             
     return groups
