8336ce3386425ddb4b67fee919fe1ba1a5cacb3b
Richard Trotta <trotta@cua.edu>
2024-02-11 12:47:20 -0500

    Ali and I have gone through and updated all statistical error calculations and implimented them.

--- src/physics_lists.py
+++ src/physics_lists.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-10 23:08:22 trottar"
+# Time-stamp: "2024-02-11 12:47:17 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -477,6 +477,11 @@ def create_lists(aveDict, ratioDict, histlist, inpDict, phisetlist, output_file_
                 if check_line not in lines:
                     write_to_file(f_aver,check_line)
 
+    f_aver = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
+    output_file_lst.append(f_aver.split('/src/')[1])
+    # Open the file in write mode, which creates a new empty file or overwrites the existing one
+    open(f_aver, "w").close()
+                    
     if float(runNumLeft[0]) != 0:
         # Open a file in read mode
         with open(f_aver, 'r') as f:
@@ -487,6 +492,11 @@ def create_lists(aveDict, ratioDict, histlist, inpDict, phisetlist, output_file_
                 if check_line not in lines:
                     write_to_file(f_aver,check_line)
 
+    f_aver = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, ParticleType, polID, Qs.replace("p",""), Ws.replace("p",""), float(EPSVAL)*100)
+    output_file_lst.append(f_aver.split('/src/')[1])
+    # Open the file in write mode, which creates a new empty file or overwrites the existing one
+    open(f_aver, "w").close()
+                    
     if float(runNumCenter[0]) != 0:
         # Open a file in read mode
         with open(f_aver, 'r') as f:
