Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

src/setup/calcTotalEffectiveCharge.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-11 01:14:21 trottar"
+# Time-stamp: "2024-02-12 17:34:44 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -39,8 +39,8 @@ LTANAPATH=lt.LTANAPATH
 # Sum of all the effective charge per run
 tot_eff_charge = np.sum(runs_eff_charge)
 
-# Normalized uncertainty (converted to %)
-tot_eff_charge_err = np.sqrt((np.sum([err**2 for err in runs_eff_charge_err])/tot_eff_charge**2))*100
+# Normalized uncertainty (relative error)
+tot_eff_charge_err = np.sqrt((np.sum([err**2 for err in runs_eff_charge_err])/tot_eff_charge**2))
 
 BashInput=("{:.3f}\n{:.5f}".format(tot_eff_charge, tot_eff_charge_err))
 print(BashInput)

src/setup/getDataTable.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-11 00:42:53 trottar"
+# Time-stamp: "2024-02-12 17:34:10 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -319,8 +319,8 @@ def calculate_eff_charge_err(runNum,efficiency_table):
     # Calculate run by run total efficiency error
     # Error propagation by addition in quadrature
     d_eff = np.sqrt(sum((float(efferr)/float(eff))**2 for efferr,eff in zip(efficiency_errDict.values(),effDict.values())))
-    d_charge = 2e-6 # 200 nC
-    # Error propagation by addition in quadrature
+    d_charge = 2e-6 # 200 nC, FIX!! NEED TO CHECK WITH DAVE MACK WRITE UP!!
+    # Error propagation by addition in quadrature (units of mC)
     eff_charge_err = np.sqrt((eff_charge**2)*(d_eff**2+d_charge**2))
     
     return eff_charge_err