5d0f472c16e172d772122e6b788da692c7d3bc44
Richard Trotta <trotta@cua.edu>
2024-02-12 20:31:49 -0500

    Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -91,13 +91,13 @@ else
     done
 fi
 
-#NumtBins=1
-#NumPhiBins=1
+NumtBins=1
+NumPhiBins=1
 #NumtBins=2
 #NumPhiBins=8
 # Q2=2p1
-NumtBins=3
-NumPhiBins=10
+#NumtBins=3
+#NumPhiBins=10
 # Q2=3p0, W=3p14
 #NumtBins=4
 #NumPhiBins=12
