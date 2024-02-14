d3f47d08bf1387f762668ad1c4760cbc463ece5c
Richard Trotta <trotta@cua.edu>
2024-02-14 18:13:07 -0500

    Updated simc input files for Q2=2p1. Increased number of events from 50k to 500k

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -662,7 +662,7 @@ done
 if [[ $Q2 = "2p1" && $W = "2p95" ]]; then
     # Q2=2p1
     NumtBins=3
-    NumPhiBins=10
+    NumPhiBins=8
     TMIN=0.001
     TMAX=0.600
 elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
