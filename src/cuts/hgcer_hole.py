5c2c03b7d3e89efcfa6eb3700a1a4cdcf9924fb2
Richard Trotta <trotta@cua.edu>
2024-02-13 23:08:40 -0500

    Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

--- src/cuts/hgcer_hole.py
+++ src/cuts/hgcer_hole.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-13 23:05:55 trottar"
+# Time-stamp: "2024-02-13 23:08:39 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -33,13 +33,13 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET, simc=False):
     cutg.SetPoint(10, 0, 25.5)
     cutg.SetPoint(11, 0, 5.5)
     cutg.SetPoint(12, 1, 4)
-    cutg.SetPoint(13, -, 31)
-    cutg.SetPoint(14, -, 22)
-    cutg.SetPoint(15, -, 12.3)
-    cutg.SetPoint(16, -, 01.5)
-    cutg.SetPoint(17, -, -11)
-    cutg.SetPoint(18, 0, -2.5)
-    cutg.SetPoint(19, 0, -25.5)
+    cutg.SetPoint(13, -1, 3)
+    cutg.SetPoint(14, -2, 2)
+    cutg.SetPoint(15, -2.3, 1)
+    cutg.SetPoint(16, -1.5, 0)
+    cutg.SetPoint(17, -1, -1)
+    cutg.SetPoint(18, 0.5, -2)
+    cutg.SetPoint(19, 0.5, -25)
     cutg.SetPoint(20, 2, -25)
 
     '''
