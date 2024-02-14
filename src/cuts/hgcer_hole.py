fd86a9958b3726fa8020eadaea8439cd7b5d5534
Richard Trotta <trotta@cua.edu>
2024-02-13 23:05:56 -0500

    Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

--- src/cuts/hgcer_hole.py
+++ src/cuts/hgcer_hole.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-13 23:03:59 trottar"
+# Time-stamp: "2024-02-13 23:05:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -20,27 +20,27 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET, simc=False):
     cutg = TCutG("cutg",21)
     cutg.SetVarX("P_hgcer_xAtCer")
     cutg.SetVarY("P_hgcer_yAtCer")
-    cutg.SetPoint(0,-25,2)
-    cutg.SetPoint(1,-2,2)
-    cutg.SetPoint(2,-1,2.5)
-    cutg.SetPoint(3,0,3)
-    cutg.SetPoint(4,1,3)
-    cutg.SetPoint(5,2,3.3)
-    cutg.SetPoint(6,3,3.0)
-    cutg.SetPoint(7,4,2.5)
-    cutg.SetPoint(8,5,2)
-    cutg.SetPoint(9,25,2)
-    cutg.SetPoint(10,25,0.5)
-    cutg.SetPoint(11,5,0.5)
-    cutg.SetPoint(12,4,1)
-    cutg.SetPoint(13,3,-1)
-    cutg.SetPoint(14,2,-2)
-    cutg.SetPoint(15,1,-2.3)
-    cutg.SetPoint(16,0,-1.5)
-    cutg.SetPoint(17,-1,-1)
-    cutg.SetPoint(18,-2,0.5)
-    cutg.SetPoint(19,-25,0.5)
-    cutg.SetPoint(20,-25,2)
+    cutg.SetPoint(0, 2, -25)
+    cutg.SetPoint(1, 2, -2)
+    cutg.SetPoint(2, 2, -1.5)
+    cutg.SetPoint(3, 3, 0)
+    cutg.SetPoint(4, 3, 1)
+    cutg.SetPoint(5, 3, 2.3)
+    cutg.SetPoint(6, 3, 3.0)
+    cutg.SetPoint(7, 2, 4.5)
+    cutg.SetPoint(8, 2, 5)
+    cutg.SetPoint(9, 2, 25)
+    cutg.SetPoint(10, 0, 25.5)
+    cutg.SetPoint(11, 0, 5.5)
+    cutg.SetPoint(12, 1, 4)
+    cutg.SetPoint(13, -, 31)
+    cutg.SetPoint(14, -, 22)
+    cutg.SetPoint(15, -, 12.3)
+    cutg.SetPoint(16, -, 01.5)
+    cutg.SetPoint(17, -, -11)
+    cutg.SetPoint(18, 0, -2.5)
+    cutg.SetPoint(19, 0, -25.5)
+    cutg.SetPoint(20, 2, -25)
 
     '''
     # Defined HGCer Geometric cuts
