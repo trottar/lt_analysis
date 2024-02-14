a8972e0e750f8d6ee80d3e62920b1027e84b12bb
Richard Trotta <trotta@cua.edu>
2024-02-13 22:42:26 -0500

    Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

--- src/cuts/hgcer_hole.py
+++ src/cuts/hgcer_hole.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-13 22:22:57 trottar"
+# Time-stamp: "2024-02-13 22:42:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -20,27 +20,27 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET, simc=False):
     cutg = TCutG("cutg",21)
     cutg.SetVarX("P_hgcer_yAtCer")
     cutg.SetVarY("P_hgcer_xAtCer")
-    cutg.SetPoint(0,-25,2+10)
-    cutg.SetPoint(1,-2,2+10)
-    cutg.SetPoint(2,-1,2.5+10)
-    cutg.SetPoint(3,0,3+10)
-    cutg.SetPoint(4,1,3+10)
-    cutg.SetPoint(5,2,3.3+10)
-    cutg.SetPoint(6,3,3.0+10)
-    cutg.SetPoint(7,4,2.5+10)
-    cutg.SetPoint(8,5,2+10)
-    cutg.SetPoint(9,25,2+10)
-    cutg.SetPoint(10,25,0.5+10)
-    cutg.SetPoint(11,5,0.5+10)
-    cutg.SetPoint(12,4,1+10)
-    cutg.SetPoint(13,3,-1+10)
-    cutg.SetPoint(14,2,-2+10)
-    cutg.SetPoint(15,1,-2.3+10)
-    cutg.SetPoint(16,0,-1.5+10)
-    cutg.SetPoint(17,-1,-1+10)
-    cutg.SetPoint(18,-2,0.5+10)
-    cutg.SetPoint(19,-25,0.5+10)
-    cutg.SetPoint(20,-25,2+10)
+    cutg.SetPoint(0,-25+10,2)
+    cutg.SetPoint(1,-2+10,2)
+    cutg.SetPoint(2,-1+10,2.5)
+    cutg.SetPoint(3,0+10,3)
+    cutg.SetPoint(4,1+10,3)
+    cutg.SetPoint(5,2+10,3.3)
+    cutg.SetPoint(6,3+10,3.0)
+    cutg.SetPoint(7,4+10,2.5)
+    cutg.SetPoint(8,5+10,2)
+    cutg.SetPoint(9,25+10,2)
+    cutg.SetPoint(10,25+10,0.5)
+    cutg.SetPoint(11,5+10,0.5)
+    cutg.SetPoint(12,4+10,1)
+    cutg.SetPoint(13,3+10,-1)
+    cutg.SetPoint(14,2+10,-2)
+    cutg.SetPoint(15,1+10,-2.3)
+    cutg.SetPoint(16,0+10,-1.5)
+    cutg.SetPoint(17,-1+10,-1)
+    cutg.SetPoint(18,-2+10,0.5)
+    cutg.SetPoint(19,-25+10,0.5)
+    cutg.SetPoint(20,-25+10,2)
 
     '''
     # Defined HGCer Geometric cuts
