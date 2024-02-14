e54ef29d7d7d973720c7a0ec7ca4587051f2d771
Richard Trotta <trotta@cua.edu>
2024-02-14 00:15:35 -0500

    Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

--- src/cuts/hgcer_hole.py
+++ src/cuts/hgcer_hole.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-13 23:59:53 trottar"
+# Time-stamp: "2024-02-14 00:15:33 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -15,6 +15,34 @@ from ROOT import TCutG
 
 def apply_HGCer_hole_cut(Q2, W, EPSSET, simc=False):
 
+    # Defined HGCer Geometric cuts
+    cutg = TCutG("cutg",21)
+    cutg.SetVarX("P_hgcer_xAtCer")
+    cutg.SetVarY("P_hgcer_yAtCer")
+    cutg.SetPoint(0,-25,2)
+    cutg.SetPoint(1,-2,2)
+    cutg.SetPoint(2,-1.5,2)
+    cutg.SetPoint(3,0,3)
+    cutg.SetPoint(4,1,3)
+    cutg.SetPoint(5,2.3,3)
+    cutg.SetPoint(6,3.0,3)
+    cutg.SetPoint(7,4.5,2)
+    cutg.SetPoint(8,5,2)
+    cutg.SetPoint(9,25,2)
+    cutg.SetPoint(10,25.5,0)
+    cutg.SetPoint(11,5.5,0)
+    cutg.SetPoint(12,4,1)
+    cutg.SetPoint(13,3,-1)
+    cutg.SetPoint(14,2,-2)
+    cutg.SetPoint(15,1,-2.3)
+    cutg.SetPoint(16,0,-1.5)
+    cutg.SetPoint(17,-1,-1)
+    cutg.SetPoint(18,-2,0.5)
+    cutg.SetPoint(19,-25,0.5)
+    cutg.SetPoint(20,-25,2)
+
+    
+    '''
     if "low" in EPSSET:
         # Defined HGCer Geometric cuts
         cutg = TCutG("cutg",21)
@@ -68,6 +96,7 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET, simc=False):
         cutg.SetPoint(18,-2,0.5)
         cutg.SetPoint(19,-25,0.5)
         cutg.SetPoint(20,-25,2)
+    '''
 
     '''
     # Defined HGCer Geometric cuts
