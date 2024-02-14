78299e291bb71d7c85aea623d4aad3f76b267711
Richard Trotta <trotta@cua.edu>
2024-02-14 00:23:37 -0500

    Wasn't actually finding ratio averages per bin, was accidentally just grabbing center ratio and using as average. Fixed this with average_ratio.f script implimentation.

--- src/cuts/hgcer_hole.py
+++ src/cuts/hgcer_hole.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-14 00:20:30 trottar"
+# Time-stamp: "2024-02-14 00:23:35 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -19,27 +19,27 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET, simc=False):
     cutg = TCutG("cutg",21)
     cutg.SetVarX("P_hgcer_xAtCer")
     cutg.SetVarY("P_hgcer_yAtCer")
-    cutg.SetPoint(0, 2+10, -25)
-    cutg.SetPoint(1, 2+10, -2)
-    cutg.SetPoint(2, 2+10, -1.5)
-    cutg.SetPoint(3, 3+10, 0)
-    cutg.SetPoint(4, 3+10, 1)
-    cutg.SetPoint(5, 3+10, 2.3)
-    cutg.SetPoint(6, 3+10, 3.0)
-    cutg.SetPoint(7, 2+10, 4.5)
-    cutg.SetPoint(8, 2+10, 5)
-    cutg.SetPoint(9, 2+10, 25)
-    cutg.SetPoint(10, 0+10, 25.5)
-    cutg.SetPoint(11, 0+10, 5.5)
-    cutg.SetPoint(12, 1+10, 4)
-    cutg.SetPoint(13, -1+10, 3)
-    cutg.SetPoint(14, -2+10, 2)
-    cutg.SetPoint(15, -2.3+10, 1)
-    cutg.SetPoint(16, -1.5+10, 0)
-    cutg.SetPoint(17, -1+10, -1)
-    cutg.SetPoint(18, 0.5+10, -2)
-    cutg.SetPoint(19, 0.5+10, -25)
-    cutg.SetPoint(20, 2+10, -25)    
+    cutg.SetPoint(0, 2+10, -25-1)
+    cutg.SetPoint(1, 2+10, -2-1)
+    cutg.SetPoint(2, 2+10, -1.5-1)
+    cutg.SetPoint(3, 3+10, 0-1)
+    cutg.SetPoint(4, 3+10, 1-1)
+    cutg.SetPoint(5, 3+10, 2.3-1)
+    cutg.SetPoint(6, 3+10, 3.0-1)
+    cutg.SetPoint(7, 2+10, 4.5-1)
+    cutg.SetPoint(8, 2+10, 5-1)
+    cutg.SetPoint(9, 2+10, 25-1)
+    cutg.SetPoint(10, 0+10, 25.5-1)
+    cutg.SetPoint(11, 0+10, 5.5-1)
+    cutg.SetPoint(12, 1+10, 4-1)
+    cutg.SetPoint(13, -1+10, 3-1)
+    cutg.SetPoint(14, -2+10, 2-1)
+    cutg.SetPoint(15, -2.3+10, 1-1)
+    cutg.SetPoint(16, -1.5+10, 0-1)
+    cutg.SetPoint(17, -1+10, -1-1)
+    cutg.SetPoint(18, 0.5+10, -2-1)
+    cutg.SetPoint(19, 0.5+10, -25-1)
+    cutg.SetPoint(20, 2+10, -25-1)
     
     '''
     if "low" in EPSSET:
