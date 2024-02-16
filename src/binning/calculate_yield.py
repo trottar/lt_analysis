f6a818f320becb24d6c6d2b030ed535cabee79a5
Richard Trotta <trotta@cua.edu>
2024-02-16 06:26:26 -0500

    Adjusting kaon sig model. Removing t dependence from sigT.

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-15 14:14:39 trottar"
+# Time-stamp: "2024-02-16 06:25:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -665,7 +665,7 @@ def calculate_yield_simc(kin_type, hist, t_bins, phi_bins, inpDict, iteration=Fa
             yld = total_count*normfac_simc
             # Calculate simc yield error (relative error)
             print("!!!!!!!!!!!!!",(1/np.sqrt(binned_unweighted_NumEvts_simc[i]))*normfac_simc)
-            yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))*normfac_simc
+            yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))*normfac_simc/100 # FIX (/100 is temporary)
         except ZeroDivisionError:
             yld = 0.0
             yld_err = 0.0
