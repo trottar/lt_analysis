bf6e38cdcbda27d14513fd0d3869183c4f826d46
Richard Trotta <trotta@cua.edu>
2024-02-19 17:02:47 -0500

    Updated Q2=0p5,3p0/W=2.32 simc input files for 500k events

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-19 17:01:03 trottar"
+# Time-stamp: "2024-02-19 17:02:45 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -31,7 +31,7 @@ def import_model(inp_model, arg_str):
     def sig_L(*par):
         if inp_model == "sigL":
             print("Calculating function for sigL...\nQ2={:.4e}, t={:.4e}\npar=({:.4e}, {:.4e}, {:.4e}, {:.4e})".format(qq, tt, *par))
-            f = (par[0]+par[1]*math.log(qq)) * math.exp((par[2]+par[3]*math.log(qq)))
+            f = (par[0]+par[1]*math.log(qq)) * math.exp((par[2]+par[3]*math.log(qq)) * (abs(tt)+0.2))
             return f
 
     # Function for SigT
