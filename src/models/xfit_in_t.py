bbef26119dd7d327a0990836daeaa14d4152ad89
Richard Trotta <trotta@cua.edu>
2024-02-21 03:26:54 -0500

    Updated parameterization for all Q2s for sigT

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-21 02:56:37 trottar"
+# Time-stamp: "2024-02-21 03:26:52 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -331,8 +331,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigT_pre.FixParameter(2, 0)
     f_sigT_pre.FixParameter(3, 0)
     # Fixing sigL terms for testing
-    f_sigT_pre.FixParameter(0, t0)
-    f_sigT_pre.FixParameter(1, t1)
+    f_sigT_pre.FixParameter(0, 15)
+    f_sigT_pre.FixParameter(1, -9)
     ##############
     ##############
     ##############    
@@ -389,8 +389,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigT.FixParameter(2, 0)
     f_sigT.FixParameter(3, 0)
     # Fixing sigL terms for testing
-    f_sigT.FixParameter(0, t0)
-    f_sigT.FixParameter(1, t1)
+    f_sigT.FixParameter(0, 15)
+    f_sigT.FixParameter(1, -9)
     ##############
     ##############
     ##############    
