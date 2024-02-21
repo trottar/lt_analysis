5ae5feb1257570bed73c190504097a6cee8845f0
Richard Trotta <trotta@cua.edu>
2024-02-21 01:51:27 -0500

    Updated parameterization for all Q2s for sigT

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-20 13:13:45 trottar"
+# Time-stamp: "2024-02-21 01:51:15 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -59,7 +59,7 @@ def fun_Sig_T(x, par):
     # RLT (2/20/2024): Added 1/Q^4 term to dampen sigT    
     #f = par[0]+par[1]*math.log(qq)+(par[2]+par[3]*math.log(qq)) * ftav
     #f = par[0]+par[1]*math.log(qq)
-    f = par[0]*math.log(qq)+par[1]/(qq**2)
+    f = par[0]*math.log(qq)+par[1]/(qq**4)
     return f
 
 ###############################################################################################################################################
