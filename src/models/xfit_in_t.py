cb38d8feb6102c4abc10fef44db0e446bfa79fdc
Richard Trotta <trotta@cua.edu>
2024-02-22 16:59:22 -0500

    Iterating models

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-22 15:55:37 trottar"
+# Time-stamp: "2024-02-22 16:58:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -331,7 +331,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigT_pre.FixParameter(2, 0)
     f_sigT_pre.FixParameter(3, 0)
     # Fixing sigL terms for testing
-    f_sigT_pre.FixParameter(0, 20)
+    f_sigT_pre.FixParameter(0, 120)
     f_sigT_pre.FixParameter(1, t1)
     ##############
     ##############
@@ -389,7 +389,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigT.FixParameter(2, 0)
     f_sigT.FixParameter(3, 0)
     # Fixing sigL terms for testing
-    f_sigT.FixParameter(0, 20)
+    f_sigT.FixParameter(0, 120)
     f_sigT.FixParameter(1, t1)
     ##############
     ##############
