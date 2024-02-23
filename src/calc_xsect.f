9d4fe29668f98a0c31e4adbb7cc3a57f654594b1
Richard Trotta <trotta@cua.edu>
2024-02-22 21:53:00 -0500

    Iterating models

--- src/calc_xsect.f
+++ src/calc_xsect.f
@@ -226,7 +226,8 @@ c angle check
             write(61,40) x_real,dx_real,x_mod,eps_mod,
      *           th_mod*180./3.14159,phi*180./3.14159,tm,tm_min,w,q2
  40         format(3G15.5,f8.5,2f7.2,4f8.5)
-            
+
+            print *,"--------------"            
             print *,"--------------"
             WRITE(*,*) 'phi (deg): ', phi*180./3.14159
             print *,'it',it
@@ -252,6 +253,7 @@ c angle check
             print *,"th_mod: ", th_mod
             print *,"x_mod: ", x_mod
             print *,"x_real: ", x_real
+            print *,"--------------"
             print *,"--------------"
             
          end do                 !phi
