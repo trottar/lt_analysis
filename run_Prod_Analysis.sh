37396c06baa085683447b048441c7a8c201bf04b
Richard Trotta <trotta@cua.edu>
2024-02-27 01:13:39 -0500

    Testing fix to lt-sep code unsep xsect error calculation

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -647,10 +647,8 @@ if [[ $Q2 = "2p1" && $W = "2p95" ]]; then
     # Q2=2p1, W=2p95
     NumtBins=3
     NumPhiBins=10
-    #TMIN=0.100
-    #TMAX=0.600
-    TMIN=0.200
-    TMAX=0.350
+    TMIN=0.100
+    TMAX=0.600
 elif [[ $Q2 = "3p0" && $W = "2p32" ]]; then
     # Q2=3p0, W=2p32
     NumtBins=5
