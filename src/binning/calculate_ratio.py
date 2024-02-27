Testing fix to lt-sep code unsep xsect error calculation

src/binning/calculate_ratio.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-26 20:16:07 trottar"
+# Time-stamp: "2024-02-27 00:02:43 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -71,9 +71,7 @@ def calculate_ratio(kin_type, phiset, yieldDict):
         try:
             ratio = yield_data/yield_simc
             # Calculate ratio error in quadrature (absolute error)
-            #ratio_err = np.sqrt((yield_simc**2)*(yield_err_data**2)+(yield_data**2)*(yield_err_simc**2))
-            # Calculate ratio error in quadrature (relative error)
-            ratio_err = np.sqrt((yield_err_data**2)+(yield_err_simc**2))
+            ratio_err = np.sqrt((yield_err_data/yield_data)**2+(yield_err_simc/yield_simc)**2)
         except ZeroDivisionError:
             ratio = 0.0
             ratio_err = 0.0
@@ -83,8 +81,8 @@ def calculate_ratio(kin_type, phiset, yieldDict):
         if math.isinf(ratio):
             ratio = 0.0
             ratio_err = 0.0
-        print("Ratio for t-bin {} phi-bin {}: {:.3e} +/- {:.3e}".format(i+1, j+1, ratio, ratio*ratio_err))
-        dict_lst.append((i, j, ratio, ratio*ratio_err))
+        print("Ratio for t-bin {} phi-bin {}: {:.3e} +/- {:.3e}".format(i+1, j+1, ratio, ratio_err))
+        dict_lst.append((i, j, ratio, ratio_err))
     
     # Group the tuples by the first two elements using defaultdict
     groups = defaultdict(list)