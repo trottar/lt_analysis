5feef50ec49895bd5a57fcb8c527f8489a8552f6
Richard Trotta <trotta@cua.edu>
2024-02-28 07:24:24 -0500

    Analyzing each Q2/W setting one by one for thesis plots

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -655,10 +655,10 @@ elif [[ $Q2 = "3p0" && $W = "2p32" ]]; then
     # Q2=3p0, W=2p32
     NumtBins=3
     NumPhiBins=10
-    #TMIN=0.100
-    #TMAX=0.990
-    TMIN=0.850
+    TMIN=0.100
     TMAX=0.990
+    #TMIN=0.850
+    #TMAX=0.990
 elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     # Q2=3p0, W=3p14
     NumtBins=5
