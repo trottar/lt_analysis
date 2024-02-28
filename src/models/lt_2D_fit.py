4ed9fd2a08e9ee875c8080a7c85a90746b7f2d1c
Richard Trotta <trotta@cua.edu>
2024-02-28 00:48:22 -0500

    Analyzing each Q2/W setting one by one for thesis plots

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-28 00:27:18 trottar"
+# Time-stamp: "2024-02-28 00:48:12 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -165,6 +165,11 @@ def single_setting(q2_set, fn_lo, fn_hi):
         glo_tmp = TGraphErrors()
         # Use SetPoint and SetPointError to fill the graph
         for j in range(nlo.GetSelectedRows()):
+
+            # Skip insane uncertainty bins
+            if nlo.GetV3()[j] == nlo.GetV1()[j]*0.60:
+                continue
+            
             glo_tmp.SetPoint(j, nlo.GetV2()[j], nlo.GetV1()[j])
             glo_tmp.SetPointError(j, 0, nlo.GetV3()[j])
 
@@ -185,6 +190,11 @@ def single_setting(q2_set, fn_lo, fn_hi):
         ghi_tmp = TGraphErrors()
         # Use SetPoint and SetPointError to fill the graph
         for j in range(nhi.GetSelectedRows()):
+            
+            # Skip insane uncertainty bins
+            if nhi.GetV3()[j] == nhi.GetV1()[j]*0.60:
+                continue
+            
             ghi_tmp.SetPoint(j, nhi.GetV2()[j], nhi.GetV1()[j])
             ghi_tmp.SetPointError(j, 0, nhi.GetV3()[j])
 
