0d6e16037c45fd730d9d6a1feddaa07b2e7ac346
Richard Trotta <trotta@cua.edu>
2024-02-29 18:50:56 -0500

    Testing to try and debug iteration issues

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-29 18:48:58 trottar"
+# Time-stamp: "2024-02-29 18:50:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -774,13 +774,6 @@ f_exp_t = TF1("f_exp_t", "[0]*exp(-[1]*x)", 0.0, 2.0)
 
 c_total_l_t = TCanvas()
 
-# Set properties for g_sig_l_total and g_sig_t_total
-g_sig_l_total.SetLineColor(1)
-g_sig_l_total.SetMarkerStyle(5)
-g_sig_t_total.SetLineColor(2)
-g_sig_t_total.SetMarkerColor(2)
-g_sig_t_total.SetMarkerStyle(4)
-
 # Create TMultiGraph and add glo, ghi
 g_sig_mult = ROOT.TMultiGraph()
 g_sig_mult.Add(g_sig_l_total)
@@ -797,6 +790,13 @@ g_sig_mult.GetXaxis().SetTitleOffset(1.4)
 g_sig_l_total.Fit(f_exp_l, "MRQ")
 g_sig_t_total.Fit(f_exp_t, "MRQ")
 
+# Set properties for g_sig_l_total and g_sig_t_total
+g_sig_l_total.SetLineColor(1)
+g_sig_l_total.SetMarkerStyle(5)
+g_sig_t_total.SetLineColor(2)
+g_sig_t_total.SetMarkerColor(2)
+g_sig_t_total.SetMarkerStyle(4)
+
 # Set line properties for g_sig_l_total and g_sig_t_total
 f_exp_l.SetLineColor(1)
 f_exp_l.SetLineWidth(2)
@@ -805,7 +805,7 @@ f_exp_t.SetLineWidth(2)
 f_exp_t.SetLineStyle(2)
 
 # Create and draw TLegend
-leg = ROOT.TLegend(0.5, 0.7, 0.75, 0.95)
+leg = ROOT.TLegend(0.7, 0.7, 0.95, 0.95)
 leg.SetFillColor(0)
 leg.SetMargin(0.4)
 leg.AddEntry(g_sig_l_total, "#it{#sigma}_{L}", "p")
@@ -827,6 +827,7 @@ c_total.Print(outputpdf)
 c_total.Clear()
 
 g_sig_t_total.SetMarkerColor(1)
+g_sig_t_total.SetLineColor(1)
 g_sig_t_total.Draw("A*")
 g_sig_t_total.Fit(f_exp, "MRQ")
 c_total.Print(outputpdf)
