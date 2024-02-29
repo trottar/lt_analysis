00c1fe82caaa9c59af8e4d62276fa03a0331a8bf
Richard Trotta <trotta@cua.edu>
2024-02-29 18:40:26 -0500

    Testing to try and debug iteration issues

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-29 18:38:17 trottar"
+# Time-stamp: "2024-02-29 18:40:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -797,15 +797,8 @@ g_sig_mult.GetXaxis().SetTitleOffset(1.4)
 g_sig_l_total.Fit(f_exp_l, "MRQ")
 g_sig_t_total.Fit(f_exp_t, "MRQ")
 
-# Set line properties for g_sig_l_total and g_sig_t_total
-f_exp_l.SetLineColor(1)
-f_exp_l.SetLineWidth(2)
-f_exp_t.SetLineColor(2)
-f_exp_t.SetLineWidth(2)
-f_exp_t.SetLineStyle(2)
-
 # Create and draw TLegend
-leg = ROOT.TLegend(0.7, 0.7, 0.97, 0.97)
+leg = ROOT.TLegend(0.5, 0.5, 0.75, 0.75)
 leg.SetFillColor(0)
 leg.SetMargin(0.4)
 leg.AddEntry(g_sig_l_total, "#it{#sigma}_{L}", "p")
