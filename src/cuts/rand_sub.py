bf3e6d168789dc1cfafd398f9049b39931640aa4
Richard Trotta <trotta@cua.edu>
2024-03-01 16:59:25 -0500

    Testing to try and debug iteration issues

--- src/cuts/rand_sub.py
+++ src/cuts/rand_sub.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-01 16:52:11 trottar"
+# Time-stamp: "2024-03-01 16:59:23 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -1254,7 +1254,7 @@ def rand_sub(phi_setting, inpDict):
         subDict["phi_setting"] = phi_setting
         particle_subtraction(subDict, inpDict, SubtractedParticle, hgcer_cutg, scale_factor=1.0)
         histDict["H_MM_SUB_DATA"] = subDict["H_MM_SUB_DATA"]
-        H_MM_DATA.Add(histDict["H_MM_SUB_DATA"],-1)
+        #H_MM_DATA.Add(histDict["H_MM_SUB_DATA"],-1)
     
     ###
     # CT plots
@@ -1301,8 +1301,8 @@ def rand_sub(phi_setting, inpDict):
         # MM sub plots    
         CMMsub = TCanvas()
 
-        histDict["H_MM_nosub_DATA"].SetLineColor(1)
-        histDict["H_MM_nosub_DATA"].Draw("same, E1")
+        #histDict["H_MM_nosub_DATA"].SetLineColor(1)
+        #histDict["H_MM_nosub_DATA"].Draw("same, E1")
         histDict["H_MM_SUB_DATA"].SetLineColor(2)
         histDict["H_MM_SUB_DATA"].Draw("same, E1")    
 
