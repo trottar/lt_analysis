2cac61e9e1b9500aed832ddc2ad3e163f29293bb
Richard Trotta <trotta@cua.edu>
2024-02-29 23:51:19 -0500

    Testing to try and debug iteration issues

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-02-29 23:49:29 trottar"
+# Time-stamp: "2024-02-29 23:51:17 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -778,7 +778,7 @@ def single_setting(q2_set, fn_lo, fn_hi):
         del c4
         del c5
 
-        return t_list
+    return t_list
 
 fn_lo =  "{}/src/{}/xsects/x_unsep.{}_Q{}W{}_{:.0f}.dat".format(LTANAPATH, ParticleType, polID, Q2.replace("p",""), W.replace("p",""), float(LOEPS)*100)
 fn_hi =  "{}/src/{}/xsects/x_unsep.{}_Q{}W{}_{:.0f}.dat".format(LTANAPATH, ParticleType, polID, Q2.replace("p",""), W.replace("p",""), float(HIEPS)*100)
