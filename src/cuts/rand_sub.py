b40cfebf8d24b6709704987452d16462b7370edf
Richard Trotta <trotta@cua.edu>
2024-03-01 23:12:57 -0500

    Testing to try and debug iteration issues

--- src/cuts/rand_sub.py
+++ src/cuts/rand_sub.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-01 23:08:22 trottar"
+# Time-stamp: "2024-03-01 23:12:55 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -149,6 +149,21 @@ def rand_sub(phi_setting, inpDict):
     #mm_max = 1.18
     mm_min = 0.7
     mm_max = 1.5
+
+    scale_dict ={
+        # Q2=2p1, W=2p95
+        "Q2p1W2p95Right_highe" : 3.0e-2,
+        "Q2p1W2p95Left_highe" : 2.5e-2,
+        "Q2p1W2p95Center_highe" : 1.0e-2,
+        "Q2p1W2p95Left_lowe" : 1.5e-2,
+        "Q2p1W2p95Center_lowe" : 1.5e-2,
+        # Q2=3p0, W=3p14
+        "Q3p0W3p14Right_highe" : 2.5e-2,
+        "Q3p0W3p14Left_highe" : 2.5e-2,
+        "Q3p0W3p14Center_highe" : 2.5e-2,
+        "Q3p0W3p14Left_lowe" : 2.5e-2,
+        "Q3p0W3p14Center_lowe" : 2.5e-2,
+    }
     
     # Adjusted HMS delta to fix hsxfp correlation
     # See Dave Gaskell's slides for more info: https://redmine.jlab.org/attachments/2316
@@ -1254,20 +1269,6 @@ def rand_sub(phi_setting, inpDict):
         subDict["H_MM_SUB_DUMMY_RAND"] = TH1D("H_MM_SUB_DUMMY_RAND","MM_{}".format(SubtractedParticle), 100, 0.7, 1.5)
         subDict["nWindows"] = nWindows
         subDict["phi_setting"] = phi_setting
-        scale_dict ={
-            # Q2=2p1, W=2p95
-            "Q2p1W2p95Right_highe" : 3.0e-2,
-            "Q2p1W2p95Left_highe" : 2.5e-2,
-            "Q2p1W2p95Center_highe" : 1.0e-2,
-            "Q2p1W2p95Left_lowe" : 2.0e-2,
-            "Q2p1W2p95Center_lowe" : 2.0e-2,
-            # Q2=3p0, W=3p14
-            "Q3p0W3p14Right_highe" : 2.5e-2,
-            "Q3p0W3p14Left_highe" : 2.5e-2,
-            "Q3p0W3p14Center_highe" : 2.5e-2,
-            "Q3p0W3p14Left_lowe" : 2.5e-2,
-            "Q3p0W3p14Center_lowe" : 2.5e-2,
-        }
         scale_factor = scale_dict["Q{}W{}{}_{}e".format(Q2,W,phi_setting,EPSSET)]
         particle_subtraction(subDict, inpDict, SubtractedParticle, hgcer_cutg, scale_factor=scale_factor)
         histDict["H_MM_SUB_DATA"] = subDict["H_MM_SUB_DATA"]
