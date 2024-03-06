27c5091e3f912f47ec00f76e9b52007e0637f23b
Richard Trotta <trotta@cua.edu>
2024-03-06 05:21:29 -0500

    Testing to try and debug iteration issues

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-06 05:15:35 trottar"
+# Time-stamp: "2024-03-06 05:21:27 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -221,8 +221,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     #f_sigL_pre.FixParameter(0, l0)
     #f_sigL_pre.FixParameter(1, l1)
     ##f_sigL_pre.FixParameter(1, 0.0)
-    f_sigL_pre.FixParameter(2, l2)
-    f_sigL_pre.FixParameter(3, l3)
+    #f_sigL_pre.FixParameter(2, l2)
+    #f_sigL_pre.FixParameter(3, l3)
     ##############
     ##############
     ##############    
@@ -302,8 +302,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     #f_sigL.FixParameter(0, l0)
     #f_sigL.FixParameter(1, l1)
     ##f_sigL.FixParameter(1, 0.0)
-    f_sigL.FixParameter(2, l2)
-    f_sigL.FixParameter(3, l3)    
+    #f_sigL.FixParameter(2, l2)
+    #f_sigL.FixParameter(3, l3)    
     ##############
     ##############
     ##############    
