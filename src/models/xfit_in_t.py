5278ba84dda3f25e436e7523096aad52eccb7ac0
Richard Trotta <trotta@cua.edu>
2024-03-10 06:31:32 -0400

    Removed +0.2 term from sigL to test Q2=3.0, W=2.32. It may be the issue with finding parameterization

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-10 06:30:20 trottar"
+# Time-stamp: "2024-03-10 06:31:25 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -926,3 +926,5 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
             for i in range(len(par_vec)):
                 f.write("{:13.5e} {:13.5e} {:3d} {:12.1f}\n".format(par_vec[i], par_err_vec[i], i+1, par_chi2_vec[i]))
                 print("  {:.3f} {:.3f} {:.1f} {:.1f}".format(par_vec[i], par_err_vec[i], i+1, par_chi2_vec[i]))
+    else:
+        print("DEBUG ENABLED: No changes to previous iteration...")
