1d8fe71039e7ac9cad697a3f0c01d80024807885
Richard Trotta <trotta@cua.edu>
2024-03-10 04:50:31 -0400

    Removed +0.2 term from sigL to test Q2=3.0, W=2.32. It may be the issue with finding parameterization

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-10 04:49:35 trottar"
+# Time-stamp: "2024-03-10 04:50:29 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -217,9 +217,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     else: 
         f_sigL_pre.SetParLimits(3, -0.001, 0.001)       
     # Fixing sigL terms for testing
-    #f_sigL_pre.FixParameter(0, l0)
+    f_sigL_pre.FixParameter(0, l0)
     f_sigL_pre.FixParameter(1, l1)
-    #f_sigL_pre.FixParameter(2, l2)
+    f_sigL_pre.FixParameter(2, l2)
     f_sigL_pre.FixParameter(3, l3)
     # Fixing sigL terms to zero
     #f_sigL_pre.FixParameter(0, 0.0)
@@ -299,9 +299,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     else: 
         f_sigL.SetParLimits(3, -0.001, 0.001)       
     # Fixing sigL terms for testing
-    #f_sigL.FixParameter(0, l0)
+    f_sigL.FixParameter(0, l0)
     f_sigL.FixParameter(1, l1)
-    #f_sigL.FixParameter(2, l2)
+    f_sigL.FixParameter(2, l2)
     f_sigL.FixParameter(3, l3)
     # Fixing sigL terms to zero
     #f_sigL.FixParameter(0, 0.0)
