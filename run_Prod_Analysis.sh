96f9711026c308576a6f4ce8449506b50357b352
Richard Trotta <trotta@cua.edu>
2024-03-11 16:09:22 -0400

    Fixed issues with average kin and yield uncertainties written to files

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -643,10 +643,12 @@ done
 # TMIN should not equal zero (unless calc_xsect.f is adapted)
 # Make sure 3 sig figs (no more)
 
-#MissMassMin=0.7
-#MissMassMax=1.5
-MissMassMin=1.10
-MissMassMax=1.16
+# Full spectrum
+MissMassMin=0.7
+MissMassMax=1.5
+# MM cut
+#MissMassMin=1.10
+#MissMassMax=1.16
 #MissMassMax=1.18
 #MissMassMax=1.24
 
