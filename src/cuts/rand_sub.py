186ed244d2eb4dfe38df45fa4f638c3f20fc2215
Richard Trotta <trotta@cua.edu>
2024-03-11 16:54:00 -0400

    Fixed issues with average kin and yield uncertainties written to files

--- src/cuts/rand_sub.py
+++ src/cuts/rand_sub.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-11 16:38:17 trottar"
+# Time-stamp: "2024-03-11 16:53:49 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -164,7 +164,7 @@ def rand_sub(phi_setting, inpDict):
         "Q3p0W3p14Right_highe" : 2.28e-2,
         "Q3p0W3p14Left_highe" : 4.75e-2,
         "Q3p0W3p14Center_highe" : 2.5e-2,
-        "Q3p0W3p14Left_lowe" : 1.50e-2,
+        "Q3p0W3p14Left_lowe" : 1.25e-2,
         "Q3p0W3p14Center_lowe" : 1.5e-2,
         # Q2=4p4, W=2p74
         "Q4p4W2p74Right_highe" : 2.28e-2,
