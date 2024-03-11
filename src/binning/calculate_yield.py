Fixed issues with average kin and yield uncertainties written to files

src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-09 08:32:13 trottar"
+# Time-stamp: "2024-03-11 02:53:29 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -693,7 +693,7 @@ def calculate_yield_simc(kin_type, hist, t_bins, phi_bins, inpDict, iteration):
             #print("!!!!!!!!!!!!!",(1/np.sqrt(binned_unweighted_NumEvts_simc[i]))*normfac_simc)
             #yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))*normfac_simc/100 # FIX (/100 is temporary)
             # FIX: Removed norm_fac because shouldn't normalize non-weighted distribution
-            yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))
+            yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))/100
         except ZeroDivisionError:
             yld = 0.0
             yld_err = 0.0