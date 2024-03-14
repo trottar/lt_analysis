c664802aa88e9c25c7da2026bc361173f73a542a
Richard Trotta <trotta@cua.edu>
2024-03-14 13:01:41 -0400

    Fixed issues with average kin and yield uncertainties written to files

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-14 12:27:50 trottar"
+# Time-stamp: "2024-03-14 13:01:38 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -222,7 +222,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigL_pre.FixParameter(0, l0)
     f_sigL_pre.FixParameter(1, l1)
     #f_sigL_pre.FixParameter(2, l2)
-    f_sigL_pre.FixParameter(2, -0.75) # HARD CODE
+    f_sigL_pre.FixParameter(2, -0.80) # HARD CODE
     f_sigL_pre.FixParameter(3, l3)
     # Fixing sigL terms to zero
     #f_sigL_pre.FixParameter(0, 0.0)
@@ -309,7 +309,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     f_sigL.FixParameter(0, l0)
     f_sigL.FixParameter(1, l1)
     #f_sigL.FixParameter(2, l2)
-    f_sigL.FixParameter(2, -0.75) # HARD CODE
+    f_sigL.FixParameter(2, -0.80) # HARD CODE
     f_sigL.FixParameter(3, l3)
     # Fixing sigL terms to zero
     #f_sigL.FixParameter(0, 0.0)
