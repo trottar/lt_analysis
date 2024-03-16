2034c62f27b74f22e07d8727090facff18882342
Richard Trotta <trotta@cua.edu>
2024-03-16 18:02:11 -0400

    Producing pion samples in kaon MM.

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -669,10 +669,12 @@ elif [[ $Q2 = "3p0" && $W = "2p32" ]]; then
     #NumPhiBins=10
     NumtBins=5
     NumPhiBins=16
-    TMIN=0.100
-    TMAX=0.990
+    #TMIN=0.100
+    #TMAX=0.990
     #TMIN=0.100
     #TMAX=1.200
+    TMIN=0.100
+    TMAX=0.600
     #TMIN=0.450 # Center low
     #TMAX=0.800 # Center low
     #TMIN=0.500 # Max value of tmin across phi settings
