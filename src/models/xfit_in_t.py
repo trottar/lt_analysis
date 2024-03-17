2d36f02f4284c7d13f3a33114bbb1894204d790f
Richard Trotta <trotta@cua.edu>
2024-03-17 16:26:08 -0400

    Producing pion samples in kaon MM.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-17 10:26:21 trottar"
+# Time-stamp: "2024-03-17 16:26:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -58,7 +58,8 @@ DEBUG=False
 # HARD CODED #
 ##############
 # Limit for finding new parameters (units of percent)
-par_lim = 0.2 # +/-20%
+par_lim = 0.05 # +/-5%
+#par_lim = 0.2 # +/-20%
 #par_lim = 0.75 # +/-75%
 ##############
 ##############
