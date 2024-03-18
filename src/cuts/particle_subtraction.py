6b346df05692ebf5089ff9e5744ce71c40b0c8ee
Richard Trotta <trotta@cua.edu>
2024-03-18 17:06:14 -0400

    Producing pion samples in kaon MM.

--- src/cuts/particle_subtraction.py
+++ src/cuts/particle_subtraction.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-18 17:04:12 trottar"
+# Time-stamp: "2024-03-18 17:06:00 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -94,10 +94,10 @@ scale_dict ={
     "Q4p4W2p74Left_lowe" : 0.25e-2,
     "Q4p4W2p74Center_lowe" : 0.25e-2,
     # Q2=5p5, W=3p02
-    "Q5p5W3p02Right_highe" : 1.25e-2,
-    "Q5p5W3p02Left_highe" : 2.75e-2,
-    "Q5p5W3p02Center_highe" : 1.00e-2,
-    "Q5p5W3p02Left_lowe" : 1.00e-2,
+    "Q5p5W3p02Right_highe" : 0.75e-2,
+    "Q5p5W3p02Left_highe" : 2.25e-2,
+    "Q5p5W3p02Center_highe" : 0.75e-2,
+    "Q5p5W3p02Left_lowe" : 0.75e-2,
     "Q5p5W3p02Center_lowe" : 0.75e-2,
 }
 
