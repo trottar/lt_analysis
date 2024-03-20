f6c3519555c2e3431bb342f8e16fd801ae512bed
Richard Trotta <trotta@cua.edu>
2024-03-20 13:42:07 -0400

    Producing pion samples in kaon MM.

--- src/calc_xsect.f
+++ src/calc_xsect.f
@@ -198,11 +198,11 @@ c      pause
 
             phi=(ip-0.5)*2.*3.14159/nphi
 
-*            if (phi.le.0.0) then
-*               phi=phi+2.*3.14159
-*            else if (phi.gt.2.*3.14159) then
-*               phi=phi-2.*3.14159
-*            end if
+            if (phi.le.0.0) then
+               phi=phi+2.*3.14159
+            else if (phi.gt.2.*3.14159) then
+               phi=phi-2.*3.14159
+            end if
             
             read(51,*) r,dr
             
