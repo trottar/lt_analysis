a7273169c6ffe50dada345274ccc7da5a5cf381f
Richard Trotta <trotta@cua.edu>
2024-03-20 14:02:49 -0400

    Producing pion samples in kaon MM.

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-20 13:47:16 trottar"
+# Time-stamp: "2024-03-20 14:02:34 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -232,8 +232,12 @@ def single_setting(q2_set, w_set, fn_lo, fn_hi):
             g_plot_err.SetPoint(g_plot_err.GetN(), g_xx, hi_eps, g_yy)
             g_plot_err.SetPointError(g_plot_err.GetN()-1, 0.0, 0.0, g_yy_err)
 
-        lo_cross_sec_err[i] = 1/math.sqrt(lo_cross_sec_err[i])            
-        hi_cross_sec_err[i] = 1/math.sqrt(hi_cross_sec_err[i])
+        try:
+            lo_cross_sec_err[i] = 1/math.sqrt(lo_cross_sec_err[i])            
+            hi_cross_sec_err[i] = 1/math.sqrt(hi_cross_sec_err[i])
+        except ZeroDivisionError:
+            lo_cross_sec_err[i] = -1000
+            hi_cross_sec_err[i] = -1000
             
         g_plot_err.SetFillColor(29)
         g_plot_err.SetMarkerSize(0.8)
