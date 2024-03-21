d6ce507562b1924d5424c2e893b560b0c66a03d6
Richard Trotta <trotta@cua.edu>
2024-03-21 11:41:35 -0400

    Producing pion samples in kaon MM.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-21 11:40:33 trottar"
+# Time-stamp: "2024-03-21 11:41:33 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -249,10 +249,10 @@ if Q2Val == 2.1:
     inpDict["Wmin"] = WVal - (2/9)*WVal
     inpDict["Wmax"] = WVal + (2/9)*WVal
 elif Q2Val == 3.0 and WVal == 2.32:
-    inpDict["Q2min"] = Q2Val - (2/9)*Q2Val
-    inpDict["Q2max"] = Q2Val + (2/9)*Q2Val
-    inpDict["Wmin"] = WVal - (2/9)*WVal
-    inpDict["Wmax"] = WVal + (2/9)*WVal
+    inpDict["Q2min"] = Q2Val - (2/8)*Q2Val
+    inpDict["Q2max"] = Q2Val + (2/8)*Q2Val
+    inpDict["Wmin"] = WVal - (2/8)*WVal
+    inpDict["Wmax"] = WVal + (2/8)*WVal
 elif Q2Val == 3.0 and WVal == 3.14:
     inpDict["Q2min"] = Q2Val - (3/7)*Q2Val
     inpDict["Q2max"] = Q2Val + (3/7)*Q2Val
