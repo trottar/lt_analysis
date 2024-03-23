17acfdb7fcf491045bf85322bc8b2c62ed69992e
Richard Trotta <trotta@cua.edu>
2024-03-23 09:42:27 -0400

    Producing pion samples in kaon MM.

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -682,7 +682,7 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     #NumtBins=5
     #NumPhiBins=16
     NumtBins=2 # Testing
-    NumPhiBins=12 # Testing
+    NumPhiBins=18 # Testing
     #TMIN=0.100
     #TMAX=1.000
     TMIN=0.100 # Testing
