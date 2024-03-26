6316ad3c77e11194e8ea23b3c6fa4230ac08b58a
Richard Trotta <trotta@cua.edu>
2024-03-26 11:30:21 -0400

    Fixing heep script after changes to effective charge calculations

--- src/HeeP/COIN/HeepCoin.py
+++ src/HeeP/COIN/HeepCoin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-26 11:29:01 trottar"
+# Time-stamp: "2024-03-26 11:30:19 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -62,6 +62,8 @@ efficiency_table = sys.argv[16]
 ###############################################################################################################################################
 # Define total efficiency vs run number plots
 
+InData_efficiency = np.array([float(x) for x in InData_efficiency.split(' ')])
+InData_error_efficiency = np.array([float(x) for x in InData_error_efficiency.split(' ')])
 data_runNums = np.array([int(x) for x in data_runNums.split(' ')])
 G_data_eff = TGraphErrors(len(InData_efficiency), \
                           np.array([float(x) for x in data_runNums]), \
@@ -70,6 +72,8 @@ G_data_eff = TGraphErrors(len(InData_efficiency), \
                           InData_error_efficiency*InData_efficiency)
 G_data_eff.SetName("G_data_eff")    
 
+InDummy_efficiency = np.array([float(x) for x in InDummy_efficiency.split(' ')])
+InDummy_error_efficiency = np.array([float(x) for x in InDummy_error_efficiency.split(' ')])
 dummy_runNums = np.array([int(x) for x in dummy_runNums.split(' ')])
 G_dummy_eff = TGraphErrors(len(InDummy_efficiency), \
                           np.array([float(x) for x in dummy_runNums]), \
