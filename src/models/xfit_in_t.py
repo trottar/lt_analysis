74c54fd7e2302aa74588cc982fb1c3dea943b219
Richard Trotta <trotta@cua.edu>
2024-03-27 10:14:31 -0400

    Removed RF cuts, which were causing issues with ssxptar

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-27 09:55:10 trottar"
+# Time-stamp: "2024-03-27 10:14:29 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -58,11 +58,11 @@ DEBUG=False
 # HARD CODED #
 ##############
 # Limit for finding new parameters (units of percent)
-par_lim = 0.001 # +/-.1%
+#par_lim = 0.001 # +/-.1%
 #par_lim = 0.01 # +/-1%
 #par_lim = 0.05 # +/-5%
 #par_lim = 0.2 # +/-20%
-#par_lim = 0.75 # +/-75%
+par_lim = 0.75 # +/-75%
 #par_lim = 10.00 # +/-1000%
 ##############
 ##############
