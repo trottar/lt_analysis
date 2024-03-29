a6580526eab69b86fa9d8f93f6636985f0c82ddb
Richard Trotta <trotta@cua.edu>
2024-03-29 10:28:51 -0400

    Removed RF cuts, which were causing issues with ssxptar

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-03-27 13:14:35 trottar"
+# Time-stamp: "2024-03-29 10:28:49 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -270,8 +270,8 @@ else:
 inpDict["Epsmin"] = 0.0
 inpDict["Epsmax"] = 1.0
 
-phisetlist = ["Center","Left","Right"]
-#phisetlist = ["Center"]
+#phisetlist = ["Center","Left","Right"]
+phisetlist = ["Center"]
 
 for phiset in phisetlist:
     # Call diamond cut script and append paramters to dictionary
