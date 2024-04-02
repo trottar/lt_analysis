c643e6b0b56b75474e50e399261db877610f4eac
Richard Trotta <trotta@cua.edu>
2024-04-02 16:16:50 -0400

    Fixed issue with previous iteration weight not being applied

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2024-04-02 16:14:33 trottar"
+# Time-stamp: "2024-04-02 16:16:42 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -138,7 +138,7 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
       #print("-"*25,"\n",i,"\n",inp_param)
       if iter_num > 1:
           inp_param = '{} {} {} {} {} {} {} {} {} '\
-                      .format(\Q2, evt.Q2i, evt.Wi, evt.ti, evt.epscm, evt.thetacm, evt.phipqi, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)
+                      .format(Q2, evt.Q2i, evt.Wi, evt.ti, evt.epscm, evt.thetacm, evt.phipqi, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)
           #inp_param = '{} {} {} {} {} {} {} {} {} '\
               #.format(Q2, evt.Q2, evt.W, evt.t, evt.epscm, evt.thetacm, evt.phipq, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)
       else:
