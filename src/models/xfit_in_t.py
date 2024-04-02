4d3e64779c83af405e43d534e629029bf57705e5
Richard Trotta <trotta@cua.edu>
2024-04-02 09:14:28 -0400

    Removed RF cuts, which were causing issues with ssxptar

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-01 23:52:31 trottar"
+# Time-stamp: "2024-04-02 09:14:26 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -51,8 +51,8 @@ m_p = 0.93827231
 m_n = 0.93956541
 mkpl = 0.493677
 
-#DEBUG=False
-DEBUG=True
+DEBUG=False
+#DEBUG=True
 
 ##############
 # HARD CODED #
