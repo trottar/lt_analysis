421419f3c57a196b93ec12724df6da208e6746a1
Richard Trotta <trotta@cua.edu>
2024-04-03 02:37:30 -0400

    Fixed issue with previous iteration weight not being applied

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2024-04-03 02:36:01 trottar"
+# Time-stamp: "2024-04-03 02:37:28 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -102,19 +102,23 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
         new_InFile_SIMC = ROOT.TFile.Open(simc_root.replace(".root","_iter_{}.root".format(iter_num)), "RECREATE")        
         
     # Clone the TTree from the original file
-    #new_TBRANCH_SIMC = TBRANCH_SIMC.CloneTree(-1, "fast") # Test, commented
-    new_TBRANCH_SIMC = new_InFile_SIMC.Get("h10")
+    new_TBRANCH_SIMC = TBRANCH_SIMC.CloneTree(-1, "fast") # Test, commented
+    #new_TBRANCH_SIMC = new_InFile_SIMC.Get("h10")
 
     if iter_num > 1:
         # Get the Weight branch from the new tree
         new_Weight_SIMC = new_TBRANCH_SIMC.GetBranch("iter_weight")
         # Get the sig branch from the new tree
-        new_sig_SIMC = new_TBRANCH_SIMC.GetBranch("iter_sig")            
+        new_sig_SIMC = new_TBRANCH_SIMC.GetBranch("iter_sig")
     else:
         # Get the Weight branch from the new tree
         new_Weight_SIMC = new_TBRANCH_SIMC.GetBranch("Weight")
         # Get the sig branch from the new tree
         new_sig_SIMC = new_TBRANCH_SIMC.GetBranch("sigcm")
+        # Get the Weight branch from the new tree
+        new_Weight_SIMC = new_TBRANCH_SIMC.GetBranch("iter_weight")
+        # Get the sig branch from the new tree
+        new_sig_SIMC = new_TBRANCH_SIMC.GetBranch("iter_sig")        
         
         # Create a new branch with the updated values
         #iweight = array('d', [0])  # Assuming 'd' is the data type, change if needed
