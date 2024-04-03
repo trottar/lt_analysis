1b2cf21f240d4064561566e83e010d9c6dfe82fd
Richard Trotta <trotta@cua.edu>
2024-04-03 01:32:17 -0400

    Fixed issue with previous iteration weight not being applied

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2024-04-03 01:30:21 trottar"
+# Time-stamp: "2024-04-03 01:32:14 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -164,15 +164,19 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
       iter_lst = iterWeight(inp_param)
 
       # Set the value of iweight
-      iweight[0] = iter_lst[0]
-      new_Weight_SIMC.SetAddress(iweight) # Test, commented
+      #iweight[0] = iter_lst[0]
+      #new_Weight_SIMC.SetAddress(iweight) # Test, commented
+
+      new_Weight_SIMC.SetAddress(iter_lst[0]) # Test
 
       # Fill the new branch with the new value for this entry
       new_weight_branch.Fill()
       
       # Set the value of isig
-      isig[0] = iter_lst[1]
-      new_sig_SIMC.SetAddress(isig) # Test, commented
+      #isig[0] = iter_lst[1]
+      #new_sig_SIMC.SetAddress(isig) # Test, commented
+
+      new_sig_SIMC.SetAddress(iter_lst[1]) # Test
       
       # Fill the new branch with the new value for this entry
       new_sig_branch.Fill()
