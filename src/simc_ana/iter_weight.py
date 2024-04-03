f47fb83b8b81917b4cf723ed214d53d024407e2b
Richard Trotta <trotta@cua.edu>
2024-04-03 02:46:07 -0400

    Fixed issue with previous iteration weight not being applied

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2024-04-03 02:44:49 trottar"
+# Time-stamp: "2024-04-03 02:46:05 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -118,10 +118,10 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
         # Create a new branch with the updated values
         #iweight = array('d', [0])  # Assuming 'd' is the data type, change if needed
         iter_weight = array('d')  # Assuming 'd' is the data type, change if needed # Test
-        new_weight_branch = new_TBRANCH_SIMC.Branch("iter_weight", iweight, "iter_weight/D")  # 'D' for double, change if needed
+        new_weight_branch = new_TBRANCH_SIMC.Branch("iter_weight", iter_weight, "iter_weight/D")  # 'D' for double, change if needed
         #isig = array('d', [0])  # Assuming 'd' is the data type, change if needed
         iter_sig = array('d')  # Assuming 'd' is the data type, change if needed # Test
-        new_sig_branch = new_TBRANCH_SIMC.Branch("iter_sig", isig, "iter_sig/D")  # 'D' for double, change if needed
+        new_sig_branch = new_TBRANCH_SIMC.Branch("iter_sig", iter_sig, "iter_sig/D")  # 'D' for double, change if needed
     
     ################################################################################################################################################
     # Run over simc root branch to determine new weight
