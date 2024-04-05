6ea19c145144b8d58985122144add9ce31b4ec38
Richard Trotta <trotta@cua.edu>
2024-04-05 19:58:26 -0400

    Reran simc with corrected fX. MM for simc is weird so checking energy/momentum

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2024-04-05 19:57:38 trottar"
+# Time-stamp: "2024-04-05 19:58:24 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -204,7 +204,8 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
         #iter_sig_branch = np.array(iter_sig, dtype=np.float64)  # Convert iter_array to numpy array with appropriate data type
 
         # Convert array (iter) to a branch and add to h20
-        iter_weight_branch = new_TBRANCH_SIMC.Branch("iter_weight", np.zeros(1, dtype=float), "iter_weight/D")  # Create a new branch in h20 tree
+        iter_weight_branch = new_TBRANCH_SIMC.Branch("iter_weight", np.zeros(1, dtype=float), "iter_weight/D")  # Create a new branch in h10 tree
+        iter_sig_branch = new_TBRANCH_SIMC.Branch("iter_sig", np.zeros(1, dtype=float), "iter_sig/D")  # Create a new branch in h10 tree
         for value in iter_weight:
             iter_weight_branch.Fill()  # Fill the branch with values from iter_array
         for value in iter_sig:
