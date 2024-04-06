f2105905307ae01693f8ae57a27df4b6494e8328
Richard Trotta <trotta@cua.edu>
2024-04-05 20:19:46 -0400

    Reran simc with corrected fX. MM for simc is weird so checking energy/momentum

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -3,7 +3,7 @@
 #
 # Description: Adapted from fortran code wt28_3.f
 # ================================================================
-# Time-stamp: "2024-04-05 20:14:42 trottar"
+# Time-stamp: "2024-04-05 20:19:44 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -207,15 +207,17 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
         iter_weight_branch = new_TBRANCH_SIMC.Branch("iter_weight", np.zeros(1, dtype=float), "iter_weight/F")  # Create a new branch in h10 tree
         iter_sig_branch = new_TBRANCH_SIMC.Branch("iter_sig", np.zeros(1, dtype=float), "iter_sig/F")  # Create a new branch in h10 tree
         for value in iter_weight:
-            iter_weight_branch.Fill()  # Fill the branch with values from iter_array
+            iter_weight_branch.SetLeafValue("iter_weight", value)
+            new_TBRANCH_SIMC.Fill()  # Fill the branch with values from iter_array
         for value in iter_sig:
-            iter_sig_branch.Fill()  # Fill the branch with values from iter_array            
-        
+            iter_sig_branch.SetLeafValue("iter_sig", value)
+            new_TBRANCH_SIMC.Fill()  # Fill the branch with values from iter_array
+            
         # Convert array to a branch and add to new iteration root tree
         #rnp.array2tree(iter_weight_branch, tree=new_TBRANCH_SIMC)
         #rnp.array2tree(iter_sig_branch, tree=new_TBRANCH_SIMC)
 
-        new_TBRANCH_SIMC.Write()
+        #new_TBRANCH_SIMC.Write()
         #new_InFile_SIMC.Write()
 
     else:
