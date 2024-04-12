cc57aa21236271f274f9ade07b40f28afe29f8a9
Richard Trotta <trotta@cua.edu>
2024-04-12 01:52:14 -0400

    Testing fixes to pion subtraction when MM cut applied

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-06 02:35:58 trottar"
+# Time-stamp: "2024-04-12 01:52:12 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -59,9 +59,9 @@ DEBUG=False
 ##############
 # Limit for finding new parameters (units of percent)
 #par_lim = 0.001 # +/-.1%
-par_lim = 0.01 # +/-1%
+#par_lim = 0.01 # +/-1%
 #par_lim = 0.05 # +/-5%
-#par_lim = 0.2 # +/-20%
+par_lim = 0.2 # +/-20%
 #par_lim = 0.75 # +/-75%
 #par_lim = 10.00 # +/-1000%
 ##############
