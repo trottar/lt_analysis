Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-17 17:03:18 trottar"
+# Time-stamp: "2024-04-17 17:04:43 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -51,7 +51,7 @@ def fun_Sig_L(x, par):
 def fun_Sig_L_tmp2(x, p0, p1, p2, p3):
     tt = np.abs(x[0])
     qq = np.abs(x[1])
-    f = (p0+p1*math.log(qq)) * math.exp((p2+p3*math.log(qq)) * (abs(tt)+0.2))
+    f = (p0+p1*np.log(qq)) * np.exp((p2+p3*np.log(qq)) * (abs(tt)+0.2))
     return f
 
 # Function for SigL