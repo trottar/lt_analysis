Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-17 16:26:01 trottar"
+# Time-stamp: "2024-04-17 16:26:46 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -347,7 +347,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     for i in range(len(w_vec)):
         t_vec.append(g_sigl.GetX()[i])
     # Combine tt and qq data as required by curve_fit
-    x_data_combined = np.vstack((, q2_vec))
+    x_data_combined = np.vstack((t_vec, q2_vec))
     y_data  = sigl_X_fit
     y_data_err  = sigl_X_fit_err
     # Fit the function to the data and account for uncertainties