a47ad65674ee6cdd208e19b307bc78bc80ee7fc5
Richard Trotta <trotta@cua.edu>
2024-04-18 01:56:03 -0400

    Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -681,13 +681,14 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     # Q2=3p0, W=3p14
     #NumtBins=5
     #NumPhiBins=16
-    NumtBins=2 # Testing
-    NumPhiBins=18 # Testing
-    #NumPhiBins=12 # Testing
-    #TMIN=0.100
-    #TMAX=1.000
-    TMIN=0.100 # Testing
-    TMAX=0.300 # Testing
+    #NumtBins=2 # Testing
+    #NumPhiBins=18 # Testing
+    NumtBins=3 # Testing
+    NumPhiBins=12 # Testing
+    TMIN=0.100
+    TMAX=1.000
+    #TMIN=0.100 # Testing
+    #TMAX=0.300 # Testing
     #TMIN=0.150
     #TMAX=0.650
     #TMIN=0.150 # Center low
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-14 10:45:20 trottar"
+# Time-stamp: "2024-04-18 01:55:23 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -270,8 +270,8 @@ else:
 inpDict["Epsmin"] = 0.0
 inpDict["Epsmax"] = 1.0
 
-#phisetlist = ["Center","Left","Right"]
-phisetlist = ["Center"]
+phisetlist = ["Center","Left","Right"]
+#phisetlist = ["Center"]
 
 for phiset in phisetlist:
     # Call diamond cut script and append paramters to dictionary
