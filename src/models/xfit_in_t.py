Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-17 18:50:05 trottar"
+# Time-stamp: "2024-04-17 21:21:01 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -579,9 +579,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     y_data = []
     y_data_err = []
     for i in range(len(w_vec)):
-        t_vec.append(g_sigl.GetX()[i])
-        y_data.append(g_sigl.GetY()[i])
-        y_data_err.append(g_sigl.GetEY()[i])
+        t_vec.append(g_sigt.GetX()[i])
+        y_data.append(g_sigt.GetY()[i])
+        y_data_err.append(g_sigt.GetEY()[i])
     # Combine tt and qq data as required by curve_fit
     x_data_combined = np.vstack((t_vec, q2_vec))
 
@@ -832,9 +832,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     y_data = []
     y_data_err = []
     for i in range(len(w_vec)):
-        t_vec.append(g_sigl.GetX()[i])
-        y_data.append(g_sigl.GetY()[i])
-        y_data_err.append(g_sigl.GetEY()[i])
+        t_vec.append(g_siglt.GetX()[i])
+        y_data.append(g_siglt.GetY()[i])
+        y_data_err.append(g_siglt.GetEY()[i])
     # Combine tt and qq data as required by curve_fit
     x_data_combined = np.vstack((t_vec, q2_vec))
 
@@ -1063,9 +1063,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     y_data = []
     y_data_err = []
     for i in range(len(w_vec)):
-        t_vec.append(g_sigl.GetX()[i])
-        y_data.append(g_sigl.GetY()[i])
-        y_data_err.append(g_sigl.GetEY()[i])
+        t_vec.append(g_sigtt.GetX()[i])
+        y_data.append(g_sigtt.GetY()[i])
+        y_data_err.append(g_sigtt.GetEY()[i])
     # Combine tt and qq data as required by curve_fit
     x_data_combined = np.vstack((t_vec, q2_vec))
 