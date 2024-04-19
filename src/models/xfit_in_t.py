fbaae2c7fe626f6fe4fc1007003e9970f6b6cc6e
Richard Trotta <trotta@cua.edu>
2024-04-19 17:48:46 -0400

    Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-19 10:26:23 trottar"
+# Time-stamp: "2024-04-19 17:48:44 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -60,10 +60,10 @@ DEBUG=False
 # Limit for finding new parameters (units of percent)
 #par_lim = 0.001 # +/-.1%
 #par_lim = 0.01 # +/-1%
-par_lim = 0.05 # +/-5%
+#par_lim = 0.05 # +/-5%
 #par_lim = 0.2 # +/-20%
 #par_lim = 0.75 # +/-75%
-#par_lim = 10.00 # +/-1000%
+par_lim = 10.00 # +/-1000%
 ##############
 ##############
 ##############
