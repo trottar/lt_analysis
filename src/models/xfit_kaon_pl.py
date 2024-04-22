3ee2b319ca69bb02d5b18ba9f85b47e5ca2bbfc8
Richard Trotta <trotta@cua.edu>
2024-04-21 20:45:11 -0400

    Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-20 17:49:38 trottar"
+# Time-stamp: "2024-04-21 20:45:08 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -38,7 +38,7 @@ def set_val(inp_pol_str, inp_q2_set):
 # Function for SigL
 def fun_Sig_L(x, par):
     tt = abs(x[0])
-    qq = abs(x[1])
+    qq = float(q2_set)
     #print("Calculating function for func_SigL...\nQ2={:.1e}, t={:.3e}\npar=({:.2e}, {:.2e}, {:.2e}, {:.2e})\n\n".format(qq, tt, *par))
     # RLT (2/19/2024): Adding a 0.2 term to t dependence to bring down the extreme slope at high t
     # RLT (3/09/2024): Removing +0.2 term for better parameterization of Q2=3.0, W=2.32
@@ -51,7 +51,7 @@ def fun_Sig_L(x, par):
 # Function for SigT
 def fun_Sig_T(x, par):
     tt = abs(x[0])
-    qq = abs(x[1])
+    qq = float(q2_set)
     tav = (0.1112 + 0.0066*math.log(float(q2_set.replace("p","."))))*float(q2_set.replace("p","."))
     ftav = (abs(tt)-tav)/tav
     #print("Calculating function for func_SigT...\nQ2={:.1e}, t={:.3e}\npar=({:.2e}, {:.2e}, {:.2e}, {:.2e})\n\n".format(qq, tt, *par))
@@ -74,7 +74,7 @@ def fun_Sig_T(x, par):
 # thetacm term is defined on function calling
 def fun_Sig_LT(x, par):
     tt = abs(x[0])
-    qq = abs(x[1])
+    qq = float(q2_set)
     #print("Calculating function for func_SigLT...\nQ2={:.1e}, t={:.3e}\npar=({:.2e}, {:.2e}, {:.2e}, {:.2e})\n\n".format(qq, tt, *par))
     f = (par[0]*math.exp(par[1]*abs(tt))+par[2]/abs(tt))
     return f
@@ -85,7 +85,7 @@ def fun_Sig_LT(x, par):
 # thetacm term is defined on function calling
 def fun_Sig_TT(x, par):
     tt = abs(x[0])
-    qq = abs(x[1])
+    qq = float(q2_set)
     if pol_str == "pl":
         f_tt=abs(tt)/(abs(tt)+mkpl**2)**2 # pole factor
     #print("Calculating function for func_SigTT...\nQ2={:.1e}, t={:.3e}\npar=({:.2e}, {:.2e}, {:.2e}, {:.2e})\n\n".format(qq, tt, *par))
