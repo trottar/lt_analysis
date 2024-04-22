f766bf78f7b852bfba173144427dd2aa3053cac0
Richard Trotta <trotta@cua.edu>
2024-04-22 19:20:39 -0400

    Testing new hgcer cuts for pion and kaon. Fixed missing t-binning for average kinematic calulcation

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-22 19:18:20 trottar"
+# Time-stamp: "2024-04-22 19:20:37 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -66,8 +66,8 @@ def fun_Sig_T(x, par):
     #f = par[0] / (1 + par[1]*qq)
     # RLT (4/20/2024): Adding in t-dependence
     #f = (par[0] / (1 + par[1]*qq)) * ftav
-    #f = (par[0] / (1 + par[1]*qq)) * abs(tt)
-    f = par[0]+par[1]*math.log(qq)+(par[2]+par[3]*math.log(qq)) * ftav
+    f = (par[0] / (1 + par[1]*qq)) * abs(tt)
+    #f = par[0]+par[1]*math.log(qq)+(par[2]+par[3]*math.log(qq)) * ftav
     return f
 
 ###############################################################################################################################################
