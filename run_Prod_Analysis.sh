adf4a50fbdccf519e470fa20fd13337980fb99e8
Richard Trotta <trotta@cua.edu>
2024-04-25 08:21:03 -0400

    Testing different functional forms for xsect

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -687,9 +687,9 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     # Q2=3p0, W=3p14
     #NumtBins=5
     #NumPhiBins=16
-    NumtBins=2 # Testing
+    #NumtBins=2 # Testing
     #NumPhiBins=18 # Testing
-    #NumtBins=7 # Testing
+    NumtBins=5 # Testing
     NumPhiBins=12 # Testing
     #TMIN=0.100
     #TMAX=1.000
