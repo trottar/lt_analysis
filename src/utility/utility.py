63e34eaf95c0c29e3c373cc1e88351a6d7eb629f
Richard Trotta <trotta@cua.edu>
2024-04-25 03:36:44 -0400

    Testing different functional forms for xsect

--- src/utility/utility.py
+++ src/utility/utility.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-25 03:31:03 trottar"
+# Time-stamp: "2024-04-25 03:36:35 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -177,7 +177,7 @@ def match_to_bin(data):
 
     # Iterate through the given data
     for item in data:
-        print("!!!!!!!!",item)
+        print("!!!!!!!!1",item)
         match = item[0][0]  # Extract the match value (e.g., 0.33)
         value = item[1][0]  # Extract the corresponding value
 
@@ -186,7 +186,7 @@ def match_to_bin(data):
             match_dict[match].append(value)
         else:
             match_dict[match] = [value]
-
+    print("!!!!!!!!2",match_dict)
     # Convert the dictionary to the desired format
     return [[match, np.array(values)] for match, values in match_dict.items()]
 
