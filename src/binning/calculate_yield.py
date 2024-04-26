4486bce11284a46d5364cf1cde8fa2ab23401524
Richard Trotta <trotta@cua.edu>
2024-04-25 20:45:12 -0400

    Testing different functional forms for xsect

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-25 03:39:36 trottar"
+# Time-stamp: "2024-04-25 20:43:00 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -142,7 +142,7 @@ def process_hist_data(tree_data, tree_dummy, t_bins, phi_bins, nWindows, phi_set
 
     hist_bin_dict = {}
 
-    # Pion subtraction by scaling simc to peak size
+    # Pion subtraction by scaling pion background to peak size
     if ParticleType == "kaon":
         from particle_subtraction import particle_subtraction_yield
         SubtractedParticle = "pion"
@@ -385,7 +385,7 @@ def process_hist_data(tree_data, tree_dummy, t_bins, phi_bins, nWindows, phi_set
             hist_bin_dict["H_MM_nosub_DUMMY_{}_{}".format(j, k)].Add(hist_bin_dict["H_MM_nosub_DUMMY_RAND_{}_{}".format(j, k)],-1)            
             hist_bin_dict["H_t_DUMMY_{}_{}".format(j, k)].Add(hist_bin_dict["H_t_DUMMY_RAND_{}_{}".format(j, k)],-1)
 
-            # Pion subtraction by scaling simc to peak size
+            # Pion subtraction by scaling pion background to peak size
             if ParticleType == "kaon":
                 
                 try:
@@ -565,7 +565,7 @@ def calculate_yield_data(kin_type, hist, t_bins, phi_bins, inpDict):
         # Scale the lists before subtraction        
         scaled_hist_val_data = [val * normfac_data for val in hist_val_data]
         scaled_hist_val_dummy = [val * normfac_dummy for val in hist_val_dummy]
-        #print("{}| Y_data = {:.5e}*{:.5e}={:.5e}".format(int(i/(len(t_bins) - 1)), np.sum(hist_val_data), normfac_data, np.sum(scaled_hist_val_data)))
+        print("{}| Y_data = {:.5e}*{:.5e}={:.5e} | {:.5e}".format(int(i/(len(t_bins) - 1)), np.sum(hist_val_data), normfac_data, np.sum(scaled_hist_val_data)))
         sub_val = np.subtract(scaled_hist_val_data, scaled_hist_val_dummy)
         total_count = np.sum(sub_val)
         try:
