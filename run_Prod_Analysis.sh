6e87f4d04bed8619f4bdccca8e4ef0bf3e6ddc95
Richard Trotta <trotta@cua.edu>
2024-05-02 11:34:14 -0400

    Testing new t-binning algorithm to assure most events are in lowest t

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -688,7 +688,7 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     #NumtBins=5
     #NumPhiBins=16
     #NumtBins=7 # Testing
-    NumtBins=9 # Testing
+    NumtBins=8 # Testing
     NumPhiBins=8 # Testing
     #TMIN=0.100
     #TMAX=1.000
