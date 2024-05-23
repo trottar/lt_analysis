ee398c1d8b07f6e9a049ded2751db7fde1d84da8
Richard Trotta <trotta@cua.edu>
2024-05-23 00:44:22 -0400

    Fixing plot of Q2/W dependence

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-05-23 00:43:03 trottar"
+# Time-stamp: "2024-05-23 00:44:20 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -579,9 +579,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
         f_sigLT_pre.SetParLimits(2, -par_lim, par_lim)    
     #f_sigLT_pre.SetParLimits(3, lt3-abs(lt3*par_lim_LTTT), lt3+abs(lt3*par_lim_LTTT))        
     # Fixing sigLT terms for testing
-    #f_sigLT_pre.FixParameter(0, lt0)
-    #f_sigLT_pre.FixParameter(1, lt1)
-    #f_sigLT_pre.FixParameter(2, lt2)
+    f_sigLT_pre.FixParameter(0, lt0)
+    f_sigLT_pre.FixParameter(1, lt1)
+    f_sigLT_pre.FixParameter(2, lt2)
     # Fixing sigLT terms to zero
     #f_sigLT_pre.FixParameter(0, 0.0)
     #f_sigLT_pre.FixParameter(1, 0.0)
@@ -665,9 +665,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
         f_sigLT.SetParLimits(2, -par_lim, par_lim)
     #f_sigLT.SetParLimits(3, lt3-abs(lt3*par_lim_LTTT), lt3+abs(lt3*par_lim_LTTT))
     # Fixing sigLT terms for testing
-    #f_sigLT.FixParameter(0, lt0)
-    #f_sigLT.FixParameter(1, lt1)
-    #f_sigLT.FixParameter(2, lt2)
+    f_sigLT.FixParameter(0, lt0)
+    f_sigLT.FixParameter(1, lt1)
+    f_sigLT.FixParameter(2, lt2)
     # Fixing sigLT terms to zero
     #f_sigLT.FixParameter(0, 0.0)
     #f_sigLT.FixParameter(1, 0.0)
@@ -749,7 +749,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     else: 
         f_sigTT_pre.SetParLimits(0, -par_lim, par_lim)
     # Fixing sigTT terms for testing
-    #f_sigTT_pre.FixParameter(0, tt0)
+    f_sigTT_pre.FixParameter(0, tt0)
     # Fixing sigTT terms to zero
     #f_sigTT_pre.FixParameter(0, 0.0)
     if DEBUG:
@@ -821,7 +821,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     else: 
         f_sigTT.SetParLimits(0, -par_lim, par_lim)
     # Fixing sigTT terms for testing
-    #f_sigTT.FixParameter(0, tt0)
+    f_sigTT.FixParameter(0, tt0)
     # Fixing sigTT terms to zero
     #f_sigTT.FixParameter(0, 0.0)
     if DEBUG:
