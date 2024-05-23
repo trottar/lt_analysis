e655bb8e06524d33e074fead97f0d0920538e9ca
Richard Trotta <trotta@cua.edu>
2024-05-23 12:47:57 -0400

    Fixing plot of Q2/W dependence

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-05-23 01:50:52 trottar"
+# Time-stamp: "2024-05-23 12:47:56 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -51,8 +51,8 @@ m_p = 0.93827231
 m_n = 0.93956541
 mkpl = 0.493677
 
-DEBUG=False
-#DEBUG=True
+#DEBUG=False
+DEBUG=True
 
 ##############
 # HARD CODED #
