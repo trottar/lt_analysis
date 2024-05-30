60e24d7816d1b5ee51a09b4259fb9417ab76d60e
Richard Trotta <trotta@cua.edu>
2024-05-30 00:50:55 -0400

    Fixing plot of Q2/W dependence

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-05-29 23:02:03 trottar"
+# Time-stamp: "2024-05-30 00:50:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -587,9 +587,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     #f_sigLT_pre.FixParameter(1, lt1)
     #f_sigLT_pre.FixParameter(2, lt2)
     # Fixing sigLT terms to zero
-    f_sigLT_pre.FixParameter(0, 0.0)
-    f_sigLT_pre.FixParameter(1, 0.0)
-    f_sigLT_pre.FixParameter(2, 0.0)
+    #f_sigLT_pre.FixParameter(0, 0.0)
+    #f_sigLT_pre.FixParameter(1, 0.0)
+    #f_sigLT_pre.FixParameter(2, 0.0)
     if DEBUG:
         f_sigLT_pre.FixParameter(0, lt0)
         f_sigLT_pre.FixParameter(1, lt1)
@@ -673,9 +673,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     #f_sigLT.FixParameter(1, lt1)
     #f_sigLT.FixParameter(2, lt2)
     # Fixing sigLT terms to zero
-    f_sigLT.FixParameter(0, 0.0)
-    f_sigLT.FixParameter(1, 0.0)
-    f_sigLT.FixParameter(2, 0.0)
+    #f_sigLT.FixParameter(0, 0.0)
+    #f_sigLT.FixParameter(1, 0.0)
+    #f_sigLT.FixParameter(2, 0.0)
     if DEBUG:
         f_sigLT.FixParameter(0, lt0)
         f_sigLT.FixParameter(1, lt1)
@@ -755,7 +755,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     # Fixing sigTT terms for testing
     #f_sigTT_pre.FixParameter(0, tt0)
     # Fixing sigTT terms to zero
-    f_sigTT_pre.FixParameter(0, 0.0)
+    #f_sigTT_pre.FixParameter(0, 0.0)
     if DEBUG:
         f_sigTT_pre.FixParameter(0, tt0)
     ##############
@@ -827,7 +827,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
     # Fixing sigTT terms for testing
     #f_sigTT.FixParameter(0, tt0)
     # Fixing sigTT terms to zero
-    f_sigTT.FixParameter(0, 0.0)
+    #f_sigTT.FixParameter(0, 0.0)
     if DEBUG:
         f_sigTT.FixParameter(0, tt0)    
     ##############
