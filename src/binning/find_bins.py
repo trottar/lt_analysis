d8ee93e95cecf5c006fbd7a5ab613310994c6879
Richard Trotta <trotta@cua.edu>
2024-06-19 17:43:17 -0400

    Testing parameterization for Q2=5.5

--- src/binning/find_bins.py
+++ src/binning/find_bins.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-06-19 17:39:40 trottar"
+# Time-stamp: "2024-06-19 17:43:15 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -226,7 +226,7 @@ def find_bins(histlist, inpDict):
         
         def adjust_bins(x, nbin, tolerance=1e-3, max_iterations=30, bad_bins_threshold=10):
             # Account for bin range
-            nbin += 1
+            #nbin += 1
             npt = len(x)  # Total number of data points
             n_per_bin = npt // nbin  # Calculate the number of events per bin
             remainder = npt % nbin  # Calculate remainder for uneven division
