5f745d9f638c89acfa660230ab76093082b52b0f
Richard Trotta <trotta@cua.edu>
2024-06-22 14:53:36 -0400

    Testing parameterization for Q2=5.5

--- src/binning/find_bins.py
+++ src/binning/find_bins.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-06-22 11:03:36 trottar"
+# Time-stamp: "2024-06-22 14:53:35 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -172,8 +172,8 @@ def find_bins(histlist, inpDict):
         # Set minimum threhold number of events per bin
         # Aim for >1000 events
         #bad_bins_threshold = 200
-        #bad_bins_threshold = 500
-        bad_bins_threshold = 1000
+        bad_bins_threshold = 500
+        #bad_bins_threshold = 1000
         #bad_bins_threshold = 2000
         ##############
         ##############
