c69eaf0d24b8928c19d474a0d4b64f6a910bbce3
Richard Trotta <trotta@cua.edu>
2024-06-26 15:56:41 -0400

    Checking Q2=5.5

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-06-26 15:42:25 trottar"
+# Time-stamp: "2024-06-26 15:56:28 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -42,8 +42,8 @@ def import_model(inp_model, arg_str):
                 #f = par[0] * math.exp(-par[1]*abs(tt)) * (1.0 / (1 + par[2]*qq))
                 # RLT (6/04/2024): Testing simplier exp form for L+T
                 ##
-                #f = (par[0]+par[1]*math.log(qq)) * math.exp(par[2] * (abs(tt)))
-                f = (par[0] * ((abs(tt)/qq)-1)) * math.exp(par[1] * (abs(tt)))
+                f = (par[0]+par[1]*math.log(qq)) * math.exp(par[2] * (abs(tt)))
+                #f = (par[0] * ((abs(tt)/qq)-1)) * math.exp(par[1] * (abs(tt)))
             except ValueError:
                 f = -1000.0
                 #print("WARNING: Overflowerror on sigL, setting to zero for this event...")
@@ -137,7 +137,7 @@ def import_model(inp_model, arg_str):
     sig_sep = sig_sep*g
 
     # Convert from ub/GeV**2 to nb/GeV**2
-    sig_sep = sig_sep*1e3
+    #sig_sep = sig_sep*1e3
     
     print("Model {} = {:.4e}".format(inp_model, sig_sep))
     
