41a163e2edc5cdfcb1d10cb293b4f5b7f0cbd814
Richard Trotta <trotta@cua.edu>
2024-06-28 13:48:33 -0400

    Checking Q2=5.5

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-06-28 13:45:18 trottar"
+# Time-stamp: "2024-06-28 13:48:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -187,9 +187,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     iteration = 0
     # Initialize adaptive parameter limits
-    par_lim_sigl_0 = random.uniform(0, max_iterations)
-    par_lim_sigl_1 = random.uniform(0, max_iterations)
-    par_lim_sigl_2 = random.uniform(0, max_iterations)
+    par_lim_sigl_0 = random.uniform(0, 1e3)
+    par_lim_sigl_1 = random.uniform(0, 1e3)
+    par_lim_sigl_2 = random.uniform(0, 1e3)
 
     # Store the parameter values and chi-square values for each iteration
     params_sigL_history = {'p1': [], 'p2': [], 'p3': []}
@@ -278,7 +278,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
                 sigl_X = (f_sigL.Eval(g_sigl.GetX()[i])) * (g_vec[i])
                 g_sigl_fit_tot.SetPoint(i, g_sigl.GetX()[i], sigl_X)
             # Options: S-> Simultaneous fit, M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
-            r_sigl_fit = g_sigl_fit.Fit(f_sigL, "S")
+            r_sigl_fit = g_sigl_fit.Fit(f_sigL, "SQ")
 
             f_sigL.Draw("same")
 
@@ -339,9 +339,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
             graph_fit_sigL_status = TGraph()
             
             # Adjust parameter limits within a random number
-            par_lim_sigl_0 = random.uniform(0, max_iterations)
-            par_lim_sigl_1 = random.uniform(0, max_iterations)
-            par_lim_sigl_2 = random.uniform(0, max_iterations)
+            par_lim_sigl_0 = random.uniform(0, 1e3)
+            par_lim_sigl_1 = random.uniform(0, 1e3)
+            par_lim_sigl_2 = random.uniform(0, 1e3)
 
             iteration = 0
 
@@ -390,8 +390,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     iteration = 0
     # Initialize adaptive parameter limits
-    par_lim_sigt_0 = random.uniform(0, max_iterations)
-    par_lim_sigt_1 = random.uniform(0, max_iterations)
+    par_lim_sigt_0 = random.uniform(0, 1e3)
+    par_lim_sigt_1 = random.uniform(0, 1e3)
 
     # Store the parameter values and chi-square values for each iteration
     params_sigT_history = {'p5': [], 'p6': []}
@@ -476,7 +476,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
                 sigt_X = (f_sigT.Eval(g_sigt.GetX()[i])) * (g_vec[i])
                 g_sigt_fit_tot.SetPoint(i, g_sigt.GetX()[i], sigt_X)
             # Options: S-> Simultaneous fit, M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
-            r_sigt_fit = g_sigt_fit.Fit(f_sigT, "S")
+            r_sigt_fit = g_sigt_fit.Fit(f_sigT, "SQ")
 
             # Check the fit status for 'f_sigT'
             #f_sigT_status = f_sigT.GetNDF()  # GetNDF() returns the number of degrees of freedom
@@ -533,8 +533,8 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
             graph_fit_sigT_status = TGraph()                
             
             # Adjust parameter limits within a random number
-            par_lim_sigt_0 = random.uniform(0, max_iterations)
-            par_lim_sigt_1 = random.uniform(0, max_iterations)
+            par_lim_sigt_0 = random.uniform(0, 1e3)
+            par_lim_sigt_1 = random.uniform(0, 1e3)
 
             iteration = 0
             
@@ -581,9 +581,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     iteration = 0
     # Initialize adaptive parameter limits
-    par_lim_siglt_0 = random.uniform(0, max_iterations)
-    par_lim_siglt_1 = random.uniform(0, max_iterations)
-    par_lim_siglt_2 = random.uniform(0, max_iterations)
+    par_lim_siglt_0 = random.uniform(0, 1e3)
+    par_lim_siglt_1 = random.uniform(0, 1e3)
+    par_lim_siglt_2 = random.uniform(0, 1e3)
 
     # Store the parameter values and chi-square values for each iteration
     params_sigLT_history = {'p9': [], 'p10': [], 'p11': []}
@@ -671,7 +671,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
                 siglt_X = (f_sigLT.Eval(g_siglt.GetX()[i]) * math.sin(th_vec[i] * PI / 180)) * (g_vec[i])
                 g_siglt_fit_tot.SetPoint(i, g_siglt.GetX()[i], siglt_X)
             # Options: S-> Simultaneous fit, M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
-            r_siglt_fit = g_siglt_fit.Fit(f_sigLT, "S")
+            r_siglt_fit = g_siglt_fit.Fit(f_sigLT, "SQ")
 
             # Check the fit status for 'f_sigLT'
             #f_sigLT_status = f_sigLT.GetNDF()  # GetNDF() returns the number of degrees of freedom
@@ -732,9 +732,9 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
             graph_fit_sigLT_status = TGraph()                
             
             # Adjust parameter limits within a random number
-            par_lim_siglt_0 = random.uniform(0, max_iterations)
-            par_lim_siglt_1 = random.uniform(0, max_iterations)
-            par_lim_siglt_2 = random.uniform(0, max_iterations)
+            par_lim_siglt_0 = random.uniform(0, 1e3)
+            par_lim_siglt_1 = random.uniform(0, 1e3)
+            par_lim_siglt_2 = random.uniform(0, 1e3)
 
             iteration = 0
             
@@ -783,7 +783,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
 
     iteration = 0
     # Initialize adaptive parameter limits
-    par_lim_sigtt_0 = random.uniform(0, max_iterations)
+    par_lim_sigtt_0 = random.uniform(0, 1e3)
 
     # Store the parameter values and chi-square values for each iteration
     params_sigTT_history = {'p13': []}
@@ -866,7 +866,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
                 sigtt_X = (f_sigTT.Eval(g_sigtt.GetX()[i]) * math.sin(th_vec[i] * PI / 180)**2) * (g_vec[i])
                 g_sigtt_fit_tot.SetPoint(i, g_sigtt.GetX()[i], sigtt_X)
             # Options: S-> Simultaneous fit, M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
-            r_sigtt_fit = g_sigtt_fit.Fit(f_sigTT, "S")
+            r_sigtt_fit = g_sigtt_fit.Fit(f_sigTT, "SQ")
 
             # Check the fit status for 'f_sigTT'
             #f_sigTT_status = f_sigTT.GetNDF()  # GetNDF() returns the number of degrees of freedom
@@ -919,7 +919,7 @@ def single_setting(ParticleType, pol_str, dir_iter, q2_set, w_set, tmin_range, t
             graph_fit_sigTT_status = TGraph()
 
             # Adjust parameter limits within a random number
-            par_lim_sigtt_0 = random.uniform(0, max_iterations)
+            par_lim_sigtt_0 = random.uniform(0, 1e3)
 
             iteration = 0
             
