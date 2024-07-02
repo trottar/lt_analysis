2a7d39d18111a11fe8f73787b4eb3bb2fd0caeee
Richard Trotta <trotta@cua.edu>
2024-07-02 02:26:22 -0400

    Checking Q2=5.5

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-02 01:31:09 trottar"
+# Time-stamp: "2024-07-02 02:26:20 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -83,8 +83,8 @@ def x_fit_in_t(ParticleType, pol_str, closest_date, Q2, W, inpDict):
     # HARD CODED #
     ##############
     # Maximum iterations before ending loop
-    max_iterations = 1000
-    #max_iterations = 10000
+    #max_iterations = 1000
+    max_iterations = 10000
     #max_iterations = int(1e6)
     ##############
     ##############
