7c0538e8735f87ae7da74b9e47ddc961e0c7611b
Richard Trotta <trotta@cua.edu>
2024-07-08 23:14:32 -0400

    Checking Q2=5.5

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-08 23:13:55 trottar"
+# Time-stamp: "2024-07-08 23:14:30 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -40,7 +40,6 @@ def fun_Sig_L(x, par):
     tt = abs(x[0])
     qq = float(q2_set.replace("p","."))
     try:
-        print("££££££££££££££")
         #print("Calculating function for func_SigL...\nQ2={:.1e}, t={:.3e}\npar=({:.2e}, {:.2e}, {:.2e}, {:.2e})\n\n".format(qq, tt, *par))
         # RLT (2/19/2024): Adding a 0.2 term to t dependence to bring down the extreme slope at high t
         # RLT (3/09/2024): Removing +0.2 term for better parameterization of Q2=3.0, W=2.32
@@ -129,7 +128,6 @@ def fun_Sig_TT(x, par):
     except OverflowError:
         f = -1000.0
         #print("WARNING: Overflowerror on sigTT, setting to zero for this event...")
-    return
-f
+    return f
 
 ###############################################################################################################################################
