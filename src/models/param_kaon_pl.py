a304133f1bc673ceabaab4a30038d5b7b44f6207
Richard Trotta <trotta@cua.edu>
2024-07-12 03:35:22 -0400

    Checking Q2=3.0

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-12 03:31:21 trottar"
+# Time-stamp: "2024-07-12 03:34:44 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -108,7 +108,7 @@ def iterWeight(arg_str):
         # RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
         #                  that incorporates Q2-dep based of pi FF
         #sigt=(p5/q2_gev)*math.exp(-p6*(q2_gev**2))
-        sigt=(p5*(1-(abs(t_gev)/q2_gev)))*math.exp(-p6*(q2_gev**2)*abs(t_gev))
+        sigt=(-p5*(abs(t_gev)/q2_gev))*math.exp(-p6*(q2_gev**2)*abs(t_gev))
 
     except OverflowError:        
         sigt = -1000.0
--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-12 03:30:54 trottar"
+# Time-stamp: "2024-07-12 03:34:59 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -91,7 +91,7 @@ def import_model(inp_model, arg_str):
                 # RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
                 #                  that incorporates Q2-dep based of pi FF
                 #f=(par[0]/qq)*math.exp(-par[1]*(qq**2))
-                f=(par[0]*(1-(abs(tt)/qq)))*math.exp(-par[1]*(qq**2)*abs(tt))
+                f=(-par[0]*(abs(tt)/qq))*math.exp(-par[1]*(qq**2)*abs(tt))
                 
             except ValueError:
                 f = -1000.0
