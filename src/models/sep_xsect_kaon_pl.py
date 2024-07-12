6e55e952858a579a9cb0a1e611682a4d9f4fbba7
Richard Trotta <trotta@cua.edu>
2024-07-12 03:24:13 -0400

    Checking Q2=3.0

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-12 03:20:14 trottar"
+# Time-stamp: "2024-07-12 03:23:57 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -91,7 +91,7 @@ def import_model(inp_model, arg_str):
                 # RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
                 #                  that incorporates Q2-dep based of pi FF
                 #f=(par[0]/qq)*math.exp(-par[1]*(qq**2))
-                f=(-par[0]/qq)*math.exp(-par[1]*(qq**2))*abs(tt)
+                f=(-ar[0]/qq)*math.exp(-par[1]*(qq**2))*abs(tt)**2
                 
             except ValueError:
                 f = -1000.0
