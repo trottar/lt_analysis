2ce6374c7ad3e88eb6a2ad19fd96e043bb36c421
Richard Trotta <trotta@cua.edu>
2024-07-19 13:25:19 -0400

    Testing Q2=2.11,3.0,5.5

--- src/utility/utility.py
+++ src/utility/utility.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-12 02:22:18 trottar"
+# Time-stamp: "2024-07-19 13:25:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -569,7 +569,7 @@ class PyFunc:
     def __call__(self, par):
         return chi2_func(par)
 
-minimizer = Math.Factory.CreateMinimizer("Minuit2", "Migrad")
+minimizer = ROOT.Math.Factory.CreateMinimizer("Minuit2", "Migrad")
 minimizer.SetMaxFunctionCalls(1000000)
 minimizer.SetMaxIterations(100000)
 minimizer.SetTolerance(0.001)
