c9d3397e47db8a601397c13a119696a8eb8ab536
Richard Trotta <trotta@cua.edu>
2024-07-30 15:55:41 -0400

    Testing Q2=2.11,3.0,5.5

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-29 21:14:11 trottar"
+# Time-stamp: "2024-07-30 15:55:38 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -111,7 +111,7 @@ def iterWeight(arg_str):
         #sigt=(p5/q2_gev)*math.exp(-p6*(q2_gev**2))
         Qdep_T=(math.exp(-q2_gev**2))/q2_gev
         #sigt=p5*(p6+math.exp(-p7*(abs(t_gev))))*(Qdep_T**p8)
-        sigt=(p5*math.exp(-p6*(abs(t_gev)))+p7*(abs(t_gev)))*(Qdep_T**p8)
+        sigt=(p5*math.exp(-p6*(abs(t_gev))*q2_gev)+p7*(abs(t_gev)))*(Qdep_T**p8)
 
     except OverflowError:        
         sigt = -1000.0
--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-27 02:02:52 trottar"
+# Time-stamp: "2024-07-30 15:55:09 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -105,7 +105,7 @@ def fun_Sig_T(x, par):
         #f=(par[0]/qq)*math.exp(-par[1]*(qq**2))
         Qdep_T=(math.exp(-qq**2))/qq
         ##f=par[0]*(par[1]+math.exp(-par[2]*(abs(tt))))*(Qdep_T**par[3])
-        f=(par[0]*math.exp(-par[1]*(abs(tt)))+par[2]*(abs(tt)))*(Qdep_T**par[3])
+        f=(par[0]*math.exp(-par[1]*(abs(tt))*qq)+par[2]*(abs(tt)))*(Qdep_T**par[3])
         
     except OverflowError:
         f = -1000.0
