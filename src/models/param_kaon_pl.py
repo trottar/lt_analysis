2fa95d4130aa6a7d6e1c18a2b4499f6c5c851659
Richard Trotta <trotta@cua.edu>
2024-07-30 16:26:24 -0400

    Testing Q2=2.11,3.0,5.5

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-30 16:10:59 trottar"
+# Time-stamp: "2024-07-30 16:26:18 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -109,7 +109,7 @@ def iterWeight(arg_str):
         # RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
         #                  that incorporates Q2-dep based of pi FF
         #sigt=(p5/q2_gev)*math.exp(-p6*(q2_gev**2))
-        Qdep_T=(math.exp(-q2_gev**2))/q2_gev
+        Qdep_T=(math.exp(-q2_gev))/q2_gev
         #sigt=p5*(p6+math.exp(-p7*(abs(t_gev))))*(Qdep_T**p8)
         sigt=(p5*math.exp(-p6*(abs(t_gev)))+p7*(abs(t_gev)))*(Qdep_T**p8)
 
