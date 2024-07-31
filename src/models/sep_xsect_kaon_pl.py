53f8eeeb3de7cdd4104f74b8934eb46e0718e50b
Richard Trotta <trotta@cua.edu>
2024-07-30 22:21:56 -0400

    Testing Q2=2.11,3.0,5.5

--- src/models/sep_xsect_kaon_pl.py
+++ src/models/sep_xsect_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-07-30 22:01:14 trottar"
+# Time-stamp: "2024-07-30 22:21:42 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -49,7 +49,8 @@ def import_model(inp_model, arg_str):
                 # RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
                 #                  that incorporates Q2-dep based of pi FF
                 ft = abs(tt) / (abs(tt) + mkpl**2)**2 # pole term
-                Qdep_L=qq/(1.0+(1.77*qq)+0.12*(qq**2))
+                ##Qdep_L=qq/(1.0+(1.77*qq)+0.12*(qq**2))
+                Qdep_L=(qq**2)/(1.0+(1.77*qq)+0.12*(qq**2))
                 ##f=(par[0]*Qdep_L*ft)*math.exp(-par[1]*(abs(tt)))
                 f=(par[0]*Qdep_L*ft)*math.exp(-par[1]*(abs(tt)))
                 
@@ -93,8 +94,7 @@ def import_model(inp_model, arg_str):
                 # RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
                 #                  that incorporates Q2-dep based of pi FF
                 #f=(par[0]/qq)*math.exp(-par[1]*(qq**2))
-                ##Qdep_T=(math.exp(-qq**2))/qq
-                Qdep_T=(math.exp(-qq**4))/qq
+                Qdep_T=(math.exp(-qq**2))/qq
                 ##f=par[0]*(par[1]+math.exp(-par[2]*(abs(tt))))*(Qdep_T**par[3])
                 f=(par[0]*math.exp(-par[1]*(abs(tt)))+par[2]*(abs(tt)))*(Qdep_T**par[3])
                 
