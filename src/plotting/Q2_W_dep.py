9b7cbc4fe1d38e3371601d97b348fe485e2653e8
Richard Trotta <trotta@cua.edu>
2024-08-13 23:09:03 -0400

    Running Q2=4.4, W=2.74

--- src/plotting/Q2_W_dep.py
+++ src/plotting/Q2_W_dep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-08-13 23:08:02 trottar"
+# Time-stamp: "2024-08-13 23:09:01 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -356,7 +356,7 @@ for tmin, tmax in tmin_tmax_pairs:
     # Use tmin and tmax in your calculations here
     print(f"tmin: {tmin:.3f}, tmax: {tmax:.3f}")
 
-    outputpdf = outputpdf.replace(f".pdf","_tmin{tmin}-tmax{tmax}.pdf")
+    outputpdf = outputpdf.replace(".pdf", f"_tmin{tmin}-tmax{tmax}.pdf")
     
     # Create a PdfPages object to manage the PDF file
     with PdfPages(outputpdf) as pdf:
