2f5279292af5538829b1800ac3e86fee403f98ca
Richard Trotta <trotta@cua.edu>
2024-08-13 23:31:02 -0400

    Running Q2=4.4, W=2.74

--- src/plotting/Q2_W_dep.py
+++ src/plotting/Q2_W_dep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-08-13 23:29:54 trottar"
+# Time-stamp: "2024-08-13 23:31:01 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -453,7 +453,7 @@ for tmin, tmax in tmin_tmax_pairs:
             #n = -2.0
             #n = -2.25 # From Phys. Rev. C 85, 018202 (2012), kaon
             #n = -2.41 # From Phys. Rev. C 85, 018202 (2012), pion
-            n = -3.0
+            n = -10.0
             w_scale_factor = ((W_ref**2-mkpl**2)**n)/((df['W']**2-mkpl**2)**n)
 
             scaled_sig = df['{}'.format(sig)]*w_scale_factor
@@ -534,7 +534,7 @@ for tmin, tmax in tmin_tmax_pairs:
             #n = -2.0
             #n = -2.25 # From Phys. Rev. C 85, 018202 (2012), kaon
             #n = -2.41 # From Phys. Rev. C 85, 018202 (2012), pion
-            n = -3.0
+            n = -10.0
             w_scale_factor = ((W_ref**2-mkpl**2)**n)/((df['W']**2-mkpl**2)**n)
 
             scaled_sig = df['{}'.format(sig)]*w_scale_factor
