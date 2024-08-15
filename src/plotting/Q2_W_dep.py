79831fbc8ab4b67182f63f89143404e971991703
Richard Trotta <trotta@cua.edu>
2024-08-15 10:39:38 -0400

    Running Q2=3.0, W=2.32

--- src/plotting/Q2_W_dep.py
+++ src/plotting/Q2_W_dep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-08-15 10:34:41 trottar"
+# Time-stamp: "2024-08-15 10:39:36 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -480,9 +480,9 @@ for tmin, tmax in tmin_tmax_pairs:
             df = merged_dict["sep_file"]
 
             W_ref = 3.0 # Scale data to W=3.0 GeV
-            n = -2.0
-            #n = -2.25 # From Phys. Rev. C 85, 018202 (2012), kaon
-            #n = -2.41 # From Phys. Rev. C 85, 018202 (2012), pion
+            n = 2.0
+            #n = 2.25 # From Phys. Rev. C 85, 018202 (2012), kaon
+            #n = 2.41 # From Phys. Rev. C 85, 018202 (2012), pion
             #w_scale_factor = ((W_ref**2-mkpl**2)**n)/((df['W']**2-mkpl**2)**n)
             w_scale_factor = ((W_ref**2-mkpl**2)**n)
 
@@ -584,9 +584,9 @@ for tmin, tmax in tmin_tmax_pairs:
             cut_str = f"t = [{tmin:.3f}, {tmax:.3f}]"
 
             W_ref = 3.0 # Scale data to W=3.0 GeV
-            n = -2.0
-            #n = -2.25 # From Phys. Rev. C 85, 018202 (2012), kaon
-            #n = -2.41 # From Phys. Rev. C 85, 018202 (2012), pion
+            n = 2.0
+            #n = 2.25 # From Phys. Rev. C 85, 018202 (2012), kaon
+            #n = 2.41 # From Phys. Rev. C 85, 018202 (2012), pion
             #w_scale_factor = ((W_ref**2-mkpl**2)**n)/((df['W']**2-mkpl**2)**n)
             w_scale_factor = ((W_ref**2-mkpl**2)**n)
 
