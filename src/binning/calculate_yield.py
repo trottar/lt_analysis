b43d311d3e7026b6e0fcadac588e1b06cd81467b
Richard Trotta <trotta@cua.edu>
2024-09-01 21:45:47 -0400

    Removed +pi from phi

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-01 21:34:57 trottar"
+# Time-stamp: "2024-09-01 21:45:46 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -579,7 +579,7 @@ def calculate_yield_data(kin_type, hist, t_bins, phi_bins, inpDict):
         bin_val_data, hist_val_data = data
         bin_val_dummy, hist_val_dummy = dummy
         # Find bin width
-        bin_width_simc = np.mean(np.diff(bin_val_simc))
+        bin_width_data = np.mean(np.diff(bin_val_data))
         # Scale the lists before subtraction        
         scaled_hist_val_data = [val * normfac_data for val in hist_val_data]
         scaled_hist_val_dummy = [val * normfac_dummy for val in hist_val_dummy]
