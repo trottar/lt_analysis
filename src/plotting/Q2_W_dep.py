ff42c30980f614f8c449cf404b764b9834f52eb9
Richard Trotta <trotta@cua.edu>
2024-09-12 01:40:46 -0400

    Updating all simc

--- src/plotting/Q2_W_dep.py
+++ src/plotting/Q2_W_dep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-12 01:33:21 trottar"
+# Time-stamp: "2024-09-12 01:40:43 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -626,8 +626,8 @@ for tmin, tmax in tmin_tmax_pairs:
 
             if (abs(df[non_zero_mask]['Q2'] - 4.4) < tolerance).any():
                 mask = abs(df[non_zero_mask]['Q2'] - 4.4) < tolerance
-                    # Use x_increment for x-axis values
-                    x_values = np.arange(0, len(ratios[non_zero_mask]))                
+                # Use x_increment for x-axis values
+                x_values = np.arange(0, len(ratios[non_zero_mask]))                
                 ax.errorbar(x_values[mask], ratios[mask], yerr=errors[mask], 
                             marker=markers[3], linestyle='', label='$Q^2$=4.4, W=2.74', 
                             color=colors[3], markeredgecolor=colors[3], 
@@ -635,8 +635,8 @@ for tmin, tmax in tmin_tmax_pairs:
 
             if (abs(df[non_zero_mask]['Q2'] - 5.5) < tolerance).any():
                 mask = abs(df[non_zero_mask]['Q2'] - 5.5) < tolerance
-                    # Use x_increment for x-axis values
-                    x_values = np.arange(0, len(ratios[non_zero_mask]))                
+                # Use x_increment for x-axis values
+                x_values = np.arange(0, len(ratios[non_zero_mask]))                
                 ax.errorbar(x_values[mask], ratios[mask], yerr=errors[mask], 
                             marker=markers[4], linestyle='', label='$Q^2$=5.5, W=3.02', 
                             color=colors[4], markeredgecolor=colors[4], 
