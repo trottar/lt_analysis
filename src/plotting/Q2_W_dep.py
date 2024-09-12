7ceb0d9b6542a22e7a7b6660bd0b57309a717586
Richard Trotta <trotta@cua.edu>
2024-09-11 21:58:22 -0400

    Updating all simc

--- src/plotting/Q2_W_dep.py
+++ src/plotting/Q2_W_dep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-11 21:57:39 trottar"
+# Time-stamp: "2024-09-11 21:58:20 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -585,8 +585,8 @@ for tmin, tmax in tmin_tmax_pairs:
 
             # Use integer division to get the correct subplot position
             ax = axes[k // 2, k % 2]
-            eps_val = "high" if eps_val == "hi" else "low"
-            ax.set_title(f"Ratio {eps_val}", fontsize=24)
+            eps_str = "high" if eps_val == "hi" else "low"
+            ax.set_title(f"Ratio {eps_str}", fontsize=24)
             df_ratio = merged_dict[f"aver_{eps_val}eps"]
             df = merged_dict[f"unsep_file_{eps_val}eps"]
 
