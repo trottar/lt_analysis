e8a719173052277971d3dc50e53d42753815a124
Richard Trotta <trotta@cua.edu>
2024-09-11 22:07:25 -0400

    Updating all simc

--- src/plotting/Q2_W_dep.py
+++ src/plotting/Q2_W_dep.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-11 22:03:18 trottar"
+# Time-stamp: "2024-09-11 22:07:23 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -590,11 +590,14 @@ for tmin, tmax in tmin_tmax_pairs:
             df_ratio = merged_dict[f"aver_{eps_val}eps"]
             df = merged_dict[f"unsep_file_{eps_val}eps"]
 
+            # Use x_increment for x-axis values
+            x_values = np.arange(0, len(ratios))
+            
             tolerance = 0.5
 
             if (abs(df['Q2'] - 2.115) < tolerance).any():
                 mask = abs(df['Q2'] - 2.115) < tolerance
-                ax.errorbar(df.loc[mask, 't'], df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
+                ax.errorbar(x_values, df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
                             marker=markers[0], linestyle='', label='$Q^2$=2.115, W=2.95', 
                             color=colors[0], markeredgecolor=colors[0], 
                             markerfacecolor='none', capsize=2)
@@ -602,32 +605,32 @@ for tmin, tmax in tmin_tmax_pairs:
             if (abs(df['Q2'] - 3.0) < tolerance).any():
                 if (abs(df['W'] - 2.32) < tolerance).any():
                     mask = (abs(df['Q2'] - 3.0) < tolerance) & (abs(df['W'] - 2.32) < tolerance)
-                    ax.errorbar(df.loc[mask, 't'], df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
+                    ax.errorbar(x_values, df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
                                 marker=markers[1], linestyle='', label='$Q^2$=3.0, W=2.32', 
                                 color=colors[1], markeredgecolor=colors[1], 
                                 markerfacecolor='none', capsize=2)
                 if (abs(df['W'] - 3.14) < tolerance).any():
                     mask = (abs(df['Q2'] - 3.0) < tolerance) & (abs(df['W'] - 3.14) < tolerance)
-                    ax.errorbar(df.loc[mask, 't'], df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
+                    ax.errorbar(x_values, df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
                                 marker=markers[2], linestyle='', label='$Q^2$=3.0, W=3.14', 
                                 color=colors[2], markeredgecolor=colors[2], 
                                 markerfacecolor='none', capsize=2)                    
 
             if (abs(df['Q2'] - 4.4) < tolerance).any():
                 mask = abs(df['Q2'] - 4.4) < tolerance
-                ax.errorbar(df.loc[mask, 't'], df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
+                ax.errorbar(x_values, df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
                             marker=markers[3], linestyle='', label='$Q^2$=4.4, W=2.74', 
                             color=colors[3], markeredgecolor=colors[3], 
                             markerfacecolor='none', capsize=2)
 
             if (abs(df['Q2'] - 5.5) < tolerance).any():
                 mask = abs(df['Q2'] - 5.5) < tolerance
-                ax.errorbar(df.loc[mask, 't'], df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
+                ax.errorbar(x_values, df_ratio["ratio"][mask], yerr=df_ratio["dratio"][mask], 
                             marker=markers[4], linestyle='', label='$Q^2$=5.5, W=3.02', 
                             color=colors[4], markeredgecolor=colors[4], 
                             markerfacecolor='none', capsize=2)
 
-            ax.set_xlabel('t')
+            ax.set_xlabel('t-$\phi$ bin')
             ax.set_ylabel("Ratio")
             ax.tick_params(axis='x', labelsize=16)
             ax.tick_params(axis='y', labelsize=16)        
