ae171fdd1af3f721cf6b6617ac64842fd611595e
Richard Trotta <trotta@cua.edu>
2024-09-17 14:22:59 -0400

    Testing for pion Q2=0.38, W=2.2

--- src/cuts/diamond.py
+++ src/cuts/diamond.py
@@ -264,13 +264,11 @@ def DiamondPlot(ParticleType, Q2Val, Q2min, Q2max, WVal, Wmin, Wmax, phi_setting
                             lbr = Q2vsW_lowe_cut.ProjectionY("y",b+fitr,b+fitr+1).FindLastBinAbove(1,1,fbr,lbr)-1
                         else:
                             check4 = True
-                        '''
                         if (fbl > lbl or fbr > lbr):                     
                             print("WARNING: Bad Fit! Refitting...If script hangs for too long, check lowe file or change Q2min/Q2max range! \n")
                             lowe_input = False
                             badfile = True
                             break
-                        '''
                     if (badfile == True):
                         break
                     minYl = Q2vsW_lowe_cut.ProjectionY("y",b+fitl,b+fitl+1).FindFirstBinAbove(minbin,1,fbl,lbl)/400*(Wmax-Wmin)+Wmin
@@ -320,8 +318,8 @@ def DiamondPlot(ParticleType, Q2Val, Q2min, Q2max, WVal, Wmin, Wmax, phi_setting
                 else:
                     print ("\n!!!!! Bad Diamond Fit!! Try Reducing fitrange or Increasing minbin and Retrying !!!!!\n")
                     fitrange -= 5
-                    #minbin -= 1
-                #badfit=False
+                    minbin -= 1
+                badfit=False
         
         
 
