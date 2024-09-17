19532fe12ce5715ea5e7326b0a5d31e32ec4e22b
Richard Trotta <trotta@cua.edu>
2024-09-17 16:42:40 -0400

    Testing for pion Q2=0.38, W=2.2

--- src/cuts/diamond.py
+++ src/cuts/diamond.py
@@ -236,7 +236,7 @@ def DiamondPlot(ParticleType, Q2Val, Q2min, Q2max, WVal, Wmin, Wmax, phi_setting
             minbin = 1
             badfile = False
             print("Q2Val Bin Val: ",Q2vsW_lowe_cut.FindBin(Q2Val))
-            fitl = Q2vsW_lowe_cut.FindBin(Q2Val)-fitrange
+            fitl = Q2vsW_lowe_cut.FindBin(Q2Val)-fitrange*2
             fitr = Q2vsW_lowe_cut.FindBin(Q2Val)+fitrange
             while (badfit == True):
                 lol.clear()
