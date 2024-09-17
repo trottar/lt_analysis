d7c4b05f1e1e901e07685db56de6e30de4b243de
Richard Trotta <trotta@cua.edu>
2024-09-17 13:30:12 -0400

    Testing for pion Q2=0.38, W=2.2

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-17 13:28:04 trottar"
+# Time-stamp: "2024-09-17 13:30:11 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -262,10 +262,10 @@ elif Q2Val == 5.5:
     inpDict["Wmin"] = WVal - (3/8)*WVal
     inpDict["Wmax"] = WVal + (3/8)*WVal
 elif Q2Val == 0.4: # Q2=0.38, pion
-    inpDict["Q2min"] = Q2Val - (2/10)*Q2Val
-    inpDict["Q2max"] = Q2Val + (2/10)*Q2Val
-    inpDict["Wmin"] = WVal - (2/10)*WVal
-    inpDict["Wmax"] = WVal + (2/10)*WVal    
+    inpDict["Q2min"] = Q2Val - (2/9)*Q2Val
+    inpDict["Q2max"] = Q2Val + (2/9)*Q2Val
+    inpDict["Wmin"] = WVal - (2/9)*WVal
+    inpDict["Wmax"] = WVal + (2/9)*WVal    
 else:
     inpDict["Q2min"] = Q2Val - (2/7)*Q2Val
     inpDict["Q2max"] = Q2Val + (2/7)*Q2Val
@@ -274,6 +274,10 @@ else:
 ##############
 ##############
 ##############
+
+print(f"Q2 range: ({inpDict['Q2min']},{inpDict['Q2max']})")
+print(f"W range: ({inpDict['Wmin']},{inpDict['Wmax']})")
+
 # Default starting values no need to change
 inpDict["Epsmin"] = 0.0
 inpDict["Epsmax"] = 1.0
