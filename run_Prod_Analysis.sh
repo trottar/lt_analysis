fe71044214d1404281af20d7746b8f01ec9ddd1e
Richard Trotta <trotta@cua.edu>
2024-09-19 15:50:43 -0400

    Testing for pion Q2=0.38, W=2.2

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -709,7 +709,7 @@ if [[ $Q2 = "0p4" && $W = "2p20" ]]; then
     NumtBins=5
     NumPhiBins=16
     TMIN=0.001
-    TMAX=0.050
+    TMAX=0.035
     
 elif [[ $Q2 = "2p1" && $W = "2p95" ]]; then
     # Q2=2p1, W=2p95
--- src/binning/find_bins.py
+++ src/binning/find_bins.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-19 15:45:55 trottar"
+# Time-stamp: "2024-09-19 15:50:34 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -251,17 +251,13 @@ def find_bins(histlist, inpDict):
         # Histogram takes the array data set and the bins as input
         # The bins are determined by an iterative algorithm (see function above)
         #print("H_t_BinTest: ", H_t_BinTest, type(H_t_BinTest))
-        for it in [10, 50, 100, 1000]:
-            try:
-                bin_edges = adjust_bins(H_t_BinTest, inpDict["NumtBins"], max_iterations=it)
-                break
-            except ValueError:
-                print("ERROR: Unavoidable empty bins. Tighten t-range or adjust number of t-bins...")
-                continue
-                #sys.exit(2)
+        try:
+            bin_edges = adjust_bins(H_t_BinTest, inpDict["NumtBins"], max_iterations=10)
+            n, bins = np.histogram(H_t_BinTest, bin_edges)
+        except ValueError:
+            print("ERROR: Unavoidable empty bins. Tighten t-range or adjust number of t-bins...")
+            sys.exit(2)
 
-        n, bins = np.histogram(H_t_BinTest, bin_edges)
-                
         # Check there are good t-bins
         if np.size(n) == 0:
             print("\n\nt-binning Failed: no valid bins avaliable!\nIncrease initial number of t-bins or change t-range...")
