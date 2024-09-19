d15e254d9e0ff7619d5d97ad65cba1e662072d87
Richard Trotta <trotta@cua.edu>
2024-09-19 11:26:36 -0400

    Testing for pion Q2=0.38, W=2.2

--- src/binning/ave_per_bin.py
+++ src/binning/ave_per_bin.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-19 11:05:53 trottar"
+# Time-stamp: "2024-09-19 11:26:07 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -153,8 +153,7 @@ def process_hist_data(tree_data, tree_dummy, t_bins, nWindows, phi_setting, inpD
         subDict = {}
 
     # Fit background and subtract
-    if ParticleType == "kaon":        
-        from background_fit import bg_fit
+    from background_fit import bg_fit
         
     # Loop through bins in t_data and identify events in specified bins
     for j in range(len(t_bins)-1):
@@ -438,13 +437,12 @@ def process_hist_data(tree_data, tree_dummy, t_bins, nWindows, phi_setting, inpD
             hist_bin_dict["H_MM_DATA_{}".format(j)].Add(subDict["H_MM_SUB_DATA_{}".format(j)],-1)
 
         # Fit background and subtract
-        if ParticleType == "kaon":
-            background_data_fit = bg_fit(phi_setting, inpDict, hist_bin_dict["H_MM_nosub_DATA_{}".format(j)])
-            hist_bin_dict["H_Q2_DATA_{}".format(j)].Add(background_data_fit[0], -1)
-            hist_bin_dict["H_W_DATA_{}".format(j)].Add(background_data_fit[0], -1)
-            hist_bin_dict["H_t_DATA_{}".format(j)].Add(background_data_fit[0], -1)
-            hist_bin_dict["H_epsilon_DATA_{}".format(j)].Add(background_data_fit[0], -1)
-            hist_bin_dict["H_MM_DATA_{}".format(j)].Add(background_data_fit[0], -1)
+        background_data_fit = bg_fit(phi_setting, inpDict, hist_bin_dict["H_MM_nosub_DATA_{}".format(j)])
+        hist_bin_dict["H_Q2_DATA_{}".format(j)].Add(background_data_fit[0], -1)
+        hist_bin_dict["H_W_DATA_{}".format(j)].Add(background_data_fit[0], -1)
+        hist_bin_dict["H_t_DATA_{}".format(j)].Add(background_data_fit[0], -1)
+        hist_bin_dict["H_epsilon_DATA_{}".format(j)].Add(background_data_fit[0], -1)
+        hist_bin_dict["H_MM_DATA_{}".format(j)].Add(background_data_fit[0], -1)
             
         processed_dict["t_bin{}".format(j+1)] = {
             "H_Q2_DATA" : remove_negative_bins(hist_bin_dict["H_Q2_DATA_{}".format(j)]),
