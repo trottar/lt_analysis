dc80d90cfebd49afe7d3db91494a6bf5ebe5720a
Richard Trotta <trotta@cua.edu>
2024-09-18 23:34:57 -0400

    Testing for pion Q2=0.38, W=2.2

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -707,9 +707,9 @@ MissMassMax=0.93 # M_Kaon/10 shifted
 # Pion Q2 = 0.38, Vijay
 if [[ $Q2 = "0p4" && $W = "2p20" ]]; then
     # Q2=0p4, W=2p20
-    NumtBins=3
+    NumtBins=8
     NumPhiBins=16
-    TMIN=0.010
+    TMIN=0.005
     TMAX=0.030
 elif [[ $Q2 = "2p1" && $W = "2p95" ]]; then
     # Q2=2p1, W=2p95
