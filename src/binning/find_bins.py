b5c1b42fe14b2604c70a5bb334d758d5aa1fa181
Richard Trotta <trotta@cua.edu>
2024-09-19 15:27:46 -0400

    Testing for pion Q2=0.38, W=2.2

--- src/binning/find_bins.py
+++ src/binning/find_bins.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-19 11:14:44 trottar"
+# Time-stamp: "2024-09-19 15:27:44 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -172,8 +172,8 @@ def find_bins(histlist, inpDict):
         # Set minimum threhold number of events per bin
         # Aim for >1000 events
         #bad_bins_threshold = 200
-        bad_bins_threshold = 500
-        #bad_bins_threshold = 1000
+        #bad_bins_threshold = 500
+        bad_bins_threshold = 1000
         #bad_bins_threshold = 2000
         ##############
         ##############
