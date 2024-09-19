0de8b4f269c22835baef2fe96c3d486a286ae6f6
Richard Trotta <trotta@cua.edu>
2024-09-18 22:16:03 -0400

    Testing for pion Q2=0.38, W=2.2

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-09-18 21:49:05 trottar"
+# Time-stamp: "2024-09-18 22:16:02 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -264,8 +264,8 @@ elif Q2Val == 5.5:
 elif Q2Val == 0.4: # Q2=0.38, pion
     inpDict["Q2min"] = 0.385 - (2/7)*0.385
     inpDict["Q2max"] = 0.385 + (2/7)*0.385
-    inpDict["Wmin"] = WVal - (2/7)*WVal
-    inpDict["Wmax"] = WVal + (2/7)*WVal
+    inpDict["Wmin"] = WVal - (1/9)*WVal
+    inpDict["Wmax"] = WVal + (1/9)*WVal
 else:
     inpDict["Q2min"] = Q2Val - (2/7)*Q2Val
     inpDict["Q2max"] = Q2Val + (2/7)*Q2Val
