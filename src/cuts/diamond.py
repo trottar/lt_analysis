a90e6fe556b0089b600c452bb51cf159697e9875
Richard Trotta <trotta@cua.edu>
2024-09-29 05:27:03 -0400

    Testing W dep of Q2=3.0, W=3.14

--- src/cuts/diamond.py
+++ src/cuts/diamond.py
@@ -78,7 +78,7 @@ def diamond_fit(Q2vsW_hist, Q2Val, fitrange=10):
         Misc.progressBar(b, fitrange*2-1, bar_length=25)
         
         # Left side
-        proj_l = Q2vsW_hist.ProjectionY("y", b+fitl, b+fitl-1)
+        proj_l = Q2vsW_hist.ProjectionY("y", b+fitl, b+fitl)
         minYl = proj_l.GetBinCenter(proj_l.FindFirstBinAbove(0))
         maxYl = proj_l.GetBinCenter(proj_l.FindLastBinAbove(0))
         lol.append(minYl)
