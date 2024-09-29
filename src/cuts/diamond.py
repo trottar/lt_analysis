b4b0519ef1d590f1f9e6205d4b4987f590772def
Richard Trotta <trotta@cua.edu>
2024-09-29 05:32:38 -0400

    Testing W dep of Q2=3.0, W=3.14

--- src/cuts/diamond.py
+++ src/cuts/diamond.py
@@ -69,7 +69,7 @@ def diamond_fit(Q2vsW_hist, Q2Val, fitrange=10):
     lol, hil, lor, hir = [], [], [], []
     xvl, xvr = [], []
     
-    fitl = Q2vsW_hist.GetXaxis().FindBin(Q2Val) - int(fitrange*10)
+    fitl = Q2vsW_hist.GetXaxis().FindBin(Q2Val) - int(fitrange*5)
     fitr = Q2vsW_hist.GetXaxis().FindBin(Q2Val) + int(fitrange)
 
     print("\nFinding diaomond fits...")
