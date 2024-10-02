9244da7a8fb154efae953bb39ae8f24876c5ef0d
Richard Trotta <trotta@cua.edu>
2024-10-01 23:06:01 -0400

    Testing W dep of Q2=3.0, W=3.14

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -724,8 +724,10 @@ elif [[ $Q2 = "3p0" && $W = "2p32" ]]; then
     # Q2=3p0, W=2p32
     NumtBins=8
     NumPhiBins=8
-    TMIN=0.400
-    TMAX=0.850
+    #TMIN=0.400
+    #TMAX=0.850
+    TMIN=0.100
+    TMAX=1.000
 elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     # Q2=3p0, W=3p14
     #NumtBins=6
