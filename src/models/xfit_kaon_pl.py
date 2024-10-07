d5713a95c165b3c7a217348f8dfb88d0b5b92803
Richard Trotta <trotta@cua.edu>
2024-10-07 05:53:07 -0400

    Testing dynamic model script

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-07 05:52:15 trottar"
+# Time-stamp: "2024-10-07 05:53:05 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -25,7 +25,7 @@ from utility import load_equations
 # Need to grab polarity and Q2 string values from xfit script
 
 # Check output equations
-DEBUG=False
+DEBUG=True
 
 # First, define empty strings
 pol_str = ""
