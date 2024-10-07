4acc49de68814d0884e60b6faeb098781987d5d2
Richard Trotta <trotta@cua.edu>
2024-10-07 07:58:18 -0400

    Testing dynamic model script

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-07 06:51:45 trottar"
+# Time-stamp: "2024-10-07 07:58:16 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -81,10 +81,10 @@ def x_fit_in_t(ParticleType, pol_str, closest_date, Q2, W, inpDict):
     # HARD CODED #
     ##############
     # Maximum iterations before ending loop
-    max_iterations = 2
+    #max_iterations = 2
     #max_iterations = 100
     #max_iterations = 500
-    #max_iterations = 1000
+    max_iterations = 1000
     #max_iterations = 5000
     #max_iterations = 10000
     ##############
