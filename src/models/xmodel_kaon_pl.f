48bb3a0cf56715a3588556759b03ef48d37f8ada
Richard Trotta <trotta@cua.edu>
2024-10-07 03:48:05 -0400

    Testing dynamic model script

--- src/models/xmodel_kaon_pl.f
+++ src/models/xmodel_kaon_pl.f
@@ -75,97 +75,19 @@ c     To calculate model cross-section, sig_T+eps*sig_L+ interfer._terms.
 *       RLT (10/8/2023): Testing new tav parameterization
       tav=(0.1112 + 0.0066*log(q2_set))*q2_set      
       f_tav=(abs(tm)-tav)/tav
-*     RLT (7/11/2024): Moved below for Q2dep func form
-*      f_tm=abs(tm)/(abs(tm)+mkpl**2)**2 ! pole factor
-      
-*     sig_L=(par(1)+par(2)*log(q2))*exp((par(3)
-*     >     +par(4)*log(q2))*(abs(tm)-0.2))
-*     RLT (10/12/2023): Removed 0.2 to keep things as simple as possible for
-*                       initial start parameterization
-*     RLT (2/19/2024): Adding a 0.2 term to t dependence to bring down the
-*                      extreme slope at high t
-*     RLT (3/09/2024): Removing +0.2 term for better parameterization of
-*                      Q2=3.0, W=2.32
-*      
-*      sig_L=(par(1)+par(2)*log(q2))*exp((par(3)
-*     >     +par(4)*log(q2))*(abs(tm)))
-*      sig_L=(par(1)+par(2)*log(q2))*exp((par(3)
-*     >     +par(4)*log(q2))*(abs(tm)+0.2))
-*     RLT (4/23/2024): Marco's thesis functional forms
-*     sig_L=par(1)*exp(-par(2)*abs(tm))*(1.0/(1.0+par(3)*q2))
-*     RLT (6/04/2024): Testing simplier exp form for L+T
-**
-**      sig_L=(par(1)+par(2)*log(q2))*exp(par(3)*(abs(tm)))
-*      sig_L=(par(1)*((abs(tm)/q2)-1))*exp(par(2)*(abs(tm)))      
-      
-*     RLT (2/15/2024): Removing t dependence from sig_T because it seems
-*                        to be driving poor sep xsects results
-*     RLT (2/20/2024): Added 1/Q^4 term to dampen sig_T
-*     RLT (2/21/2024): Using global analysis sig T model and params
-*     (https://journals.aps.org/prc/pdf/10.1103/PhysRevC.85.018202)
-*      
-*      sig_T=par(5)+par(6)*log(q2)+(par(7)+par(8)*log(q2))*f_tav
-*     sig_T=par(5)+par(6)*log(q2)
-*     sig_T=par(5)*log(q2)++par(6)/(q2**2)
-*      sig_T=par(5)/(1+par(6)*q2)
-*     RLT (4/20/2024): Adding in t-dependence
-*     sig_T=(par(5)/(1+par(6)*q2))*f_tav
-*     sig_T=(par(5)/(1+par(6)*q2))*abs(tm)
-*     RLT (4/20/2024): Exponential t-dependence
-*      sig_T=(par(5)/(1+par(6)*q2))*exp(par(7)*abs(tm))
-*     RLT (4/23/2024): Marco's thesis functional forms
-*     sig_T=par(5)*exp(-par(6)*abs(tm))*(1.0/(1.0+par(7)*q2))
-*     RLT (6/04/2024): Testing simplier exp form for L+T
-**      
-**      sig_T=(par(5)*((abs(tm)/q2)-1))*exp(par(6)*(abs(tm)))
-*      sig_T=(par(5)+par(6)*log(q2))*exp(par(7)*(abs(tm)))            
 
-**
-      sig_LT=(par(9)*exp(par(10)*abs(tm))+par(11)/abs(tm))*sin(thetacm)
-*     sig_LT=(par(9)+par(11)/abs(tm))*sin(thetacm)
-*     RLT (4/23/2024): Marco's thesis functional forms
-*      sig_LT=par(9)*exp(-par(10)*abs(tm))*(1.0/(1.0+(q2**2)*par(11)))
-*     RLT (1/2/2024): Need to have 16 parameters (4 for L/T/LT/TT) for
-*                     the xfit_in_t.py script to work. LT/TT are zeros
-*                     Therefore param 12 was also changed to 13
-**      
-**      sig_TT=(par(13)*q2*exp(-q2))*f_tm*sin(thetacm)**2
-*     RLT (4/23/2024): Marco's thesis functional forms
-*      sig_TT=par(13)*exp(-par(14)*abs(tm))*(1.0/(1.0+(q2**2)*par(15)))
-
-*     RLT (9/25/2023): There are two tav parameterizations in here.
-*                      I am only using the one above, for now.
-*      tav=(-0.178+0.315*log(q2))*q2
-
-**      
-*     RLT (7/11/2024): Redefined functional forms of L, T, LT, TT
-*                      that incorporates Q2-dep based of pi FF
       f_tm=abs(tm)/(abs(tm)+mkpl**2)**2 ! pole factor
       Qdep_L=q2/(1.0+(1.77*q2)+0.12*(q2**2))
-**      sig_L=(par(1)*Qdep_L*f_tm)*exp(-par(2)*(abs(tm)))
       sig_L=(par(1)*Qdep_L*f_tm)*exp(-par(2)*(abs(tm)))
-*     sig_T=(par(5)/q2)*exp(-par(6)*(q2**2))
       Qdep_T=(exp(-q2**2))/q2
-**      sig_T=par(5)*(par(6)+exp(-par(7)*(abs(tm))))*(Qdep_T**par(8))
       sig_T=(par(5)*exp(-par(6)*(abs(tm)))+par(7)*(abs(tm)))
      >     *(Qdep_T**par(8))
-***      sig_LT=(par(9)/(1+q2))*sin(thetacm)
-***     >     *exp(-par(10)*(abs(tm)))
-*      sig_LT=(par(9)*exp(par(10)*abs(tm))
-*     >     +par(11)/abs(tm))*sin(thetacm)
-*      sig_TT=(-par(13)/(1+q2))*(sin(thetacm)**2)
-*     >     *exp(-par(14)*(abs(tm)))
-***      sig_TT=(par(13)/(1+q2))*(sin(thetacm)**2)
-***   >     *f_tm*exp(-par(14)*(q2))
+      sig_LT=(par(9)*exp(par(10)*abs(tm))+par(11)/abs(tm))*sin(thetacm)      
       sig_TT=((-par(13)*abs(tm)+par(14))*(abs(tm)
      >     **(q2/par(15)))-par(16)*q2)*sin(thetacm)**2
       
 c     Correct for W.
-*      g_W=1./(W**2-targ**2)**2  ! W factor
-*      g_W=1./(W**2-targ**2)**2.45 ! Q2=5.5,W=3.02, Q2=2.115, W=2.95
-*      g_W=1./(W**2-targ**2)**2.65 ! W factor, Q2=4.4, W=2.74      
-*     g_W=1./(W**2-targ**2)**2.25       ! W factor, Q2=3.0, W=3.14
-      g_W=1./(W**2-targ**2)**3.4       ! W factor, Q2=3.0, W=2.32
+      g_W=1./(W**2-targ**2)**2  ! W factor
 
       wfactor=g_W
       sig_L=sig_L*wfactor
