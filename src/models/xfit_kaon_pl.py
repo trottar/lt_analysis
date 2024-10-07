4bf9d8036072d8462d5cc8f7cd452635b3a913e4
Richard Trotta <trotta@cua.edu>
2024-10-07 09:37:34 -0400

    Testing dynamic model script

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-07 09:36:33 trottar"
+# Time-stamp: "2024-10-07 09:37:28 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -35,16 +35,16 @@ fun_Sig_TT_optimized = None
 def prepare_equations(equations, sig_type):
     if sig_type == "sig_L":
         eq_list = [f"{k} = {v}" for k, v in equations.items() if k not in ('sig_T', 'sig_LT', 'sig_TT')]
-        func_str = f"def {sig_type}_optimized(q2_set, w_set, qq, ww, tt, p1, p2, p3, p4):\n"
+        func_str = f"def {sig_type}_optimized(q2_set, w_set, tt, p1, p2, p3, p4):\n"
     if sig_type == "sig_T":
         eq_list = [f"{k} = {v}" for k, v in equations.items() if k not in ('sig_L', 'sig_LT', 'sig_TT')]
-        func_str = f"def {sig_type}_optimized(q2_set, w_set, qq, ww, tt, p5, p6, p7, p8):\n"
+        func_str = f"def {sig_type}_optimized(q2_set, w_set, tt, p5, p6, p7, p8):\n"
     if sig_type == "sig_LT":
         eq_list = [f"{k} = {v}" for k, v in equations.items() if k not in ('sig_L', 'sig_T', 'sig_TT')]
-        func_str = f"def {sig_type}_optimized(q2_set, w_set, qq, ww, tt, p9, p10, p11, p12):\n"
+        func_str = f"def {sig_type}_optimized(q2_set, w_set, tt, p9, p10, p11, p12):\n"
     if sig_type == "sig_TT":
         eq_list = [f"{k} = {v}" for k, v in equations.items() if k not in ('sig_L', 'sig_T', 'sig_LT')]
-        func_str = f"def {sig_type}_optimized(q2_set, w_set, qq, ww, tt, p13, p14, p15, p16):\n"
+        func_str = f"def {sig_type}_optimized(q2_set, w_set, tt, p13, p14, p15, p16):\n"
     
     func_str += "    " + "\n    ".join(eq_list) + "\n"
     func_str += f"    return {sig_type}"
@@ -74,25 +74,25 @@ def fun_Sig_L(x, par):
     q2_set = float(Q2.replace("p","."))
     w_set = float(W.replace("p","."))
     p1, p2, p3, p4 = [par[i] if i < len(par) else 0.0 for i in range(4)]
-    return fun_Sig_L_optimized(q2_set, w_set, qq, ww, tt, p1, p2, p3, p4)
+    return fun_Sig_L_optimized(q2_set, w_set, tt, p1, p2, p3, p4)
 
 def fun_Sig_T(x, par):
     tt = abs(x[0])
     q2_set = float(Q2.replace("p","."))
     w_set = float(W.replace("p","."))
     p5, p6, p7, p8 = [par[i] if i < len(par) else 0.0 for i in range(4)]
-    return fun_Sig_T_optimized(q2_set, w_set, qq, ww, tt, p5, p6, p7, p8)
+    return fun_Sig_T_optimized(q2_set, w_set, tt, p5, p6, p7, p8)
 
 def fun_Sig_LT(x, par):
     tt = abs(x[0])
     q2_set = float(Q2.replace("p","."))
     w_set = float(W.replace("p","."))
     p9, p10, p11, p12 = [par[i] if i < len(par) else 0.0 for i in range(4)]
-    return fun_Sig_LT_optimized(q2_set, w_set, qq, ww, tt, p9, p10, p11, p12)
+    return fun_Sig_LT_optimized(q2_set, w_set, tt, p9, p10, p11, p12)
 
 def fun_Sig_TT(x, par):
     tt = abs(x[0])
     q2_set = float(Q2.replace("p","."))
     w_set = float(W.replace("p","."))
     p13, p14, p15, p16 = [par[i] if i < len(par) else 0.0 for i in range(4)]
-    return fun_Sig_TT_optimized(q2_set, w_set, qq, ww, tt, p13, p14, p15, p16)
+    return fun_Sig_TT_optimized(q2_set, w_set, tt, p13, p14, p15, p16)
