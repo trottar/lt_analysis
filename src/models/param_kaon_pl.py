182056a1c836ceba82e4b7f66e07c3636de9d731
Richard Trotta <trotta@cua.edu>
2024-10-07 03:20:36 -0400

    Testing dynamic model script

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-07 03:18:33 trottar"
+# Time-stamp: "2024-10-07 03:20:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -11,9 +11,6 @@
 # Copyright (c) trottar
 #
 import math, sys
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 ##################################################################################################################################################
 # Importing utility functions
@@ -24,8 +21,6 @@ from utility import load_equations
 ##################################################################################################################################################
 
 def iterWeight(arg_str):
-
-    DEBUG=False    
     
     # Split and convert the input string into a list of floats
     args = list(map(float, arg_str.split()))
@@ -36,8 +31,6 @@ def iterWeight(arg_str):
     
     # Load equations
     equations = load_equations(f"Q{str(q2_set).replace('.','p')}W{str(w_set).replace('.','p')}.model")
-    if DEBUG:    
-        logging.debug(f"Loaded equations: {equations}")
 
     ##############
     # HARD CODED #
@@ -52,19 +45,10 @@ def iterWeight(arg_str):
     local_vars = locals()
     for key, equation in equations.items():
         try:
-            if DEBUG:
-                logging.debug(f"Evaluating equation for {key}: {equation}")
             local_vars[key] = eval(equation, {"__builtins__": None, "math": math}, local_vars)
-            if DEBUG:
-                logging.debug(f"Result for {key}: {local_vars[key]}")
         except OverflowError:
-            logging.warning(f"OverflowError for {key}, setting to -1000.0")
+            print(f"OverflowError for {key}, setting to -1000.0")
             local_vars[key] = -1000.0
-        except Exception as e:
-            logging.error(f"Error evaluating equation for {key}: {equation}")
-            logging.error(f"Error message: {str(e)}")
-            logging.error(f"Local variables: {local_vars}")
-            raise
         
     pi, sig_L, sig_T, sig_LT, sig_TT, wfactor = [local_vars[key] for key in ['pi', 'sig_L', 'sig_T', 'sig_LT', 'sig_TT', 'wfactor']]
     
