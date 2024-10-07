23513f0b10e051216448eb8936986b83cd396909
Richard Trotta <trotta@cua.edu>
2024-10-07 06:12:43 -0400

    Testing dynamic model script

--- src/models/xfit_kaon_pl.py
+++ src/models/xfit_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-07 06:09:42 trottar"
+# Time-stamp: "2024-10-07 06:12:41 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -73,7 +73,7 @@ def fun_Sig_L(x, par):
     # Evaluate equations
     local_vars = locals()
     for key, equation in equations.items():
-        if key == 'sig_L':
+        if (key != 'sig_T') and (key != 'sig_LT') and (key != 'sig_TT'):
             try:
                 if DEBUG:
                     logging.debug(f"Evaluating equation for {key}: {equation}")
@@ -124,22 +124,23 @@ def fun_Sig_T(x, par):
     # Evaluate equations
     local_vars = locals()
     for key, equation in equations.items():
-        try:
-            if DEBUG:
-                logging.debug(f"Evaluating equation for {key}: {equation}")
-            local_vars[key] = eval(equation, {"__builtins__": None, "math": math}, local_vars)
-            if DEBUG:
-                logging.debug(f"Result for {key}: {local_vars[key]}")
-        except OverflowError:
-            logging.warning(f"OverflowError for {key}, setting to -1000.0")
-            local_vars[key] = -1000.0
-        except Exception as e:
-            logging.error(f"Error evaluating equation for {key}: {equation}")
-            logging.error(f"Error message: {str(e)}")
-            logging.error(f"Local variables: {local_vars}")
-            raise
+        if (key != 'sig_L') and (key != 'sig_LT') and (key != 'sig_TT'):
+            try:
+                if DEBUG:
+                    logging.debug(f"Evaluating equation for {key}: {equation}")
+                local_vars = eval(equation, {"__builtins__": None, "math": math}, local_vars)
+                if DEBUG:
+                    logging.debug(f"Result for {key}: {local_vars[key]}")
+            except OverflowError:
+                logging.warning(f"OverflowError for {key}, setting to -1000.0")
+                local_vars[key] = -1000.0
+            except Exception as e:
+                logging.error(f"Error evaluating equation for {key}: {equation}")
+                logging.error(f"Error message: {str(e)}")
+                logging.error(f"Local variables: {local_vars}")
+                raise
 
-    f = [local_vars[key] for key in ['sig_T']]
+    f = local_vars
 
     return f
 
@@ -173,22 +174,23 @@ def fun_Sig_LT(x, par):
     # Evaluate equations
     local_vars = locals()
     for key, equation in equations.items():
-        try:
-            if DEBUG:
-                logging.debug(f"Evaluating equation for {key}: {equation}")
-            local_vars[key] = eval(equation, {"__builtins__": None, "math": math}, local_vars)
-            if DEBUG:
-                logging.debug(f"Result for {key}: {local_vars[key]}")
-        except OverflowError:
-            logging.warning(f"OverflowError for {key}, setting to -1000.0")
-            local_vars[key] = -1000.0
-        except Exception as e:
-            logging.error(f"Error evaluating equation for {key}: {equation}")
-            logging.error(f"Error message: {str(e)}")
-            logging.error(f"Local variables: {local_vars}")
-            raise
+        if (key != 'sig_L') and (key != 'sig_T') and (key != 'sig_TT'):
+            try:
+                if DEBUG:
+                    logging.debug(f"Evaluating equation for {key}: {equation}")
+                local_vars = eval(equation, {"__builtins__": None, "math": math}, local_vars)
+                if DEBUG:
+                    logging.debug(f"Result for {key}: {local_vars[key]}")
+            except OverflowError:
+                logging.warning(f"OverflowError for {key}, setting to -1000.0")
+                local_vars[key] = -1000.0
+            except Exception as e:
+                logging.error(f"Error evaluating equation for {key}: {equation}")
+                logging.error(f"Error message: {str(e)}")
+                logging.error(f"Local variables: {local_vars}")
+                raise
 
-    f = [local_vars[key] for key in ['sig_LT']]
+    f = local_vars
 
     return f
 
@@ -222,22 +224,23 @@ def fun_Sig_TT(x, par):
     # Evaluate equations
     local_vars = locals()
     for key, equation in equations.items():
-        try:
-            if DEBUG:
-                logging.debug(f"Evaluating equation for {key}: {equation}")
-            local_vars[key] = eval(equation, {"__builtins__": None, "math": math}, local_vars)
-            if DEBUG:
-                logging.debug(f"Result for {key}: {local_vars[key]}")
-        except OverflowError:
-            logging.warning(f"OverflowError for {key}, setting to -1000.0")
-            local_vars[key] = -1000.0
-        except Exception as e:
-            logging.error(f"Error evaluating equation for {key}: {equation}")
-            logging.error(f"Error message: {str(e)}")
-            logging.error(f"Local variables: {local_vars}")
-            raise
+        if (key != 'sig_L') and (key != 'sig_T') and (key != 'sig_LT'):
+            try:
+                if DEBUG:
+                    logging.debug(f"Evaluating equation for {key}: {equation}")
+                local_vars = eval(equation, {"__builtins__": None, "math": math}, local_vars)
+                if DEBUG:
+                    logging.debug(f"Result for {key}: {local_vars[key]}")
+            except OverflowError:
+                logging.warning(f"OverflowError for {key}, setting to -1000.0")
+                local_vars[key] = -1000.0
+            except Exception as e:
+                logging.error(f"Error evaluating equation for {key}: {equation}")
+                logging.error(f"Error message: {str(e)}")
+                logging.error(f"Local variables: {local_vars}")
+                raise
 
-    f = [local_vars[key] for key in ['sig_TT']]
+    f = local_vars
 
     return f
 
