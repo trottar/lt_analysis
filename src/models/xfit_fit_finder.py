c6991a97a6dfe9714b216eb1d7602bc36e6c0a97
Richard Trotta <trotta@cua.edu>
2024-10-11 05:21:18 -0400

    Testing fix for number of parameters greater than number of data points

--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-11 05:00:24 trottar"
+# Time-stamp: "2024-10-11 05:21:15 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -92,7 +92,7 @@ def find_fit(sig_fit_dict, inp_dict, par_vec, par_err_vec, par_chi2_vec):
             # Prompts selection of specific parameter to fit for this iteration
             initial_params = select_valid_parameter(sig_name, val["params"])
             # Remove used parameter, but keep track of unused for further iterations
-            original_params = val["params"]
+            original_params = val["params"].copy()
             original_params.remove(initial_params[0])
             num_params = len(initial_params)
 
