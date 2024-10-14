34526cf2b65c6ada5a3d0eb698fd5412fc5f7582
Richard Trotta <trotta@cua.edu>
2024-10-14 16:49:04 -0400

    Testing fix for number of parameters greater than number of data points

--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-14 16:48:04 trottar"
+# Time-stamp: "2024-10-14 16:49:03 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -1737,6 +1737,7 @@ def find_fit(inp_dict, par_vec, par_err_vec, par_chi2_vec):
                                     residual = (observed - expected) / g_sig.GetEY()[i] if g_sig.GetEY()[i] != 0 else (observed - expected)
                                     residuals.append(residual)                                
                                 mse = np.mean(np.square(residuals))
+                                print("GRGE")
                                 l2_reg = sum(p**2 for p in [current_params])
                                 print("GRGE")
                                 current_cost_try = mse + lambda_try * l2_reg
