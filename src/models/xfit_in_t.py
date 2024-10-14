5e9cefbe592cdca2125ad244b1d7090f569d918e
Richard Trotta <trotta@cua.edu>
2024-10-14 12:48:43 -0400

    Testing fix for number of parameters greater than number of data points

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-14 12:47:10 trottar"
+# Time-stamp: "2024-10-14 12:48:41 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -81,8 +81,7 @@ def x_fit_in_t(ParticleType, pol_str, closest_date, Q2, W, inpDict):
     max_iterations = 1000
 
     # Number of times to run the algorithm
-    num_optimizations = 2
-    #num_optimizations = 100
+    num_optimizations = 100
     ##############
     ##############
     ##############
