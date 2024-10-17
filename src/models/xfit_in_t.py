69a3351950bf00a40f5842509cb9b9065a708f95
Richard Trotta <trotta@cua.edu>
2024-10-17 18:34:21 -0400

    Testing fix for number of parameters greater than number of data points

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-17 09:29:06 trottar"
+# Time-stamp: "2024-10-17 18:34:17 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -87,9 +87,9 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     # Sine exponent (sin(theta* PI / 180)**sine_exp_LT/TT) of LT, TT
     #sine_exp_LT = 0.0 # Off
     sine_exp_LT = 1.0 # Default
-    #sine_exp_TT = 0.0 # Off
+    sine_exp_TT = 0.0 # Off
     #sine_exp_TT = 1.0 # Trial 34
-    sine_exp_TT = 2.0 # Default
+    #sine_exp_TT = 2.0 # Default
     #sine_exp_TT = 3.0 # Trial 35
     ##############
     ##############
