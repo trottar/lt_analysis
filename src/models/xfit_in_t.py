a0d50bca551f3139d630d9bbbf251920b166fe2f
Richard Trotta <trotta@cua.edu>
2024-10-26 05:38:01 -0400

    Testing xfit algorithm improvements.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-26 05:36:03 trottar"
+# Time-stamp: "2024-10-26 05:37:58 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -83,7 +83,7 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     max_iterations = 5000
 
     # Number of times to run the algorithm
-    num_optimizations = 100
+    num_optimizations = 10
 
     # Initial max/min bounds of finding parameter values
     initial_param_bounds = 1e6
