91e9d9063dae4ee0554c40781396272447cd6393
Richard Trotta <trotta@cua.edu>
2024-10-26 04:38:28 -0400

    Testing xfit algorithm improvements.

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-10-25 10:42:38 trottar"
+# Time-stamp: "2024-10-26 04:38:26 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -79,7 +79,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     # HARD CODED #
     ##############
     # Maximum iterations before ending loop
-    max_iterations = 1000
+    #max_iterations = 1000
+    max_iterations = 5000
 
     # Number of times to run the algorithm
     num_optimizations = 10
