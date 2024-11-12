a8bcc2fcef02c693ef4f283509d2fe71c6206a37
Richard Trotta <trotta@cua.edu>
2024-11-12 12:00:48 -0500

    Testing new binning scheme Q2=3.0,W=3.14

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -741,7 +741,7 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     NumtBins=20
     NumPhiBins=15    
     TMIN=0.150
-    TMAX=0.500
+    TMAX=0.450
 elif [[ $Q2 = "4p4" && $W = "2p74" ]]; then
     # Q2=4p4, W=2p74
     NumtBins=6
