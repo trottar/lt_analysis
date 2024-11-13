f0441fd7faaf7afa14eaf8719bf32335a7119f6f
Richard Trotta <trotta@cua.edu>
2024-11-13 03:27:12 -0500

    Testing new binning scheme Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-11-12 17:06:27 trottar"
+# Time-stamp: "2024-11-13 03:27:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -183,11 +183,10 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
 
     # Find fits for L, T, LT, TT
     fit_params = {
+        #"L": [l0, l1, l2, l3],
+        #"T": [t0, t1, t2, t3],
+        #"LT": [lt0, lt1, lt2, lt3],
         "TT": [tt0, tt1, tt2, tt3],
-        "L": [l0, l1, l2, l3],
-        "T": [t0, t1, t2, t3],
-        "LT": [lt0, lt1, lt2, lt3],
-        #"TT": [tt0, tt1, tt2, tt3],
     }
     
     inp_dict = {
