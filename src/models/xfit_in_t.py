138d3e13c15a2917c9061f6788bae7a908506589
Richard Trotta <trotta@cua.edu>
2024-11-13 13:11:11 -0500

    Testing new binning scheme Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-11-13 13:01:19 trottar"
+# Time-stamp: "2024-11-13 13:10:58 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -85,8 +85,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     #max_iterations = 5000
 
     # Number of times to run the algorithm
-    #num_optimizations = 10
-    num_optimizations = 50
+    num_optimizations = 10
+    #num_optimizations = 50
 
     # Initial max/min bounds of finding parameter values
     initial_param_bounds = 1e6
