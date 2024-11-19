33f623eb28b95ac98b834bc6fda6f1aa83aecebb
Richard Trotta <trotta@cua.edu>
2024-11-19 00:58:36 -0500

    Testing new binning scheme Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-11-18 14:56:01 trottar"
+# Time-stamp: "2024-11-19 00:58:34 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -94,9 +94,9 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
 
     # Threshold on how bad red. chi2 can be
     #chi2_threshold = 1.0
-    #chi2_threshold = 6.0
+    chi2_threshold = 6.0
     #chi2_threshold = 10.0
-    chi2_threshold = 600.0
+    #chi2_threshold = 600.0
     ##############
     ##############
     ##############
