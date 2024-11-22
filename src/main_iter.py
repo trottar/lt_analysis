b1ae701b343a11770f4ba499aa9a507e2fd69e70
Richard Trotta <trotta@cua.edu>
2024-11-22 00:28:07 -0500

    Updating cache path because its read only now

--- src/main_iter.py
+++ src/main_iter.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-11-22 00:17:11 trottar"
+# Time-stamp: "2024-11-22 00:28:05 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -141,11 +141,12 @@ print("\n\tIteration number", iter_num)
 print("="*50)
 print("\n\n")
 
-# Save this as the directory to grab further information
-prev_iter_dir_cache = "{}/{}/{}/Q{}W{}/{}".format(CACHEPATH, USER, ParticleType.lower(), Q2, W, closest_date)
-prev_iter_dir = "{}/{}/Q{}W{}/{}".format(TEMP_CACHEPATH, ParticleType.lower(), Q2, W, closest_date)
-print("\nCopying {} to {}".format(prev_iter_dir_cache, prev_iter_dir))
-shutil.copytree(prev_iter_dir_cache, prev_iter_dir, symlinks=False, ignore=None, dirs_exist_ok=False)
+if EPSSET == "low":
+    # Save this as the directory to grab further information
+    prev_iter_dir_cache = "{}/{}/{}/Q{}W{}/{}".format(CACHEPATH, USER, ParticleType.lower(), Q2, W, closest_date)
+    prev_iter_dir = "{}/{}/Q{}W{}/{}".format(TEMP_CACHEPATH, ParticleType.lower(), Q2, W, closest_date)
+    print("\nCopying {} to {}".format(prev_iter_dir_cache, prev_iter_dir))
+    shutil.copytree(prev_iter_dir_cache, prev_iter_dir, symlinks=False, ignore=None, dirs_exist_ok=False)
 
 if not os.path.exists(prev_iter_dir):
     print("\n\n\tERROR: {} does not exist...".format(prev_iter_dir))
