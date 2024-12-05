b541d60edc53cf0aa402cabecde44408cc5e46a3
Richard Trotta <trotta@cua.edu>
2024-12-05 06:37:57 -0500

    Figuring out why cuts are not working for data

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -741,7 +741,7 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     NumtBins=12
     NumPhiBins=15    
     TMIN=0.150
-    TMAX=0.750
+    TMAX=0.600
 elif [[ $Q2 = "4p4" && $W = "2p74" ]]; then
     # Q2=4p4, W=2p74
     #NumtBins=6
