7a54205bdf963eecf7d6de9d1f4562fdc76293b7
Richard Trotta <trotta@cua.edu>
2024-12-11 03:31:57 -0500

    Figuring out why cuts are not working for data

--- src/check_ratios.py
+++ src/check_ratios.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-11 03:30:33 trottar"
+# Time-stamp: "2024-12-11 03:31:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -60,7 +60,7 @@ for f in [aver_hi_file, aver_lo_file]:
     print(lines)
 
     # Filter out lines that contain '*'
-    lines = [f'   0.0000   0.0000  {line[-6]}' if '*' in line else line for line in lines]
+    lines = [f'   0.0000   0.0000  {line[-5:]}' if '*' in line else line for line in lines]
 
     num_lines = len(lines)
     
