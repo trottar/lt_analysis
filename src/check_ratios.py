ee95e41125108c44ad4b4378f67d1e9ec2927614
Richard Trotta <trotta@cua.edu>
2024-12-11 02:52:16 -0500

    Figuring out why cuts are not working for data

--- src/check_ratios.py
+++ src/check_ratios.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-11 02:49:55 trottar"
+# Time-stamp: "2024-12-11 02:52:14 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -47,8 +47,8 @@ inp_W = sys.argv[4]
 inp_loeps = sys.argv[5]
 inp_hieps = sys.argv[6]
 
-aver_lo_file = '{}/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, inp_pid, inp_pol, inp_Q2.replace(".",""), inp_W.replace(".",""), float(inp_loeps)*100)
-aver_hi_file = '{}/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, inp_pid, inp_pol, inp_Q2.replace(".",""), inp_W.replace(".",""), float(inp_hieps)*100)
+aver_lo_file = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, inp_pid, inp_pol, inp_Q2.replace(".",""), inp_W.replace(".",""), float(inp_loeps)*100)
+aver_hi_file = '{}/src/{}/averages/aver.{}_Q{}W{}_{:.0f}.dat'.format(LTANAPATH, inp_pid, inp_pol, inp_Q2.replace(".",""), inp_W.replace(".",""), float(inp_hieps)*100)
 
 for f in [aver_hi_file, aver_lo_file]:
 
