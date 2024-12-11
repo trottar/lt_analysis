1f43ef13de33181622abe05e7638ab2136cc75ea
Richard Trotta <trotta@cua.edu>
2024-12-11 06:07:59 -0500

    Figuring out why cuts are not working for data

--- src/setup/compare_iterations.py
+++ src/setup/compare_iterations.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-11 06:07:29 trottar"
+# Time-stamp: "2024-12-11 06:07:57 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -383,6 +383,8 @@ def compare_iters(pol_str, ParticleType, Q2, W, LOEPS, HIEPS):
 
                 date_df = iter_data["date"]
                 param_df  = iter_data["params"]
+
+                print("!!!!!!!!!",date_df,param_df)
                 
                 # Extract values for specific sets
                 for key, values in settings.items():
@@ -390,7 +392,7 @@ def compare_iters(pol_str, ParticleType, Q2, W, LOEPS, HIEPS):
                     # Unpack values into variables
                     Q2, W, LOEPS, HIEPS, param_arr, date = values.values()
 
-                    if date_df[0] == date:
+                    if date_df == date:
                     
                         # Collect parameters
                         param_subset = param_arr[:len(param_arr)]  # Adjust if needed
