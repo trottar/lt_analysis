aadc2afeb45d3dd66f90c546e08d6cdd0943f5f4
Richard Trotta <trotta@cua.edu>
2024-12-11 07:09:25 -0500

    Figuring out why cuts are not working for data

--- src/setup/compare_iterations.py
+++ src/setup/compare_iterations.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-11 06:57:55 trottar"
+# Time-stamp: "2024-12-11 07:09:23 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -409,7 +409,7 @@ def compare_iters(pol_str, ParticleType, Q2, W, LOEPS, HIEPS):
 
             # 1. Parameter Evolution Plot
             fig = plt.figure(figsize=(12, 6))
-            for i in range(k, k+4):  # Maximum of 4 parameters per L, T, LT, TT
+            for i in range(4*k+1, 4*(k+1)):  # Maximum of 4 parameters per L, T, LT, TT
                 param_evolution = [params[i] for params in params_values]
                 plt.plot(dates, param_evolution, label=f'Parameter {i}', marker='o')
 
