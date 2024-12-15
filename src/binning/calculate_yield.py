0648ddf13a783fcef1305b468dae7d8077c24cbe
Richard Trotta <trotta@cua.edu>
2024-12-15 01:40:51 -0500

    Testing new t-range/binning for Q2=3.0,W=3.14

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-15 01:17:40 trottar"
+# Time-stamp: "2024-12-15 01:40:49 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -483,9 +483,9 @@ def process_hist_data(tree_data, tree_dummy, normfac_data, normfac_dummy, t_bins
 
     # Checks for first plots and calls +'(' to Print
     canvas_iter = 0
-    total_plots = (len(t_bins)-1) * (len(phi_bins)-1) * len(list(["H_MM_DATA_{}_{}".format(j, k), "H_t_DATA_{}_{}".format(j, k), "H_MM_DUMMY_{}_{}".format(j, k), "H_t_DUMMY_{}_{}".format(j, k)])) # '-1' to remove t-phi bin edges
+    total_plots = (len(t_bins)-1) * (len(phi_bins)-1) * len(list(["H_MM_DATA_{}_{}".format(j, k), "H_t_DATA_{}_{}".format(j, k), "H_MM_DUMMY_{}_{}".format(j, k), "H_t_DUMMY_{}_{}".format(j, k)]))-1 # '-1' to remove t-phi bin edges
 
-    print("!!!!!!!!!",total_plots, "=", len(t_bins), len(phi_bins), len(list(["H_MM_DATA_{}_{}".format(j, k), "H_t_DATA_{}_{}".format(j, k), "H_MM_DUMMY_{}_{}".format(j, k), "H_t_DUMMY_{}_{}".format(j, k)])))
+    print("!!!!!!!!!",total_plots, "=", len(t_bins)-1, len(phi_bins)-1, len(list(["H_MM_DATA_{}_{}".format(j, k), "H_t_DATA_{}_{}".format(j, k), "H_MM_DUMMY_{}_{}".format(j, k), "H_t_DUMMY_{}_{}".format(j, k)])))
     
     # Loop through bins in t_data and identify events in specified bins
     for j in range(len(t_bins)-1):
@@ -863,7 +863,7 @@ def process_hist_simc(tree_simc, normfac_simc, t_bins, phi_bins, phi_setting, in
     canvas_iter = 0
     total_plots = (len(t_bins)-1) * (len(phi_bins)-1) * len(list(["H_MM_SIMC", "H_t_SIMC"]))-1 # '-1' to remove t-phi bin edges
 
-    print("!!!!!!!!!",total_plots, "=", len(t_bins), len(phi_bins), len(list(["H_MM_SIMC", "H_t_SIMC"]))-1) 
+    print("!!!!!!!!!",total_plots, "=", len(t_bins)-1, len(phi_bins)-1, len(list(["H_MM_SIMC", "H_t_SIMC"]))-1) 
 
     # Loop through bins in t_simc and identify events in specified bins
     for j in range(len(t_bins)-1):
