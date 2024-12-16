ba3d4040909119bf42d25ccb3f1ebf5086dafca1
Richard Trotta <trotta@cua.edu>
2024-12-16 12:48:13 -0500

    Testing new t-range/binning for Q2=3.0,W=3.14

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-16 12:14:37 trottar"
+# Time-stamp: "2024-12-16 12:48:11 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -97,6 +97,7 @@ def iterWeight(arg_str):
              math.sqrt(2.0 * eps * (1. + eps)) * math.cos(phi_cm) * sig_LT)
 
     ##sig = sig / 2.0 / math.pi / 1e6  # dsig/dtdphicm in microbarns/MeV**2/rad
+    sig = 1e3 * (sig / 2.0 / math.pi / 1e6)  # dsig/dtdphicm in microbarns/MeV**2/rad
     #sig = sig / 2.0 / math.pi  # dsig/dtdphicm in microbarns/GeV**2/rad
     
     wtn = weight_prev_iter * (sig / sig_prev_iter)
