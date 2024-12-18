ed22a81705223119fbc72e0a07d3f70270a5bfda
Richard Trotta <trotta@cua.edu>
2024-12-18 11:50:10 -0500

    Testing new t-range/binning for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-18 02:21:17 trottar"
+# Time-stamp: "2024-12-18 11:47:03 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -87,15 +87,15 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     ##############
     # Maximum iterations before ending loop
     #max_iterations = 100
-    max_iterations = 1000
-    #max_iterations = 2000
+    #max_iterations = 1000
+    max_iterations = 2000
     #max_iterations = 5000
     #max_iterations = 10000
 
     # Number of times to run the algorithm
     #num_optimizations = 5
-    num_optimizations = 10
-    #num_optimizations = 50
+    #num_optimizations = 10
+    num_optimizations = 50
 
     # Initial max/min bounds of finding parameter values
     initial_param_bounds = 1e6
