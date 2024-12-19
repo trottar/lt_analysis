1fa4710734f1860e130ab1df6756836d97898e26
Richard Trotta <trotta@cua.edu>
2024-12-19 08:45:33 -0500

    Testing new t-range/binning for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-19 08:44:34 trottar"
+# Time-stamp: "2024-12-19 08:45:31 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -242,13 +242,13 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
         # Don't find new fits if debugging
         if key not in fixed_params:        
             # Find optimized fits
-            c2, c3, c4, c5, c6 = find_fit(inp_dict, graph_dict, canvas_dict, par_vec, par_err_vec, par_chi2_vec, it, key, val)            
+            c2, c3, c4, c5, c6 = find_fit(inp_dict, graph_dict, par_vec, par_err_vec, par_chi2_vec, it, key, val)            
         else:
             for j in range(4):
                 par_vec[4*it+j] = prv_par_vec[4*it+j]
                 par_err_vec[4*it+j] = prv_err_vec[4*it+j]
                 par_chi2_vec[4*it+j] = prv_chi2_vec[4*it+j]
-            c2, c3, c4, c5, c6 = plot_fit(inp_dict, graph_dict, canvas_dict, par_vec, par_err_vec, par_chi2_vec, it, key, val)
+            c2, c3, c4, c5, c6 = plot_fit(inp_dict, graph_dict, par_vec, par_err_vec, par_chi2_vec, it, key, val)
             
     c2.Print(outputpdf+'(')
     c3.Print(outputpdf)
