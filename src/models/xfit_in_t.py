a83584410b6fe902fb6325dc8fc511425c522d09
Richard Trotta <trotta@cua.edu>
2024-12-20 16:31:47 -0500

    Testing new t-range/binning for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-20 16:18:38 trottar"
+# Time-stamp: "2024-12-20 16:31:38 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -80,7 +80,7 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     ##############
     #fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
     #fixed_params = ["L", "LT", "TT"]
-    fixed_params = ["L", "TT"]
+    fixed_params = ["TT"]
     #fixed_params = [] # Update all
     
     # Maximum iterations before ending loop
