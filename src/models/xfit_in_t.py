020526824ebf00fdfe33494c61c87ddb62a243b0
Richard Trotta <trotta@cua.edu>
2024-12-21 22:13:18 -0500

    Testing new t-range/binning for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-21 21:31:22 trottar"
+# Time-stamp: "2024-12-21 22:13:15 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -222,12 +222,17 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     
     parameterize(inp_dict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_err_vec, prv_chi2_vec, fixed_params)
     bad_chi2_bool, bad_chi2_indices = check_chi_squared_values(par_chi2_vec, chi2_threshold, fit_params, equations)
-    
-    while bad_chi2_bool:
+
+    i = 0
+    while bad_chi2_bool or i>10:
         fixed_params = ["L", "T", "LT", "TT"]
         fixed_params = [x for i, x in enumerate(fixed_params) if i not in bad_chi2_indices]            
         parameterize(inp_dict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_err_vec, prv_chi2_vec, fixed_params)
         bad_chi2_bool, bad_chi2_indices = check_chi_squared_values(par_chi2_vec, chi2_threshold, fit_params, equations)
+        i +=1
+
+    if i > 10:
+        sys.exit(2)
 
     # Check if parameter values changed and print changes to terminal
     for i, (old, new) in enumerate(zip(prv_par_vec, par_vec)):
