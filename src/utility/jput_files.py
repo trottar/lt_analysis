cdf1c67aae2046272f30a709ff9fab4fbc1e3e2c
Richard Trotta <trotta@cua.edu>
2024-12-28 12:11:52 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/utility/jput_files.py
+++ src/utility/jput_files.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-28 12:10:55 trottar"
+# Time-stamp: "2024-12-28 12:11:51 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -63,15 +63,15 @@ def run_jput_in_batches(source_dir, dest_prefix, batch_size=100):
     :param dest_prefix: Target directory prefix for the stub.
     :param batch_size: Number of files per batch.
     """
-    file_batches = list(generate_file_batches(source_dir, batch_size))
+    file_batches = list(generate_file_batches(source_dir, batch_size))[0]
     for i, batch in enumerate(file_batches):
         # Progress bar
-        Misc.progressBar(i, len(file_batches[0])-1, bar_length=25)
+        Misc.progressBar(i, len(file_batches)-1, bar_length=25)
         command = ["jput", "-r", source_dir, dest_prefix] + batch
         try:
             #print(" ".join(command))
-            #subprocess.run(command, check=True)
-            subprocess.run(" ".join(command), check=True)
+            subprocess.run(command, check=True)
+            #subprocess.run(" ".join(command), check=True)
             print(f"Batch with {len(batch)} files uploaded successfully.")
         except subprocess.CalledProcessError as e:
             print(f"Error executing jput command for batch: {e}")
