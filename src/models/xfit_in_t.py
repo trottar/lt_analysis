50423b8f443142df80cf793f4d63e659ece15c29
Richard Trotta <trotta@cua.edu>
2024-12-29 23:15:50 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-29 22:37:18 trottar"
+# Time-stamp: "2024-12-29 23:15:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -101,8 +101,9 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
 
     # Threshold on how bad red. chi2 can be
     #chi2_threshold = 1.0
+    chi2_threshold = 5.0
     #chi2_threshold = 10.0
-    chi2_threshold = 30.0
+    #chi2_threshold = 30.0
     #chi2_threshold = 600.0
     ##############
     ##############
