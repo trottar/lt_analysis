420f6b1b137a567fb827ccda0daa7201c67bc5f0
Richard Trotta <trotta@cua.edu>
2024-12-30 15:53:10 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-30 15:47:14 trottar"
+# Time-stamp: "2024-12-30 15:53:09 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -100,12 +100,7 @@ def iterWeight(arg_str):
     
     sig = sig / 2.0 / math.pi / 1e6  # dsig/dtdphicm in microbarns/MeV**2/rad
 
-    if sig_prev_iter == sig:
-        wtn = weight_prev_iter    
-    elif sig_prev_iter > sig:
-        wtn = weight_prev_iter * (sig_prev_iter / sig)
-    else:
-        wtn = weight_prev_iter * (sig / sig_prev_iter)
+    wtn = weight_prev_iter * (sig / sig_prev_iter)
         
     #print("sig",sig)
     #print("sigcm",sig_prev_iter)
