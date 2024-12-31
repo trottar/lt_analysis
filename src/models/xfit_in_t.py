c1e219c530dc2d18687880db25287191acdfb49e
Richard Trotta <trotta@cua.edu>
2024-12-31 18:41:31 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-31 18:08:54 trottar"
+# Time-stamp: "2024-12-31 18:41:28 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -264,6 +264,10 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     par_vec = best_par_vec
     par_err_vec = best_err_vec
     par_chi2_vec = best_chi2_vec
+
+    prv_par_vec = par_vec
+    prv_err_vec = par_err_vec
+    prv_chi2_vec = par_chi2_vec
     
     # Update plots with best chi2
     fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
