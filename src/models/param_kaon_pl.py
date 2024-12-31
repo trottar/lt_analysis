774cd7fab9583a7ba60c3e7b075c9e849b6bb68b
Richard Trotta <trotta@cua.edu>
2024-12-31 03:55:01 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-31 03:50:45 trottar"
+# Time-stamp: "2024-12-31 03:54:59 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -96,6 +96,7 @@ def iterWeight(arg_str):
     sig_L = fun_Sig_L_optimized(q2_set, w_set, qq, ww, tt, theta_cm, par1, par2, par3, par4)
     sig_T = fun_Sig_T_optimized(q2_set, w_set, qq, ww, tt, theta_cm, par5, par6, par7, par8)
     sig_LT = fun_Sig_LT_optimized(q2_set, w_set, qq, ww, tt, theta_cm, par9, par10, par11, par12)
+    print("!!!!!!!!!!!",q2_set, w_set, qq, ww, tt, theta_cm, par13, par14, par15, par16
     sig_TT = fun_Sig_TT_optimized(q2_set, w_set, qq, ww, tt, theta_cm, par13, par14, par15, par16)
     
     # Calculate W-factor
