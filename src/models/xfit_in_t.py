fe997abd44a93b52aa45ee0e491465841f2e9f34
Richard Trotta <trotta@cua.edu>
2024-12-31 18:08:57 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-12-31 17:54:52 trottar"
+# Time-stamp: "2024-12-31 18:08:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -243,7 +243,7 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
 
     if not DEBUG:
         i = 0
-        max_checks = 1
+        max_checks = 3
         while bad_chi2_bool and i < max_checks:
             fixed_params = ["L", "T", "LT", "TT"]
             fixed_params = [x for i, x in enumerate(fixed_params) if i not in bad_chi2_indices]
