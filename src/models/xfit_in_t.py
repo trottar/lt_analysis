d087f6cc160ed5e0d6b90983f726ddd56589423e
Richard Trotta <trotta@cua.edu>
2025-01-01 16:39:36 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-01 16:27:01 trottar"
+# Time-stamp: "2025-01-01 16:39:34 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -93,8 +93,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
     #max_iterations = 10000
 
     # Number of times to run the algorithm
-    #num_optimizations = 5
-    num_optimizations = 10
+    num_optimizations = 5
+    #num_optimizations = 10
     #num_optimizations = 50
     #num_optimizations = 1000
 
@@ -255,7 +255,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
 
             # Update best values for each group of 4 elements
             for j in range(0, len(par_chi2_vec), 4):
-                if np.abs(np.mean(par_chi2_vec[j:j+4]) - 1) < np.abs(np.mean(best_chi2_vec[j:j+4]) - 1):                    
+                print("!!!!!!!!!",np.abs(np.mean(par_chi2_vec[j:j+4]) - 1), "<" ,np.abs(np.mean(best_chi2_vec[j:j+4]) - 1))
+                if np.abs(np.mean(par_chi2_vec[j:j+4]) - 1) < np.abs(np.mean(best_chi2_vec[j:j+4]) - 1):
                     print(f"\n\nNew set of best chi2 values found for sig_{list(fit_params.keys())[j // 4]} of {par_chi2_vec[j]:.1f}...")
                     best_par_vec[j:j+4] = par_vec[j:j+4].copy()
                     best_err_vec[j:j+4] = par_err_vec[j:j+4].copy()
