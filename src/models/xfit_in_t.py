50fe67676f1ca61aaf29186870ae29ef824985b8
Richard Trotta <trotta@cua.edu>
2025-01-03 19:44:39 -0500

    Added jput script for Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-01 19:22:12 trottar"
+# Time-stamp: "2025-01-03 19:44:37 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -74,8 +74,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict):
 
     iter_num = inpDict["iter_num"]
     
-    DEBUG=False
-    #DEBUG=True
+    #DEBUG=False
+    DEBUG=True
     
     ##############
     # HARD CODED #
