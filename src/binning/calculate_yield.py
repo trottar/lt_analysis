6fcec761448db5d077433ffb9b778f1ad9066605
Richard Trotta <trotta@cua.edu>
2025-01-07 14:15:55 -0500

    Optimizing Q2=3.0,W=3.14

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-07 13:11:41 trottar"
+# Time-stamp: "2025-01-07 14:15:54 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -707,7 +707,7 @@ def calculate_yield_data(kin_type, hist, t_bins, phi_bins, inpDict):
             yld = total_count # Normalization applied above
             # Calculate experimental yield error (relative error)
             #yld_err = np.sqrt(data_charge_err**2+(1/np.sqrt(np.sum(hist_val_data)))**2)
-            yld_err = np.sqrt(data_charge_err**2+(1/np.sqrt(np.sum(sub_val/normfac_data)))**2)
+            yld_err = np.sqrt(data_charge_err**2+(1/np.sqrt(np.sum(sub_val)))**2)
             # Convert to absolute error (required for average_ratio.f)
             yld_err = yld_err*yld
         except ZeroDivisionError:
@@ -1064,7 +1064,7 @@ def calculate_yield_simc(kin_type, hist, t_bins, phi_bins, inpDict, iteration):
             # Calculate simc yield error (relative error)
             # No norm_fac, shouldn't normalize non-weighted distribution
             #yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))
-            yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]/normfac_simc))
+            yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i] * normfac_simc))
             # Convert to absolute error (required for average_ratio.f)
             yld_err = yld_err*yld
         except ZeroDivisionError:
