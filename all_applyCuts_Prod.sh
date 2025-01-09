b16bcdbc36606826608a5b62302c9cc170b918bd
Richard Trotta <trotta@cua.edu>
2025-01-08 20:42:59 -0500

    Optimizing Q2=3.0,W=3.14

--- all_applyCuts_Prod.sh
+++ all_applyCuts_Prod.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-08 20:38:54 trottar"
+# Time-stamp: "2025-01-08 20:42:53 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -66,24 +66,24 @@ grab_runs () {
 #Q2="3p0"
 #W="2p32"
 # DONE
-Q2="3p0"
-W="3p14"
+#Q2="3p0"
+#W="3p14"
 # DONE
 #Q2="4p4"
 #W="2p74"
 # ONGOING **
-#Q2="5p5"
-#W="3p02"
+Q2="5p5"
+W="3p02"
 
 KIN="Q${Q2}W${W}"
 
-#TARGET=("LH2" "dummy")
-#EPS=("high" "low")
-#PHISET=("center" "left" "right")
+TARGET=("LH2" "dummy")
+EPS=("high" "low")
+PHISET=("center" "left" "right")
 
-TARGET=("dummy")
-EPS=("low")
-PHISET=("left")
+#TARGET=("dummy")
+#EPS=("low")
+#PHISET=("left")
 
 for t in "${TARGET[@]}"; do
     for e in "${EPS[@]}"; do
