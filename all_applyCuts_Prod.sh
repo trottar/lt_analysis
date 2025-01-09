32567e31d9445d7e5ea9907344efc66ab2af89ef
Richard Trotta <trotta@cua.edu>
2025-01-08 20:15:48 -0500

    Optimizing Q2=3.0,W=3.14

--- all_applyCuts_Prod.sh
+++ all_applyCuts_Prod.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-08 20:00:55 trottar"
+# Time-stamp: "2025-01-08 20:15:36 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -66,24 +66,24 @@ grab_runs () {
 #Q2="3p0"
 #W="2p32"
 # DONE
-#Q2="3p0"
-#W="3p14"
+Q2="3p0"
+W="3p14"
 # DONE
-Q2="4p4"
-W="2p74"
+#Q2="4p4"
+#W="2p74"
 # ONGOING **
 #Q2="5p5"
 #W="3p02"
 
 KIN="Q${Q2}W${W}"
 
-TARGET=("LH2" "dummy")
-EPS=("high" "low")
-PHISET=("center" "left" "right")
+#TARGET=("LH2" "dummy")
+#EPS=("high" "low")
+#PHISET=("center" "left" "right")
 
-#TARGET=("dummy")
-#EPS=("low")
-#PHISET=("center")
+TARGET=("data")
+EPS=("low")
+PHISET=("left")
 
 for t in "${TARGET[@]}"; do
     for e in "${EPS[@]}"; do
@@ -126,7 +126,6 @@ for t in "${TARGET[@]}"; do
 			    echo "Running ${number}"
 			    cd "${LTANAPATH}"
 			    rootfile=/cache/hallc/kaonlt/Pass3_Dec_2023/ROOTfiles/Analysis/KaonLT/Kaon_coin_replay_production_${number}_-1.root
-			    # Using the test command with -e option
 			    ./applyCuts_Prod.sh -pm ${e} ${p} ${Q2} ${W} ${t} ${number} kaon
 			    if test -e $rootfile; then
 				echo
