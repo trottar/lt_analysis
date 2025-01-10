96441bf6e35e2087a79c81bb769a41acd4cc7732
Richard Trotta <trotta@cua.edu>
2025-01-10 10:06:27 -0500

    Optimizing Q2=3.0,W=3.14

---
+++ src/models/.#Q3p0W3p14.model
@@ -0,0 +1,1 @@
+trottar@trottar.14218:1734079740
--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-09 17:17:23 trottar"
+# Time-stamp: "2025-01-10 10:06:25 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -1589,7 +1589,7 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_e
                                 f_sig.SetParameter(1, current_params[1])
                                 f_sig.SetParameter(2, current_params[2])
                                 f_sig.SetParameter(3, current_params[3])
-                                if iter_num == 2:
+                                if iter_num == 0:
                                     f_sig.SetParLimits(0, -max_param_bounds, max_param_bounds)                                
                                     f_sig.SetParLimits(2, -max_param_bounds, max_param_bounds)
                                     f_sig.SetParLimits(3, -max_param_bounds, max_param_bounds)
