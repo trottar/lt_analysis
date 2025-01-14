3f5ba939ce157c9ee405ad29d7f3b751910a4ddb
Richard Trotta <trotta@cua.edu>
2025-01-14 13:42:40 -0500

    Optimizing all Q2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-14 13:40:45 trottar"
+# Time-stamp: "2025-01-14 13:42:39 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -88,10 +88,9 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
     fixed_params = [] # Update all
     
     # Maximum iterations before ending loop (should always aim for >5000)
-
-    max_iterations = 2000
+    #max_iterations = 2000
     #max_iterations = 5000
-    #max_iterations = 10000
+    max_iterations = 10000
 
     # Number of times to run the algorithm
     num_optimizations = 5
