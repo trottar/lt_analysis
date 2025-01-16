9052ab3fac78130478ed47bba29258e5d22d5f56
Richard Trotta <trotta@cua.edu>
2025-01-15 22:33:42 -0500

    Optimizing all Q2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-15 17:20:33 trottar"
+# Time-stamp: "2025-01-15 22:33:40 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -81,24 +81,19 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
     full_optimization = True
 
     # Fixed separated xsect parameterization
-    #fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
+    fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
     #fixed_params = ["T", "LT", "TT"]
     #fixed_params = ["L", "LT"]
     #fixed_params = ["TT"]
-    fixed_params = [] # Update all
+    #fixed_params = [] # Update all
     
-    # Maximum iterations before ending loop (should always aim for >5000)
-    #max_iterations = 2000
-    #max_iterations = 5000
+    # Maximum iterations before ending loop (should always aim for >10000)
     #max_iterations = 10000
     max_iterations = 15000
 
     # Number of times to run the algorithm
     num_optimizations = 5
     #num_optimizations = 10
-    #num_optimizations = 50
-    #num_optimizations = 100
-    #num_optimizations = 1000
 
     # Initial max/min bounds of finding parameter values (only used for iter=1)
     initial_param_bounds = 1e4
