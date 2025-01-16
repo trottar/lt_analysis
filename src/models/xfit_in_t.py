ee4ee62a2cbf279e94b9f372f3025dee32662bc1
Richard Trotta <trotta@cua.edu>
2025-01-16 13:35:21 -0500

    Optimizing all Q2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-15 22:33:40 trottar"
+# Time-stamp: "2025-01-16 13:30:22 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -284,7 +284,7 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
         for i in range(len(par_vec)):
             f.write("{:13.5e} {:13.5e} {:3d} {:12.1f}\n".format(par_vec[i], par_err_vec[i], i+1, par_chi2_vec[i]))
             print("  {:.3e} {:.3e} {:.1e} {:.1e}".format(par_vec[i], par_err_vec[i], i+1, par_chi2_vec[i]))
-
+    
     print("\n\nWould you like to continue with the analysis?\n")
     if not request_yn_response():
         print("-"*25)
