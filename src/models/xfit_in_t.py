d845e0555d9ccf265f6fddb991af7c50f161e4f0
Richard Trotta <trotta@cua.edu>
2025-01-24 01:54:33 -0500

    Optimizing all Q2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-22 12:33:44 trottar"
+# Time-stamp: "2025-01-24 01:54:26 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -81,7 +81,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
     full_optimization = True
 
     # Fixed separated xsect parameterization
-    fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
+    #fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
+    fixed_params = ["LT", "T", "L", "TT"]
     #fixed_params = ["T", "LT", "TT"]
     #fixed_params = ["L", "LT"]
     #fixed_params = ["TT"]
