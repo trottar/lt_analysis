095fea5724aad7fdb71993f39ce91750ebbc893c
Richard Trotta <trotta@cua.edu>
2025-01-26 11:20:00 -0500

    Optimizing all Q2

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-01-26 11:17:29 trottar"
+# Time-stamp: "2025-01-26 11:19:11 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -112,7 +112,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
     #chi2_threshold = 600.0
 
     # Number of rechecks to assure chi2_threshold not reached (set to zero to ignore chi2_threshold)
-    max_checks = 1
+    #max_checks = 1
+    max_checks = 0
     
     ##############
     ##############
