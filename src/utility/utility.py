babab9e2317dd047d10ea592a071a597ebcce7ed
Richard Trotta <trotta@cua.edu>
2025-02-03 00:33:55 -0500

    Optimizing all Q2

--- src/utility/utility.py
+++ src/utility/utility.py
@@ -2,7 +2,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-03 00:32:30 trottar"
+# Time-stamp: "2025-02-03 00:33:53 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -947,7 +947,7 @@ def calculate_cost(f_sig, g_sig, current_params, num_events, num_params, lambda_
             chi_square = f_sig.GetChisquare()
             nu = f_sig.GetNDF()
             cost = (chi_square + lambda_val * l2_reg) / nu
-            print("!!!!!!!",chi_square, nu)
+            print("!!!!!!!",chi_square, lambda_val, l2_reg, nu)
         return cost
 
     residuals = calculate_residuals()
