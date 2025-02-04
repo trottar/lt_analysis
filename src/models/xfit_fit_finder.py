b5c0032c05b235aaf310fa1a754570ca82aed4a6
Richard Trotta <trotta@cua.edu>
2025-02-04 01:33:00 -0500

    Optimizing all Q2

--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-04 00:32:22 trottar"
+# Time-stamp: "2025-02-04 01:32:58 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -228,8 +228,8 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec,
                                 g_sig.SetPointError(i_data, 0, y_err)
 
                             for i_pt in range(len(w_vec)):
-                                sig_X_fit = g_sig.GetY()[i_pt]# / (g_vec[i_pt])
-                                sig_X_fit_err = g_sig.GetEY()[i_pt]# / (g_vec[i_pt])
+                                sig_X_fit = g_sig.GetY()[i_pt] / (g_vec[i_pt])
+                                sig_X_fit_err = g_sig.GetEY()[i_pt] / (g_vec[i_pt])
                                 g_sig_fit.SetPoint(i_pt, g_sig.GetX()[i_pt], sig_X_fit)
                                 g_sig_fit.SetPointError(i_pt, 0, sig_X_fit_err)
 
@@ -395,8 +395,8 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec,
                 g_sig.SetPoint(i, nsep.GetV2()[i], nsep.GetV1()[i])
                 g_sig.SetPointError(i, 0, nsep.GetV3()[i])
             for i in range(len(w_vec)):
-                sig_X_fit = g_sig.GetY()[i]# / (g_vec[i])
-                sig_X_fit_err = g_sig.GetEY()[i]# / (g_vec[i])
+                sig_X_fit = g_sig.GetY()[i] / (g_vec[i])
+                sig_X_fit_err = g_sig.GetEY()[i] / (g_vec[i])
                 g_sig_fit.SetPoint(i, g_sig.GetX()[i], sig_X_fit)
                 g_sig_fit.SetPointError(i, 0, sig_X_fit_err)
             c2.cd(it+1).SetLeftMargin(0.12)
@@ -539,8 +539,8 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec,
                     g_sig.SetPoint(i, nsep.GetV2()[i], nsep.GetV1()[i])
                     g_sig.SetPointError(i, 0, nsep.GetV3()[i])
                 for i in range(len(w_vec)):
-                    sig_X_fit = g_sig.GetY()[i]# / (g_vec[i])
-                    sig_X_fit_err = g_sig.GetEY()[i]# / (g_vec[i])
+                    sig_X_fit = g_sig.GetY()[i] / (g_vec[i])
+                    sig_X_fit_err = g_sig.GetEY()[i] / (g_vec[i])
                     g_sig_fit.SetPoint(i, g_sig.GetX()[i], sig_X_fit)
                     g_sig_fit.SetPointError(i, 0, sig_X_fit_err)
                 if sig_name == "L":
@@ -611,6 +611,13 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec,
             latex.DrawLatex(0.35, 0.85, f"Best #chi^{{2}}: {best_overall_cost:.3f}")
             c2.Update()
             print("\n")
+        c2.Update()
+        c3.Update()
+        c4.Update()
+        c5.Update()
+        c6.Update()
+        c7.Update()
+        c8.Update()
         # Print all canvases to the output PDF
         c2.Print(outputpdf+'(')
         c3.Print(outputpdf)
