b616d5a0deb36e5d70e028d106ae3e9bd064582c
Richard Trotta <trotta@cua.edu>
2025-02-04 06:57:31 -0500

    Optimizing all Q2

--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-04 06:56:46 trottar"
+# Time-stamp: "2025-02-04 06:57:25 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -332,8 +332,7 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_e
                             # On error => re-random
                             if debug:
                                 print(f"[DEBUG] Exception => {str(e)}, re-randomizing.")
-                            #initial_param_bounds = random.uniform(0.0, initial_param_bounds)
-                            initial_param_bounds = initial_param_bounds * 0.9
+                            initial_param_bounds = random.uniform(100.0, initial_param_bounds)
                             current_params = [
                                 random.uniform(-initial_param_bounds, initial_param_bounds)
                                 for _ in range(num_params)
