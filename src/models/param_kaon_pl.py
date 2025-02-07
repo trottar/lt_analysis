c87d545f1de1308fd66b16ff10618c55e9f477c6
Richard Trotta <trotta@cua.edu>
2025-02-06 19:33:43 -0500

    Testing Q2=4.4, W=2.74

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-06 17:05:02 trottar"
+# Time-stamp: "2025-02-06 19:33:41 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -81,8 +81,6 @@ def iterWeight(arg_str):
     sig = sig * wfactor
 
     sig = sig / 2.0 / math.pi / 1e6  # dsig/dtdphicm in microbarns/MeV**2/rad
-
-    sig = sig / 1e3
     
     try:
         wtn = weight_prev_iter * (sig / sig_prev_iter)
