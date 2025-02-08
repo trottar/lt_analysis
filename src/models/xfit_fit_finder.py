c9cb2fffd0a57c4136fae8e4e671e1450cb0af40
Richard Trotta <trotta@cua.edu>
2025-02-07 20:26:27 -0500

    Testing Q2=4.4, W=2.74

--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-07 15:11:46 trottar"
+# Time-stamp: "2025-02-07 20:26:25 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -239,8 +239,8 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_e
                                 g_sig.SetPointError(i_data, 0, y_err)
 
                             for i_pt in range(len(w_vec)):
-                                sig_X_fit = g_sig.GetY()[i_pt]# / (g_vec[i_pt])
-                                sig_X_fit_err = g_sig.GetEY()[i_pt]# / (g_vec[i_pt])
+                                sig_X_fit = g_sig.GetY()[i_pt] / (g_vec[i_pt]) / 1e3
+                                sig_X_fit_err = g_sig.GetEY()[i_pt] / (g_vec[i_pt]) / 1e3
                                 graphs_sig_fit[it].SetPoint(i_pt, g_sig.GetX()[i_pt], sig_X_fit)
                                 graphs_sig_fit[it].SetPointError(i_pt, 0, sig_X_fit_err)
 
@@ -553,8 +553,8 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_e
                     g_sig.SetPoint(i_data, x_val, y_val)
                     g_sig.SetPointError(i_data, 0, y_err)
                 for i_pt in range(len(w_vec)):
-                    sig_X_fit = g_sig.GetY()[i_pt]# / (g_vec[i_pt])
-                    sig_X_fit_err = g_sig.GetEY()[i_pt]# / (g_vec[i_pt])
+                    sig_X_fit = g_sig.GetY()[i_pt] / (g_vec[i_pt]) / 1e3
+                    sig_X_fit_err = g_sig.GetEY()[i_pt] / (g_vec[i_pt]) / 1e3
                     graphs_sig_fit[it].SetPoint(i_pt, g_sig.GetX()[i_pt], sig_X_fit)
                     graphs_sig_fit[it].SetPointError(i_pt, 0, sig_X_fit_err)
                 fits_sig[it].SetParNames(*[f"p{4*it + i}" for i in range(num_params)])
