d5a78041781ab532b98728afc6bf25f5148b2cce
Richard Trotta <trotta@cua.edu>
2025-02-11 14:17:12 -0500

    Testing Q2=4.4, W=2.74

--- src/models/xfit_fit_finder.py
+++ src/models/xfit_fit_finder.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-08 17:31:05 trottar"
+# Time-stamp: "2025-02-11 14:17:06 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -111,7 +111,7 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_e
     # -----------------------------------------------------------------------------
     for it, (sig_name, val) in enumerate(fit_params.items()):
 
-        for b in [2]:
+        for b in [0]:
             if sig_name not in fixed_params:
 
                 num_params, init_params, equation_str = inpDict["initial_params"](sig_name, val)
