3da78da6ceeb6c85749f5e01b4d825b574ee173b
Richard Trotta <trotta@cua.edu>
2025-03-02 07:10:20 -0500

    Testing Q2=4.4,5.5

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2025-02-28 21:52:00 trottar"
+# Time-stamp: "2025-03-02 07:10:17 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -150,7 +150,7 @@ def single_setting(q2_set, w_set, fn_lo, fn_hi):
         print(" Starting t-bin {0} (t={1:.4f})...".format(i+1, float(t_list[i])))
         print("\n/*--------------------------------------------------*/\n\n")
 
-        tcut = "t=={0} && x>0.0".format(float(t_list[i]))
+        tcut = "t=={0} && x<=0.0".format(float(t_list[i]))
         #tcut = "t=={0} && x!=0.0".format(float(t_list[i]))
         #tcut = "t=={0} && x!=0.0 && (phi>25)".format(float(t_list[i]))
         print(tcut)
