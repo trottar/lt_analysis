5873cc8d5984a6edbbaafe5d661ec8a22b42da71
Richard Trotta <trotta@cua.edu>
2025-04-01 01:17:19 -0400

    Testing Q2=4.4,5.5

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -732,7 +732,8 @@ elif [[ $Q2 = "3p0" && $W = "2p32" ]]; then
     NumtBins=4
     NumPhiBins=10
     TMIN=0.350
-    TMAX=0.850    
+    #TMAX=0.850
+    TMAX=0.750
 elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
     # Q2=3p0, W=3p14
     #NumtBins=6
