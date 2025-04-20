aa73052454d4d3579866556dbe01c8b44c0d643d
trottar <trottar@jlab.org>
2025-04-20 15:55:12 -0400

    Testing Q2=3.0,W=2.32

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -87,8 +87,8 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
         fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
     else:
         #fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
-        fixed_params = ["L", "LT"]
-        #fixed_params = ["LT", "TT"]
+        #fixed_params = ["L", "LT"]
+        fixed_params = ["L", "LT", "TT"]
         #fixed_params = ["TT"]
         #fixed_params = [] # Update all
     
