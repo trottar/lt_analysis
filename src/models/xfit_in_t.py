fde3c9b3387a06b2fab5871202e8e6663fd72cf9
trottar <trottar@jlab.org>
2025-04-20 22:00:49 -0400

    Testing Q2=3.0,W=3.14

--- src/models/xfit_in_t.py
+++ src/models/xfit_in_t.py
@@ -88,9 +88,9 @@ def x_fit_in_t(ParticleType, pol_str, dir_iter, q2_set, w_set, inpDict, output_f
     else:
         #fixed_params = ["L", "T", "LT", "TT"] # Skip optimization
         #fixed_params = ["L", "LT"]
-        fixed_params = ["L", "LT", "TT"]
+        #fixed_params = ["L", "LT", "TT"]
         #fixed_params = ["TT"]
-        #fixed_params = [] # Update all
+        fixed_params = [] # Update all
     
     # Maximum iterations before ending loop (should always aim for >10000)
     #max_iterations = 1000
