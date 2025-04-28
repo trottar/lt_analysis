997456b12ac699a822c39e61623c9274e7993720
trottar <trottar@jlab.org>
2025-04-27 22:57:20 -0400

    Testing Q2=3.0,W=3.14

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -756,8 +756,8 @@ elif [[ $Q2 = "3p0" && $W = "3p14" ]]; then
 	#
     NumtBins=5
     NumPhiBins=15
-    TMIN=0.150
-    TMAX=0.300
+    TMIN=0.170
+    TMAX=0.400
 elif [[ $Q2 = "4p4" && $W = "2p74" ]]; then
     # Q2=4p4, W=2p74
     #NumtBins=6
