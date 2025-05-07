8675f677bf51e1cbdd246abcd2386115f5ba42fb
trottar <trottar@jlab.org>
2025-05-07 16:50:31 -0400

    Updating unsep fit Q2=3.0, W=2.32

--- src/models/lt_2D_fit.py
+++ src/models/lt_2D_fit.py
@@ -266,10 +266,10 @@ def single_setting(q2_set, w_set, fn_lo, fn_hi):
 
         # Set parameter 0 and 1
         fff2.SetParameter(0, 1)
-        fff2.SetParLimits(0, -5.0, 1000.0)
+        fff2.SetParLimits(0, -1e6, 1e6)
 
         fff2.SetParameter(1, 1)
-        fff2.SetParLimits(1, -5.0, 1000.0)
+        fff2.SetParLimits(1, -1e6, 1e6)
 
         # Fix parameter 2 and 3
         fff2.FixParameter(2, 0.0)
@@ -312,7 +312,7 @@ def single_setting(q2_set, w_set, fn_lo, fn_hi):
 
         # Set parameter 2
         fff2.SetParameter(2, 0.0)
-        fff2.SetParLimits(2, -5.0, 1000.0)
+        fff2.SetParLimits(2, -1e6, 1e6)
 
         # Options: M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
         g_plot_err.Fit(fff2, "MRQ")
@@ -373,7 +373,7 @@ def single_setting(q2_set, w_set, fn_lo, fn_hi):
 
         # Set parameter 3
         fff2.SetParameter(3, 0.0)
-        fff2.SetParLimits(3, -5.0, 1000.0)
+        fff2.SetParLimits(3, -1e6, 1e6)
 
         # Options: M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
         g_plot_err.Fit(fff2, "MRQ")
@@ -463,10 +463,10 @@ def single_setting(q2_set, w_set, fn_lo, fn_hi):
         fff2.ReleaseParameter(3)
 
         # Set parameter limits
-        fff2.SetParLimits(0, -5.0, 1000.0)
-        fff2.SetParLimits(1, -5.0, 1000.0)
-        fff2.SetParLimits(2, -5.0, 1000.0)
-        fff2.SetParLimits(3, -5.0, 1000.0)
+        fff2.SetParLimits(0, -1e6, 1e6)
+        fff2.SetParLimits(1, -1e6, 1e6)
+        fff2.SetParLimits(2, -1e6, 1e6)
+        fff2.SetParLimits(3, -1e6, 1e6)
 
         # Fit
         # Options: M-> Improve fit info splash, R-> Use range specified, Q-> Quiet splash
