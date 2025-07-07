cd031f4d39e2136bd41d65b657803ae2e6bd03e9
trottar <trottar@jlab.org>
2025-07-07 11:12:01 -0400

    Rerunning simc for Q2=4.4, W=2.74

--- src/binning/ave_per_bin.py
+++ src/binning/ave_per_bin.py
@@ -458,7 +458,7 @@ def process_hist_data(tree_data, tree_dummy, t_bins, nWindows, phi_setting, inpD
                     subDict[f"H_MM_nosub_SUB_DATA_{j}"],
                     pi_mm_min, pi_mm_max
                 )
-                scale_factor = (kaon_amp / pion_background_amp) * 0.25                              
+                scale_factor = (kaon_amp / pion_background_amp)                          
                 ##############
                 ##############
                 ##############                
--- src/cuts/rand_sub.py
+++ src/cuts/rand_sub.py
@@ -1660,7 +1660,7 @@ def rand_sub(phi_setting, inpDict):
                 subDict["H_MM_nosub_SUB_DATA"],
                 pi_mm_min, pi_mm_max
             )
-            scale_factor = (kaon_amp / pion_background_amp) * 0.25
+            scale_factor = (kaon_amp / pion_background_amp)
             ##############
             ##############
             ##############
