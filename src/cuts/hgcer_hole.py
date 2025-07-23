1f03c748aebe728c7889fd35270099c25ca923d2
trottar <trottar@jlab.org>
2025-07-23 12:13:40 -0400

    Optimizing functions for Q2=4.4, W=2.74

--- src/cuts/hgcer_hole.py
+++ src/cuts/hgcer_hole.py
@@ -17,6 +17,33 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET):
 
     # Defined HGCer Geometric cuts
     cutg = TCutG("cutg",21)
+    cutg.SetVarX("P_hgcer_yAtCer")
+    cutg.SetVarY("P_hgcer_xAtCer")
+    cutg.SetPoint(0, -25, 2)
+    cutg.SetPoint(1, -2, 2)
+    cutg.SetPoint(2, -1, 2.5)
+    cutg.SetPoint(3, 0, 3)
+    cutg.SetPoint(4, 1, 3)
+    cutg.SetPoint(5, 2, 3.3)
+    cutg.SetPoint(6, 3, 3.0)
+    cutg.SetPoint(7, 4, 2.5)
+    cutg.SetPoint(8, 5, 2)
+    cutg.SetPoint(9, 25, 2)
+    cutg.SetPoint(10, 25, 0.5)
+    cutg.SetPoint(11, 5, 0.5)
+    cutg.SetPoint(12, 4, 1)
+    cutg.SetPoint(13, 3, -1)
+    cutg.SetPoint(14, 2, -2)
+    cutg.SetPoint(15, 1, -2.3)
+    cutg.SetPoint(16, 0, -1.5)
+    cutg.SetPoint(17, -1, -1)
+    cutg.SetPoint(18, -2, 0.5)
+    cutg.SetPoint(19, -25, 0.5)
+    cutg.SetPoint(20, -25, 2)
+
+    '''
+    # OG: Defined HGCer Geometric cuts
+    cutg = TCutG("cutg",21)
     cutg.SetVarX("P_hgcer_xAtCer")
     cutg.SetVarY("P_hgcer_yAtCer")
     cutg.SetPoint(0, 2, -25)
@@ -40,7 +67,8 @@ def apply_HGCer_hole_cut(Q2, W, EPSSET):
     cutg.SetPoint(18, 0.5, -2)
     cutg.SetPoint(19, 0.5, -25)
     cutg.SetPoint(20, 2, -25)
-
+    '''
+    
     '''
     # Defined HGCer Geometric cuts
     cutg = TCutG("cutg",21)
