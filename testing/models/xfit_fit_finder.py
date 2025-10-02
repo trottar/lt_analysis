9b35ec0cf66d50dff4abad7f29da12b98bcef010
trottar <trottar@jlab.org>
2025-10-02 04:06:09 -0400

    Adding testing enviroment for checking algorithm with other datasets

--- testing/models/xfit_fit_finder.py
+++ testing/models/xfit_fit_finder.py
@@ -412,12 +412,32 @@ def parameterize(inpDict, par_vec, par_err_vec, par_chi2_vec, prv_par_vec, prv_e
                 fits_sig[it].SetParNames(*[f"p{4*it + i}" for i in range(num_params)])
                 for i in range(num_params):
                     fits_sig[it].FixParameter(i, best_overall_params[i])
+
+                # ---- Clamp the TF1 domain to the data (prevents exp overflow during sampling)
+                # You already computed x_min/x_max just above via graphs_sig_fit[it]
+                tmin = float(x_min)
+                tmax = float(x_max)
+                fits_sig[it].SetRange(tmin, tmax)
+
+                # ---- Sample safely for plotting
                 n_points = 100
-                fit_y_values = [fits_sig[it].Eval(x) for x in np.linspace(tmin_range, tmax_range, n_points)]
-                fit_y_min = min(fit_y_values)
-                fit_y_max = max(fit_y_values)
-                y_min = min(y_min, fit_y_min)
-                y_max = max(y_max, fit_y_max)
+                tgrid = np.linspace(tmin, tmax, n_points)
+
+                def _eval_safe(f, x):
+                    try:
+                        y = float(f.Eval(float(x)))
+                        return y if np.isfinite(y) else float('nan')
+                    except OverflowError:
+                        return float('nan')
+
+                fit_y_values = np.array([_eval_safe(fits_sig[it], x) for x in tgrid])
+                finite_vals = fit_y_values[np.isfinite(fit_y_values)]
+                if finite_vals.size > 0:
+                    fit_y_min = float(finite_vals.min())
+                    fit_y_max = float(finite_vals.max())
+                    y_min = min(y_min, fit_y_min)
+                    y_max = max(y_max, fit_y_max)
+                # (If all NaN, we just leave y_min/y_max unchanged)
                 margin = 0.1 * (y_max - y_min)
                 graphs_sig_fit[it].GetYaxis().SetRangeUser(y_min - margin, y_max + margin)
                 r_sig_fit = graphs_sig_fit[it].Fit(fits_sig[it], "SQ")
