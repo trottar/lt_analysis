362c1fa49491e1a5cc7419c9c28fe9663dbd489a
trottar <trottar@jlab.org>
2025-10-08 02:51:18 -0400

    Finding best parameterization for Q2=3.0, W=2.32/Q2=4.4, W=2.74/Q2=5.5, W=3.02

--- test_model.sh
+++ test_model.sh
@@ -38,15 +38,15 @@ LTANAPATH=`echo ${PATHFILE_INFO} | cut -d ','  -f17`
 #TMIN=0.001
 #TMAX=0.300
 #
-#Q2="2p4"
-#W="2p22"
-#TMIN=0.100
-#TMAX=0.600
-# Combined Fpi-2
-Q2="2p0"
+Q2="2p4"
 W="2p22"
-TMIN=0.001
+TMIN=0.100
 TMAX=0.600
+# Combined Fpi-2
+#Q2="2p0"
+#W="2p22"
+#TMIN=0.001
+#TMAX=0.600
 ParticleType="pion"
 
 POL="+1" # Positive polarity
