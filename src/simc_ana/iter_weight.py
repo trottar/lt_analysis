0b8d201f8ab121194acb2d9367ddef00efa72815
trottar <trottar@jlab.org>
2025-10-24 13:03:48 -0400

    Finding best parameterization for Q2=3.0, W=2.32/Q2=4.4, W=2.74/Q2=5.5, W=3.02

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -348,12 +348,9 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
           # After 8/28/2024
           #inp_param = '{} {} {} {} {} {} {} {} {} {} '\
                       #.format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epsilon, evt.thetacm, evt.phipqi, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)
-          # After 7/31/2025
+          # After 10/24/2025
           inp_param = '{} {} {} {} {} {} {} {} {} {} '\
-                    .format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epscm, evt.thetacm, evt.phipqi, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)          
-                    #.format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epsilon, evt.thetapq, evt.phipqi+math.pi, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)                    
-
-          print(f"thetapq: {evt.thetapq},thetacm: {evt.thetacm}, phipq: {evt.phipq}, phipqi: {evt.phipqi}:{evt.phipqi})")
+                    .format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epsilon, evt.thetapqi, evt.phipqi, evt.iter_sig, evt.iter_weight)+' '.join(param_arr)
           
           iter_lst = iterWeight(inp_param)
 
@@ -386,10 +383,9 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
           # After 8/28/2024
           #inp_param = '{} {} {} {} {} {} {} {} {} {} '\
                       #.format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epsilon, evt.thetacm, evt.phipq, evt.sigcm, evt.Weight)+' '.join(param_arr)
-          # After 7/31/2025
+          # After 10/24/2025
           inp_param = '{} {} {} {} {} {} {} {} {} {} '\
-                    .format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epscm, evt.thetacm, evt.phipqi, evt.sigcm, evt.Weight)+' '.join(param_arr)                    
-                    #.format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epsilon, evt.thetapq, evt.phipqi-math.pi, evt.sigcm, evt.Weight)+' '.join(param_arr)            
+                    .format(Q2, W, evt.Q2i, evt.Wi, evt.ti, evt.epsilon, evt.thetapqi, evt.phipqi, evt.sigcm, evt.Weight)+' '.join(param_arr)
 
           iter_lst = iterWeight(inp_param)
 
