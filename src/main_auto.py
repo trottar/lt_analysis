01fac57c033003548d28d67037b98acc36783409
trottar <trottar@jlab.org>
2025-11-12 23:28:18 -0500

    Added automated iteration algorithm, testing now...

--- src/main_auto.py
+++ src/main_auto.py
@@ -220,7 +220,7 @@ inpDict["iter_num"] = iter_num
 inpDict["LOEPS"] = LOEPS
 inpDict["HIEPS"] = HIEPS
 
-
+## DEBUG ##
 if iter_num > 1:        
     # Track continues
     c = 1
@@ -252,6 +252,8 @@ if iter_num > 1:
 
     print("-"*50)
     print("-"*50)
+sys.exit(2)
+## DEBUG ##    
 
 # Copy input model to specific particle type directory
 print("\nCopying {} to {}".format('{}/src/models/Q{}W{}.model'.format(LTANAPATH, Q2, W), '{}/src/{}/functions/Q{}W{}.model'.format(LTANAPATH, ParticleType, Q2, W)))
