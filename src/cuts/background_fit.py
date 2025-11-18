53c589ec19e8bb1cde16fba6b12f673410ad445e
trottar <trottar@jlab.org>
2025-11-17 21:05:50 -0500

    Added automated iteration algorithm, testing now...

--- src/cuts/background_fit.py
+++ src/cuts/background_fit.py
@@ -364,7 +364,7 @@ def is_good_background_shape(
         x_min,
         x_max,
         *,
-        neg_tol=0.1,
+        neg_tol=0.25,
 ):
     """
     Sanity check on the *fit function itself*.
