0ce67bdd2a1aea506dfa00553a34adc560c0c263
trottar <trottar@jlab.org>
2025-12-16 23:39:49 -0500

    Testing new empirical fits for Q2=3.0/W=2.32

--- src/average_ratios.f
+++ src/average_ratios.f
@@ -189,7 +189,9 @@ c      pause
          do ip=1,nphi
             r=0.
             e=0.
-            if (ymc(ip,it) /= 0.0 .and. .not. isnan(ymc(ip,it))) then
+*     RLT: Calculate ratio only if both yields are non-zero            
+            if (ymc(ip,it) /= 0.0 .and. .not. isnan(ymc(ip,it))
+     *        yrd(ip,it) /= 0.0 .and. .not. isnan(yrd(ip,it))) then
                r=(yrd(ip,it))/ymc(ip,it)
 *               r=1.0
 *     Calculate ratio error in quadrature (absolute error)
