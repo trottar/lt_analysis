1ff2ef1224ef3e04631925b355b4dca00788d288
trottar <trottar@jlab.org>
2026-01-15 02:48:57 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/main.py
+++ src/main.py
@@ -277,8 +277,7 @@ print("-"*25)
 inpDict["Epsmin"] = 0.0
 inpDict["Epsmax"] = 1.0
 
-#phisetlist = ["Center","Left","Right"]
-phisetlist = ["Center","Left"]
+phisetlist = ["Center","Left","Right"]
 #phisetlist = ["Center"]
 
 for phiset in phisetlist:
