7de11cf1c3b9746324aeaf4fd02b05bdf65b9e4f
trottar <trottar@jlab.org>
2026-02-16 01:19:09 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -1252,8 +1252,8 @@ def process_hist_simc(tree_simc, normfac_simc, t_bins, phi_bins, phi_setting, in
             # Loop through bins in t_simc and identify events in specified bins
             for j in range(len(t_bins)-1):
                 for k in range(len(phi_bins)-1):            
-                    if t_bins[j] <= -evt.t <= t_bins[j+1]:
-                        if phi_bins[k] <= (phi_shift)*(180 / math.pi) <= phi_bins[k+1]:
+                    if t_bins[j] <= -evt.t < t_bins[j+1]:
+                        if phi_bins[k] <= (phi_shift)*(180 / math.pi) < phi_bins[k+1]:
                             if iteration:                                
                                 hist_bin_dict["H_t_SIMC_{}_{}".format(j, k)].Fill(-evt.t, evt.iter_weight)
                                 hist_bin_dict["H_MM_SIMC_{}_{}".format(j, k)].Fill(adj_missmass, evt.iter_weight)
@@ -1437,8 +1437,7 @@ def calculate_yield_simc(kin_type, hist, t_bins, phi_bins, inpDict, iteration):
         arr_simc = np.array(hist_val_simc)
         try:
             #print(f"{i} | SIMC Yield: {np.sum(arr_simc)/bin_width_simc:.3e} =  NumEvts: {np.sum(arr_simc):.3e} / BinWidth: {bin_width_simc:.3e}")
-            #yld = np.sum(arr_simc)
-            yld = mm_hist_simc[i].Integral()
+            yld = np.sum(arr_simc)
             # Calculate simc yield error (relative error)
             # No norm_fac, shouldn't normalize non-weighted distribution
             yld_err = (1/np.sqrt(binned_unweighted_NumEvts_simc[i]))
