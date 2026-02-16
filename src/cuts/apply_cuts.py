84d350fcfe529cce1a8568213a4a947ce082d5b9
trottar <trottar@jlab.org>
2026-02-15 21:26:19 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/cuts/apply_cuts.py
+++ src/cuts/apply_cuts.py
@@ -197,6 +197,6 @@ def apply_simc_cuts(evt, mm_min=0.7, mm_max=1.5):
     MMCUT =  (mm_min<adj_missmass) & (adj_missmass<mm_max)
       
     #ALLCUTS = HMS_Acceptance and SHMS_Acceptance and Diamond and t_RANGE and MMCUT
-    ALLCUTS = SHMS_Acceptance and Diamond and t_RANGE and MMCUT
+    ALLCUTS = HMS_Acceptance and SHMS_Acceptance and Diamond and MMCUT
     
     return ALLCUTS
