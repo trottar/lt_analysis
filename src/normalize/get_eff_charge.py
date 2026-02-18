08a15f8bbe885771e42f225a2973fa2abe9edceb
trottar <trottar@jlab.org>
2026-02-18 02:41:27 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/normalize/get_eff_charge.py
+++ src/normalize/get_eff_charge.py
@@ -301,8 +301,7 @@ def find_events(hist, inpDict):
         if "Ncontribute" in line:
             val = line.split("=")
             simc_nevents = int(val[1])
-        #if "normfac" in line:
-        if "wtcontr" in line:
+        if "normfac" in line:
             val = line.split("=")
             simc_normfactor = float(val[1])
     if 'simc_nevents' and 'simc_normfactor' not in locals():
