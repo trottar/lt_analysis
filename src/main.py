9dbf36794d81eaf51e6a101feb0c38e4349ebb53
trottar <trottar@jlab.org>
2026-02-18 16:59:03 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/main.py
+++ src/main.py
@@ -407,6 +407,7 @@ initial_param_file = '{}/src/models/par_{}_Q{}W{}'.format(LTANAPATH, pol_str, Q2
 sys.path.append("simc_ana")
 from iter_weight import iter_weight
 
+'''
 # Upate hist dictionary with effective charge and simc histograms
 for hist in histlist:
     # ***Create root directory here since it is used for weight iteration***
@@ -423,6 +424,7 @@ for hist in histlist:
     else:
         print("ERROR: Issue with simc root file {}".format(rootFileSimc))
         sys.exit(2)
+'''
 
 # SIMC
 sys.path.append("simc_ana")    
