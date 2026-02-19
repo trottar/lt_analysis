2732910a03bc05b7e06fede9a3c4371061d994f7
trottar <trottar@jlab.org>
2026-02-19 14:07:43 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/binning/calculate_yield.py
+++ src/binning/calculate_yield.py
@@ -1261,8 +1261,8 @@ def process_hist_simc(tree_simc, normfac_simc, t_bins, phi_bins, phi_setting, in
                     for k in range(len(phi_bins)-1):                                
                         if phi_bins[k] <= phi_shift < phi_bins[k+1]:
                             #print("SIMC Event {}: t-bin {} phi-bin {} phi value {}".format(i, j+1, k+1, (phi_shift)*(180 / math.pi)))
-                            hist_bin_dict["H_t_SIMC_{}_{}".format(j, k)].Fill(-evt.t, evt.iter_weight*evt.siglab)
-                            hist_bin_dict["H_MM_SIMC_{}_{}".format(j, k)].Fill(adj_missmass, evt.iter_weight*evt.siglab)
+                            hist_bin_dict["H_t_SIMC_{}_{}".format(j, k)].Fill(-evt.t, evt.iter_weight*normfac_simc)
+                            hist_bin_dict["H_MM_SIMC_{}_{}".format(j, k)].Fill(adj_missmass, evt.iter_weight*normfac_simc)
                             hist_bin_dict["H_MM_SIMC_unweighted_{}_{}".format(j, k)].Fill(adj_missmass)
                             break                            
                     break
@@ -1276,8 +1276,8 @@ def process_hist_simc(tree_simc, normfac_simc, t_bins, phi_bins, phi_setting, in
         for k in range(len(phi_bins)-1):
 
             # Normalize for yields
-            hist_bin_dict["H_MM_SIMC_{}_{}".format(j, k)].Scale(normfac_simc)
-            hist_bin_dict["H_t_SIMC_{}_{}".format(j, k)].Scale(normfac_simc)
+            #hist_bin_dict["H_MM_SIMC_{}_{}".format(j, k)].Scale(normfac_simc)
+            #hist_bin_dict["H_t_SIMC_{}_{}".format(j, k)].Scale(normfac_simc)
             
             processed_dict["t_bin{}phi_bin{}".format(j+1, k+1)] = {
                 "H_MM_SIMC" : hist_bin_dict["H_MM_SIMC_{}_{}".format(j, k)],
