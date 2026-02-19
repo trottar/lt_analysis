482b205763286115e9a7b725c5fc776e20f9619f
trottar <trottar@jlab.org>
2026-02-19 01:02:05 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -84,8 +84,10 @@ def iterWeight(arg_str):
     # Calculate W-factor
     wfactor = fun_wfactor_optimized(q2_set, w_set, qq, ww, tt)    
 
-    sig = (sig_T + eps * sig_L + eps * math.cos(2. * phicm) * sig_TT +
-             math.sqrt(2.0 * eps * (1. + eps)) * math.cos(phicm) * sig_LT)
+    #sig = (sig_T + eps * sig_L + eps * math.cos(2. * phicm) * sig_TT +
+    #         math.sqrt(2.0 * eps * (1. + eps)) * math.cos(phicm) * sig_LT)
+
+    sig = (sig_T + eps * sig_L)
 
     sig = sig * wfactor
 
