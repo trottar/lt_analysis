5d5979414cba5dd89d4181233fdd3125ef497e33
trottar <trottar@jlab.org>
2026-02-19 01:16:50 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/models/param_kaon_pl.py
+++ src/models/param_kaon_pl.py
@@ -87,7 +87,8 @@ def iterWeight(arg_str):
     #sig = (sig_T + eps * sig_L + eps * math.cos(2. * phicm) * sig_TT +
     #         math.sqrt(2.0 * eps * (1. + eps)) * math.cos(phicm) * sig_LT)
 
-    sig = (eps * math.cos(2. * phicm) * sig_TT + math.sqrt(2.0 * eps * (1. + eps)) * math.cos(phicm) * sig_LT)
+    sig = (sig_T + eps * sig_L + eps * sig_TT +
+             math.sqrt(2.0 * eps * (1. + eps)) * sig_LT)
 
     sig = sig * wfactor
 
