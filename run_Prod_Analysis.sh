1d04bc72eb324be121b961a712a10761998f9ff7
trottar <trottar@jlab.org>
2026-02-20 02:07:23 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -789,10 +789,10 @@ elif [[ $Q2 = "4p4" && $W = "2p74" ]]; then
     #TMAX=0.850
     ##TMAX=0.750
 	# Kin
-	#NumtBins=4
-    #NumPhiBins=12
-	NumtBins=1
-    NumPhiBins=3
+	NumtBins=4
+    NumPhiBins=12
+	#NumtBins=1
+    #NumPhiBins=3
     TMIN=0.400
     TMAX=0.750	
 elif [[ $Q2 = "5p5" && $W = "3p02" ]]; then
