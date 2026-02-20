18a7cf58ce2555c952ecd3c2f7ae9f55893a9691
trottar <trottar@jlab.org>
2026-02-20 12:49:35 -0500

    Testing new empirical fits for Q2=3.0/W=2.32 and Q2=4.4/W=2.74

--- src/simc_ana/iter_weight.py
+++ src/simc_ana/iter_weight.py
@@ -205,8 +205,10 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
     TBRANCH_SIMC.SetBranchAddress("phipq", phipq_array);
     TBRANCH_SIMC.SetBranchAddress("phicm", phicm_array);
     TBRANCH_SIMC.SetBranchAddress("missmass", missmass_array);
+    shift_exists = False
     try:
         TBRANCH_SIMC.SetBranchAddress("missmass_shift", missmass_shift_array);    
+        shift_exists = True
     except TypeError:
         print("No missmass_shift branch found...skipping...")
     TBRANCH_SIMC.SetBranchAddress("mmnuc", mmnuc_array);
@@ -287,7 +289,8 @@ def iter_weight(param_file, simc_root, inpDict, phi_setting):
     new_TBRANCH_SIMC.Branch("phipq", phipq_array, "phipq/F");
     new_TBRANCH_SIMC.Branch("phicm", phicm_array, "phicm/F");
     new_TBRANCH_SIMC.Branch("missmass", missmass_array, "missmass/F");
-    new_TBRANCH_SIMC.Branch("missmass_shift", missmass_shift_array, "missmass_shift/F");    
+    if shift_exists:
+        new_TBRANCH_SIMC.Branch("missmass_shift", missmass_shift_array, "missmass_shift/F");    
     new_TBRANCH_SIMC.Branch("mmnuc", mmnuc_array, "mmnuc/F");
     new_TBRANCH_SIMC.Branch("phad", phad_array, "phad/F");
     new_TBRANCH_SIMC.Branch("t", t_array, "t/F");
