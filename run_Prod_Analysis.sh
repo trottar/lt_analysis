33fa3b1c254f220f54b4f5b4b31168ce8ed13f04
trottar <trottar@jlab.org>
2026-03-27 02:12:22 -0400

    Converted theta_cm to sin(theta_cm) throughout scripts

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -794,7 +794,8 @@ elif [[ $Q2 = "4p4" && $W = "2p74" ]]; then
 	NumtBins=3
     NumPhiBins=9
     TMIN=0.400
-    TMAX=0.750	
+	TMAX=0.800	
+    #TMAX=0.750	
 elif [[ $Q2 = "5p5" && $W = "3p02" ]]; then
     # Q2=5p5, W=3p02
     NumtBins=2
