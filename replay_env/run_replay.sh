beb31ff1ccd8c5261900719ad5c0c9919caacb81
trottar <trottar@jlab.org>
2026-03-31 02:12:57 -0400

    Added batch script for skim root files, as well as many other minor changes due to update ltsep package

--- replay_env/run_replay.sh
+++ replay_env/run_replay.sh
@@ -71,7 +71,7 @@ if [[ "${HOST}" = *"farm"* ]]; then
 	source /site/12gev_phys/softenv.sh 2.3
 	source /apps/root/6.18.04/setroot_CUE.bash
     fi
-    source "$HCANAPATH/setup.sh"
+    source "$HCANAPATH"
     cd "$REPLAYPATH"
     source "$REPLAYPATH/setup.sh"
 elif [[ "${HOST}" = *"qcd"* ]]; then
