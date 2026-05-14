c212acf5f70d9cba2bdcf5acb0be20e3c5428a2f
Richard Trotta <trottar@jlab.org>
2026-05-14 13:54:39 -0400

    Fit 1 and 2 used now, 7

--- run_Prod_Analysis.sh
+++ run_Prod_Analysis.sh
@@ -728,10 +728,9 @@ done
 #MissMassMin=0.7
 #MissMassMax=1.5
 # MM cut (kaon)
-##MissMassMin=1.08
-##MissMassMax=1.16
 MissMassMin=1.10
-MissMassMax=1.14
+MissMassMax=1.16
+#MissMassMax=1.14
 #MissMassMax=1.13
 # MM cut (pion, Vijay)
 #MissMassMin=0.91
