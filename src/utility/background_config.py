986197f86f4a7015869f389c0ac36895914c9adc
Richard Trotta <trottar@jlab.org>
2026-06-24 05:06:51 -0400

    Background implementation | 7

--- src/utility/background_config.py
+++ src/utility/background_config.py
@@ -192,7 +192,7 @@ PARTICLE_SUBTRACTION_COMPONENT_FIT_WINDOW_CONFIG = {
     "pion_control": {
         "apply_mm_offset_data": True,
         "staged_fit_passes": 1,
-        "fit_order": ("pi_sidis", "pi_n", "k_sigma0_signal", "pi_delta"),
+        "fit_order": ("pi_n", "pi_sidis", "k_sigma0_signal", "pi_delta"),
         "stage_amplitude_windows": {
             "pi_delta": (1.18, 1.25),
             "k_sigma0_signal": (1.18, 1.23),
@@ -235,14 +235,14 @@ PARTICLE_SUBTRACTION_COMPONENT_FIT_WINDOW_CONFIG = {
         "windows": {
             "pi_n": (0.90, 0.95),
             "pi_delta": (1.16, 1.25),
-            "pi_sidis": ((1.05, 1.10), (1.45, 1.50)),
+            "pi_sidis": ((1.05, 1.10), (1.25, 1.30), (1.45, 1.50)),
             "k_sigma0_signal": (1.17, 1.23),
         },
     },
     "kaon_nosub": {
         "apply_mm_offset_data": True,
         "staged_fit_passes": 1,
-        "fit_order": ("pi_sidis", "pi_n", "k_sigma0_signal", "pi_delta"),
+        "fit_order": ("pi_n", "pi_sidis", "k_sigma0_signal", "pi_delta"),
         "stage_amplitude_windows": {
             "pi_delta": (1.16, 1.23),
             "k_sigma0_signal": (1.17, 1.23),
@@ -288,7 +288,7 @@ PARTICLE_SUBTRACTION_COMPONENT_FIT_WINDOW_CONFIG = {
         "windows": {
             "pi_n": (0.90, 0.95),
             "pi_delta": (1.16, 1.23),
-            "pi_sidis": ((1.05, 1.10), (1.45, 1.50)),
+            "pi_sidis": ((1.05, 1.10), (1.25, 1.30), (1.45, 1.50)),
             "k_sigma0_signal": (1.17, 1.23),
         },
         "enabled_excluded_windows": {
