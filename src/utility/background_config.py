208c440be1b7a8ca4b856e210d246668ffe47817
Richard Trotta <trottar@jlab.org>
2026-06-24 04:12:58 -0400

    Background implementation | 7

--- src/utility/background_config.py
+++ src/utility/background_config.py
@@ -192,7 +192,7 @@ PARTICLE_SUBTRACTION_COMPONENT_FIT_WINDOW_CONFIG = {
     "pion_control": {
         "apply_mm_offset_data": True,
         "staged_fit_passes": 1,
-        "fit_order": ("pi_delta", "pi_n", "k_sigma0_signal", "pi_sidis"),
+        "fit_order": ("pi_n", "pi_sidis", "k_sigma0_signal", "pi_sidis"),
         "stage_amplitude_windows": {
             "pi_delta": (1.18, 1.25),
             "k_sigma0_signal": (1.18, 1.23),
