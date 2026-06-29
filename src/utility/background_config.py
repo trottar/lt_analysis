03e08e6944c9d188073256feb28f6b63bd035db4
Richard Trotta <trottar@jlab.org>
2026-06-29 11:03:18 -0400

    Updating json farm files for different skim root path

--- src/utility/background_config.py
+++ src/utility/background_config.py
@@ -242,7 +242,7 @@ PARTICLE_SUBTRACTION_COMPONENT_FIT_WINDOW_CONFIG = {
             "k_sigma0_signal": False,
         },
         "windows": {
-            "pi_n": (0.90, 0.95),
+            "pi_n": (0.88, 0.95),
             "pi_delta": (1.18, 1.23),
             "pi_sidis": ((1.07, 1.10),(1.45, 1.50)),
             #"pi_sidis": ((1.05, 1.10), (1.25, 1.30), (1.45, 1.50)),
@@ -305,7 +305,7 @@ PARTICLE_SUBTRACTION_COMPONENT_FIT_WINDOW_CONFIG = {
             "k_sigma0_signal": False,
         },
         "windows": {
-            "pi_n": (0.90, 0.95),
+            "pi_n": (0.88, 0.95),
             "pi_delta": (1.18, 1.23),
             "pi_sidis": ((1.07, 1.10),(1.45, 1.50)),
             #"pi_sidis": ((1.05, 1.10), (1.25, 1.30), (1.45, 1.50)),
